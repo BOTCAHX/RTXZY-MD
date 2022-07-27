6a7bc48ba6c6bb47a9bb7c4669ec7a3fe936201d
[TioXd] <94074076+BOTCAHX@users.noreply.github.com>
2022-07-27 20:23:46 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+pkg update && pkg upgrade
+pkg install git -y
+pkg install nodejs -y
+pkg install ffmpeg -y
+pkg install imagemagick -y
+pkg install yarn
+git clone https://github.com/BOTCAHX/BOTWA-2022
+cd BOTWA-2022
+yarn
+node .
+
