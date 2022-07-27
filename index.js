b1f0fb7f060577ff361758ba879d02c0d3ca5440
[TioXd] <94074076+BOTCAHX@users.noreply.github.com>
2022-07-27 20:24:30 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,70 @@
+console.log('Starting...')
+let cluster = require('cluster')
+let path = require('path')
+let fs = require('fs')
+let package = require('./package.json')
+const CFonts = require('cfonts')
+const Readline = require('readline')
+const yargs = require('yargs/yargs')
+const rl = Readline.createInterface(process.stdin, process.stdout)
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+
+CFonts.say('BOT BY\n BOTCAHX', {
+  colors: ['blueBright','yellowBright'],                                        font: 'block',
+  align: 'center',
+})
+CFonts.say(`BY BOTCAHX`, {                                            
+colors: ['yellow'],                                                           
+font: 'console',                                                              align: 'center',
+})
+
+var isRunning = false
+/**
+ * Start a js file
+ * @param {String} file `path/to/file`
+ */
+function start(file) {
+  if (isRunning) return
+  isRunning = true
+  let args = [path.join(__dirname, file), ...process.argv.slice(2)]
+  CFonts.say([process.argv[0], ...args].join(' '), {
+    font: 'console',
+    align: 'center',
+    gradient: ['red', 'magenta']
+  })
+  cluster.setupMaster({
+    exec: path.join(__dirname, file),
+    args: args.slice(1),
+  })
+  let p = cluster.fork()
+  p.on('message', data => {
+    console.log('[RECEIVED]', data)
+    switch (data) {
+      case 'reset':
+        p.kill()
+        isRunning = false
+        start.apply(this, arguments)
+        break
+      case 'uptime':
+        p.send(process.uptime())
+        break
+    }
+  })
+  p.on('exit', code => {
+    isRunning = false
+    console.error('Exited with code:', code)
+    if (code === 0) return
+    fs.watchFile(args[0], () => {
+      fs.unwatchFile(args[0])
+      start(file)
+    })
+  })
+  let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+  if (!opts['test'])
+    if (!rl.listenerCount()) rl.on('line', line => {
+      p.emit('message', line.trim())
+    })
+  // console.log(p)
+}
+
+start('main.js')
