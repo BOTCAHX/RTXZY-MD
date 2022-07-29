9b3676ef2d7e1f7661c6eb07fe70c529dda1822c
[TioXd] <94074076+BOTCAHX@users.noreply.github.com>
2022-07-29 19:50:19 +0700

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,9 +1,3 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+//base awal replit respond
 
-conn.sendImageAsSticker(m.chat, 'https://database.tioclkp02.repl.co/f61a7c96f3f86b6c5214f.png', m, { packname: "Database Replit", author: "replit @tioclkp02 500gb Room\n\n> No enc, Private Content" })
-}
-
-handler.customPrefix = /^(ok)$/i
-handler.command = new RegExp
-
-module.exports = handler
+function _0x2bb4(_0xe3b306,_0x576335){const _0x5c6356=_0x5c63();return _0x2bb4=function(_0x2bb4b1,_0x5fba69){_0x2bb4b1=_0x2bb4b1-0xf2;let _0xecd14d=_0x5c6356[_0x2bb4b1];return _0xecd14d;},_0x2bb4(_0xe3b306,_0x576335);}function _0x5c63(){const _0x47b235=['2021466zvVJZV','https://database.tioclkp02.repl.co/f61a7c96f3f86b6c5214f.png','5wHcfvD','replit\x20@tioclkp02\x20500gb\x20Room\x0a\x0a>\x20No\x20enc,\x20Private\x20Content','1082394NztDGX','21014hmGVNb','934484FVDcIy','477468PrZJGn','sendImageAsSticker','150EJmvgV','55342uIdRgV','Database\x20Replit','106205WwfENp','38sQbVBl','40QmfHAq'];_0x5c63=function(){return _0x47b235;};return _0x5c63();}(function(_0x11ae8b,_0x5d118f){const _0x1f8670=_0x2bb4,_0x4d553c=_0x11ae8b();while(!![]){try{const _0x1437d5=-parseInt(_0x1f8670(0xfe))/0x1*(parseInt(_0x1f8670(0xf6))/0x2)+-parseInt(_0x1f8670(0xf8))/0x3+-parseInt(_0x1f8670(0xf7))/0x4*(-parseInt(_0x1f8670(0xf3))/0x5)+parseInt(_0x1f8670(0x100))/0x6+parseInt(_0x1f8670(0xfb))/0x7*(-parseInt(_0x1f8670(0xff))/0x8)+parseInt(_0x1f8670(0xf5))/0x9+-parseInt(_0x1f8670(0xfa))/0xa*(-parseInt(_0x1f8670(0xfd))/0xb);if(_0x1437d5===_0x5d118f)break;else _0x4d553c['push'](_0x4d553c['shift']());}catch(_0x2d3d3e){_0x4d553c['push'](_0x4d553c['shift']());}}}(_0x5c63,0x3a067));let handler=async(_0x4af37c,{conn:_0x34d598,text:_0x2811fa,usedPrefix:_0x4ad158,command:_0x1d4da8})=>{const _0x573170=_0x2bb4;_0x34d598[_0x573170(0xf9)](_0x4af37c['chat'],_0x573170(0xf2),_0x4af37c,{'packname':_0x573170(0xfc),'author':_0x573170(0xf4)});};handler['customPrefix']=/^(ok)$/i,handler['command']=new RegExp(),module['exports']=handler;
