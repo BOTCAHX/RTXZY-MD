e5e7f7505d6d9aa2f44b47cb98a095def982c1ee
[TioXd] <94074076+BOTCAHX@users.noreply.github.com>
2022-08-04 16:45:42 +0700

    Create _restmaker3.js

---
+++ plugins/_restmaker3.js
@@ -0,0 +1,13 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ').split('|')
+  if (!args[0]) throw 'Masukkan Text\nContoh : .flaming4 BOTCAHX'
+  m.reply('_Proses..._')
+  let res = `https://botcahx-rest-api.herokuapp.com/api/photooxy/naruto?text=${response[0]}`
+  conn.sendFile(m.chat, res, 'botcahx.jpg', `© BOTCAHX`, m, false)
+}
+handler.help = ['flaming9'].map(v => v + ' <text>')
+handler.tags = ['maker']
+handler.command = /^(flaming9)$/i
+
+module.exports = handler
