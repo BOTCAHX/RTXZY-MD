c861aca4a2bda512d15ed2d2da172c06dcb63906
[TioXd] <94074076+BOTCAHX@users.noreply.github.com>
2022-08-05 10:49:17 +0700

    Update anime-all.js

--- plugins/anime-all.js
+++ plugins/anime-all.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, command }) => {
-let nyenye = `https://sekha.me/api/wallpaper/${command}?apikey=apirey`
+let nyenye = `https://api-reysekha.herokuapp.com/api/wallpaper/${command}?apikey=APIKEY`
     conn.sendButtonImg(m.chat, nyenye, 'Nih', wm2, 'Next', `.${command}`, m) 
 }
 handler.help = ['akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'hinata', 'inori', 'isuzu', 'itachi', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumi', 'madara', 'mikasa', 'miku', 'minato', 'naruto', 'nezuko', 'sagiri', 'sasuke', 'sakura']
