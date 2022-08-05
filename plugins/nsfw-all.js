baa287ec3ce397f96850e3dc77a3eecf6fd87a40
[TioXd] <94074076+BOTCAHX@users.noreply.github.com>
2022-08-06 01:49:25 +0700

    Update nsfw-all.js

--- plugins/nsfw-all.js
+++ plugins/nsfw-all.js
@@ -1,9 +1,9 @@
+
 let handler = async (m, { conn, command }) => {
-let nyenye = `https://zenzapis.xyz/api/morensfw/${command}?apikey=${zenzkey}`
+let nyenye = `https://zenzapis.xyz/api/morensfw/${command}?apikey=80b5045dce`
     conn.sendButtonImg(m.chat, nyenye, 'Nih', wm2, 'Next', `.${command}`, m) 
 }
-handler.help = ['ahegao', 'ass', 'bdsm', 'blowjob', 'cum', 'ero', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'jahy', 'maid', 'manga', 'neko', 'orgy', 'panties', 'pussy','sfwneko', 'tentacles', 'thighs', 'yuri']
+handler.help = ['ahegao', 'ass', 'bdsm', 'blowjob', 'cum', 'cuckold', 'ero', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'hentaigif', 'jahy', 'maid', 'manga', 'neko', 'orgy', 'panties', 'pussy','sfwneko', 'tentacles', 'thighs', 'yuri', 'zettairyouiki']
 handler.tags = ['nsfw']
-handler.command = /^(ahegao|ass|bdsm|blowjob|cum|ero|femdom|foot|gangbang|glasses|hentai|jahy|maid|manga|neko|orgy|panties|pussy|sfwneko|tentacles|thighs|yuri)$/i
-//buatan hyzer, jgn hapus atuh 😊
+handler.command = /^(ahegao|ass|bdsm|blowjob|cum|cuckold|ero|femdom|foot|gangbang|glasses|hentai|hentaigif|jahy|maid|manga|neko|orgy|panties|pussy|sfwneko|tentacles|thighs|yuri|zettairyouiki)$/i
 module.exports = handler
