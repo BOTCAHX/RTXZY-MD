06caf75f3e1a90193d2524d6e8f8a1c7d27c0fac
[TioXd] <94074076+BOTCAHX@users.noreply.github.com>
2022-08-05 10:29:13 +0700

    Update ssweb.js

--- plugins/ssweb.js
+++ plugins/ssweb.js
@@ -1,9 +1,17 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command, args }) => {
+  if (args[0] === 'Nekopoi.care') {
+      conn.reply(m.chat, '*Tobat woy*', m)
+      reject
+  }
+  if (args[0] === 'xnxx.healt') {
+      conn.reply(m.chat, '*Tobat woy*', m)
+      reject
+  }
   let full = /f$/i.test(command)
   if (!args[0]) return conn.reply(m.chat, 'Tidak ada url', m)
   let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
-  let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
+  let ss = await (await fetch('https://botcahx-rest-api.herokuapp.com/api/tools/ssweb?link=' + encodeURIComponent(url) + '&device=desktop&full=on')).buffer()
   conn.sendFile(m.chat, ss, 'screenshot.png', url, m)
 }
 handler.help = ['ss', 'ssf'].map(v => v + ' <url>')
