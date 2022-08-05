3d217999d314fe6317b84b17e36e4036f64523bb
[TioXd] <94074076+BOTCAHX@users.noreply.github.com>
2022-08-06 01:47:06 +0700

    Update internet-cerpen.js

--- plugins/internet-cerpen.js
+++ plugins/internet-cerpen.js
@@ -8,28 +8,16 @@ let handler = async(m, { conn, text }) => {
 *Kategori*: ${res.data.result.kategori}
 *Story*:
 ${res.data.result.story}`
-    const ftroli = {
-    key : {
-    remoteJid: '6283136505591-1614953337@g.us',
-    participant : '0@s.whatsapp.net'
-    },
-    message: {
-    orderMessage: {
-    itemCount : 26-9999, 
-    status: 1,
-    surface : 1,
-    message: 'Random Cerita Pendek', 
-    orderTitle: `▮Menu ▸`,
-    thumbnail: 'https://telegra.ph/file/5ecbec3e82e247671a18e.jpg', 
-    sellerJid: '0@s.whatsapp.net' 
-    }
-    }
-    }
-    conn.reply(m.chat, hasil, ftroli)
+
+    conn.reply(m.chat, hasil, m)
 	})
 }
 handler.help = ['cerpen']
-handler.tags = ['fun', 'internet']
+handler.tags = ['fun']
 handler.command = /^(cerpen)$/i
+
+handler.fail = null
+handler.exp = 0
+handler.limit = true
 
 module.exports = handler
