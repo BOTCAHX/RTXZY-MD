a96ede9484d77b7199d3d8a9b04437f7b56e39b6
[TioXd] <94074076+BOTCAHX@users.noreply.github.com>
2022-08-10 19:39:19 +0700

    Update sesion.js

--- plugins/sesion.js
+++ plugins/sesion.js
@@ -1,8 +1,8 @@
 let fs = require('fs')
 let handler = async (m, { conn, text }) => {
     m.reply('Tunggu Sebentar, Proses Getting File wamd.data.json')
-    let sesi = await fs.readFileSync('./wamd.data.json')
-    return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'wamd.data.json' }, { quoted: m })
+    let sesi = await fs.readFileSync('./sesiontxzy.data.json')
+    return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'sesiontxzy.data.json' }, { quoted: m })
 }
 handler.help = ['getsessi']
 handler.tags = ['internet']
