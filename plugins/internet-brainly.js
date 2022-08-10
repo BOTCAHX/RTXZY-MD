9e70f8917317c8c357baf19afd584fcd926052ff
[TioXd] <94074076+BOTCAHX@users.noreply.github.com>
2022-08-10 19:31:55 +0700

    Update internet-brainly.js

--- plugins/internet-brainly.js
+++ plugins/internet-brainly.js
@@ -6,7 +6,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text, command, usedPrefix }) => {
     if (!text) throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} membaca`
-    let res = await fetch(`https://api.xteam.xyz/brainly?soal=${text}&APIKEY=8872c3ff7452002c`)
+    let res = await fetch(`https://api.xteam.xyz/brainly?soal=${text}&APIKEY=cristian9407`)
     if (!res.ok) throw eror
     let json = await res.json()
     if (!json.status) throw json
