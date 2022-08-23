e3c902944870927a57f3a7cdacc0ececce343b3a
[TioXd] <94074076+BOTCAHX@users.noreply.github.com>
2022-08-23 22:55:21 +0700

    Update main.js

--- main.js
+++ main.js
@@ -124,8 +124,8 @@ global.reloadHandler = function (restatConn) {
     conn.ev.off('creds.update', conn.credsUpdate)
   }
 
-  conn.welcome = 'Hai Selamat Datang\nDi Grup @subject'
-  conn.bye = 'Member Grup Telah Meninggalkan Percakapan Grup.'
+  conn.welcome = 'Hai @user Selamat Datang\nDi Grup @subject'
+  conn.bye = '@user Grup Telah Meninggalkan Percakapan Grup.'
   conn.spromote = '@user sekarang admin!'
   conn.sdemote = '@user sekarang bukan admin!'
   conn.handler = handler.handler.bind(conn)
