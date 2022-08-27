159f751001e848ee0b3a2fd03da93dc79b25265f
[TioXd] <94074076+BOTCAHX@users.noreply.github.com>
2022-08-27 07:13:41 +0700

    Update main-update.js

--- plugins/main-update.js
+++ plugins/main-update.js
@@ -1,7 +1,9 @@
+//nyoba doang
+
 let { execSync } = require('child_process')
 let handler = async (m, { conn, text, isROwner }) => {
   if (global.conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git remote set-url origin https://github.com/Paquito1923/elyasmd.git && git pull' + (isROwner && text ? ' ' + text : ''))
+    let stdout = execSync('git remote set-url origin https://github.com/BOTCAHX/RTXZY-MD.git && git pull' + (isROwner && text ? ' ' + text : ''))
     if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
     m.reply(stdout.toString())
   }
