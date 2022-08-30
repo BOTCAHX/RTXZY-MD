8707f8ecae1f8613fefa02e2e13626b1217e422c
[TioXd] <94074076+BOTCAHX@users.noreply.github.com>
2022-08-30 23:04:12 +0700

    Update Group-setting.js

--- plugins/Group-setting.js
+++ plugins/Group-setting.js
@@ -1,5 +1,19 @@
-let { groupSettingUpdate } = require('@adiwajshing/baileys-md')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+let { groupsSettingUpdate } = require('@adiwajshing/baileys')
+let handler = async (m, { isAdmin, isOwner, isBotAdmin, conn, args, usedPrefix, command }) => {
+	if (!(isAdmin || isOwner)) {
+		global.dfail('admin', m, conn)
+		throw false
+	}
+	if (!isBotAdmin) {
+		global.dfail('botAdmin', m, conn)
+		throw false
+	}
+let prefix = usedPrefix
+let bu = `Group telah di buka oleh @${m.sender.split`@`[0]} dan sekarang  semua member dapat mengirim pesan
+
+ketik *${usedPrefix}group buka*
+Untuk membuka grup!`.trim()            
+    
 	let isClose = {
 		'open': 'not_announcement',
 		'buka': 'not_announcement',
@@ -11,25 +25,36 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 		'0': 'announcement',
 	}[(args[0] || '')]
 	if (isClose === undefined) {
-		await conn.send2But(m.chat, `
-contoh penggunaan:
-*${usedPrefix + command} tutup*
-*${usedPrefix + command} buka*
-	`.trim(), 'setting grup', 'Buka', '.grup 1', 'Tutup', '.grup 0')
+		await conn.sendButton(m.chat, `
+contoh:
+${usedPrefix + command} tutup
+${usedPrefix + command} buka
+	`.trim(), wm, null, [['Buka', '#grup 1'], ['Tutup', '#grup 0']], m)
 		throw false
-	}
+	} else if (isClose === 'announcement') {
 	await conn.groupSettingUpdate(m.chat, isClose)
+	let teks = `Group telah di tutup oleh @${m.sender.split`@`[0]} dan sekarang hanya admin yang dapat mengirim pesan
+
+ketik *${usedPrefix}group buka*
+Untuk membuka grup!`.trim()
+	await conn.sendButton(m.chat, teks, wm, null, [['Buka', '.group buka']], m, { mentions: [m.sender] })
+	} else if (isClose === 'not_announcement') {
+	await conn.groupSettingUpdate(m.chat, isClose)
+	await conn.sendButton(m.chat, bu, wm, null, [['Tutup', '.group tutup']], m, { mentions: [m.sender] })
+	} else if (isClose === undefined) {
+	await conn.sendButton(m.chat, `
+contoh:
+${usedPrefix + command} tutup
+${usedPrefix + command} buka
+	`.trim(), wm, null, [['Buka', '#grup 1'], ['Tutup', '#grup 0']], m)
+	}
 }
-handler.help = ['grup <buka/tutup>']
+
+handler.help = ['grup <open/close>']
 handler.tags = ['group']
-handler.command = /^(grup|group)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
+handler.command = /^(g(ro?up|c?)?)$/i
 handler.group = true
-handler.private = false
-handler.admin = true
 handler.botAdmin = true
-handler.fail = null
-handler.exp = 0
+
 module.exports = handler
+
