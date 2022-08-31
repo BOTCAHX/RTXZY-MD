4ecb84e0c6b60d0256083c2ac2ee52a51d64795d
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-01 04:00:14 +0700

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,17 +1,17 @@
-let { instagramdl, instagramdlv2 } = require('@bochilteam/scraper')
+let { instagramdlv3, instagramdlv4 } = require('@bochilteam/scraper')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
     if (!args[0]) throw `*Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_medium=copy_link`
     if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `*Link salah! Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_medium=copy_link`
-    const results = await instagramdl(args[0]).catch(async _ => await instagramdlv2(args[0]))
-    for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `ðŸ”— *Url:* ${await shortlink(url)}\n*${global.wm}*`, m)
+    const results = await instagramdlv3(args[0]).catch(async _ => await instagramdlv4(args[0]))
+    for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `🔗 *Url:* ${await shortlink(url)}\n*${global.wm}*`, m)
 }
 
 handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^(ig|igdl|instagram)$/i
-handler.limit = false
-handler.group = false
+handler.limit = true
+handler.group = true
 
 module.exports = handler
 
