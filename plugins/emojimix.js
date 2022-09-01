ab31a7b584eff3a3469e2eccf1665c1d9b82c73d
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-02 02:44:29 +0700

    Update emojimix.js

--- plugins/emojimix.js
+++ plugins/emojimix.js
@@ -47,8 +47,8 @@ const { MessageType } = require('@adiwajshing/baileys')
 const { sticker } = require('../lib/sticker')
 
 let handler = async (m, { conn, text, args }) => {
-if (!args[0]) throw 'Contoh penggunaan:\n\n*.emojimix 🤨&😣*'
-   	let [emoji1, emoji2] = text.split`&`
+if (!args[0]) throw 'Contoh penggunaan:\n\n*.emojimix 🤨+😣*'
+   	let [emoji1, emoji2] = text.split`+`
 		let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
 		for (let res of anu.results) {
 			let stiker = await sticker(false, res.url, global.packname, global.author)
