430d8f248c8600fc22a3d3023ba1afd827750c1a
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-23 00:22:32 +0700

    Update pctlove.js

--- plugins/pctlove.js
+++ plugins/pctlove.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
   if (!args[0]) throw 'Masukkan Text\nContoh : .pictlove BOTCAHX'
   m.reply('_Proses..._')
-  let res = `https://botcahx-rest-api.herokuapp.com/api/photooxy/picture-of-love?text=${response[0]}`
+  let res = `https://botcahx-rest-api.up.railway.app/api/photooxy/picture-of-love?text=${response[0]}`
   conn.sendFile(m.chat, res, 'botcahx.jpg', `© BOTCAHX`, m, false)
 }
 handler.help = ['pictlove'].map(v => v + ' <text>')
