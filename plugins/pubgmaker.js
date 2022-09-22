fc6a76402a3ad18101afc39dd35be57108c12d9a
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-23 00:23:10 +0700

    Update pubgmaker.js

--- plugins/pubgmaker.js
+++ plugins/pubgmaker.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
   if (!args[0]) throw 'Masukkan Text\nContoh : .pubg BOTCAHX'
   m.reply('_Proses..._')
-  let res = `https://botcahx-rest-api.herokuapp.com/api/photooxy/pubg?text=Pubg&text2=${response[0]}`
+  let res = `https://botcahx-rest-api.up.railway.app/api/photooxy/pubg?text=Pubg&text2=${response[0]}`
   conn.sendFile(m.chat, res, 'botcahx.jpg', `© BOTCAHX`, m, false)
 }
 handler.help = ['pubg'].map(v => v + ' <text>')
