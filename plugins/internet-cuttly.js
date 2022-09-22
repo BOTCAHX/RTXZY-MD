ca047d9cc7334d1fba182de9fb060f4a6c126571
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-23 00:30:21 +0700

    Update internet-cuttly.js

--- plugins/internet-cuttly.js
+++ plugins/internet-cuttly.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
   if (!text) throw 'Masukan url/link nya mana?\n> .cuttly https://botcahx-rest-api.herokuapp.com'
-  let res = await fetch(`https://botcahx-rest-api.herokuapp.com/api/linkshort/cuttly?link=${text}`)
+  let res = await fetch(`https://botcahx-rest-api.up.railway.app/api/linkshort/cuttly?link=${text}`)
   let json = await res.json()
   if (json.status) m.reply(json.result)
   else throw 'Link Invalid!\nPeriksa url anda'
