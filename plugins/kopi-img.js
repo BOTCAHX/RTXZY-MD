7989afa08e94ab216a4579dfb21bc8ddff45d438
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-23 00:19:46 +0700

    Update kopi-img.js

--- plugins/kopi-img.js
+++ plugins/kopi-img.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, command }) => {
-let nyenye = `https://botcahx-rest-api.herokuapp.com/api/randomgambar/coffee`
+let nyenye = `https://botcahx-rest-api.up.railway.app/api/randomgambar/coffee`
     conn.sendButtonImg(m.chat, nyenye, 'Nih', wm2, 'Next', `.ngopi`, m) 
 }
 handler.help = ['ngopi']
