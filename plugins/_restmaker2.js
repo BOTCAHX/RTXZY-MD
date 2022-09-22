a002f0519cd983efb83584fb6cf138c74a8d6ef3
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-09-23 00:17:25 +0700

    Update _restmaker2.js

--- plugins/_restmaker2.js
+++ plugins/_restmaker2.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
   if (!args[0]) throw 'Masukkan Text\nContoh : .flaming4 BOTCAHX'
   m.reply('_Proses..._')
-  let res = `https://botcahx-rest-api.herokuapp.com/api/photooxy/shadow-sky?text=${response[0]}`
+  let res = `https://botcahx-rest-api.up.railway.app/api/photooxy/shadow-sky?text=${response[0]}`
   conn.sendFile(m.chat, res, 'botcahx.jpg', `© BOTCAHX`, m, false)
 }
 handler.help = ['flaming8'].map(v => v + ' <text>')
