f139e8c585e1513c157a80d34c7d07b8040c1aa3
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-10-01 05:00:09 +0700

    eror njir

--- plugins/info-tqto.js
+++ plugins/info-tqto.js
@@ -4,13 +4,4 @@
 * jangan dihilangin, boleh di tambahin 🐦
 **/
 
-let handler = async (m, { conn }) => {
-bear = "Source Code"
-let esce = 'Big Thanks to\n\n Allah swt\nMy friends\nDatabase Replit.'
-conn.sendButtonImg(m.chat, fla + 'Thanks To', esce, wm2, 'Makasih', 'thanks', m) 
-}
-handler.help = ['tqto', 'team']
-handler.tags = ['info']
-handler.command = /^(tqto|team)$/i
-
-module.exports = handler
+const _0xbaf57d=_0x36c1;(function(_0x4040fc,_0x3eb5e6){const _0x2bc561=_0x36c1,_0x622e74=_0x4040fc();while(!![]){try{const _0x174e65=parseInt(_0x2bc561(0xcd))/0x1+parseInt(_0x2bc561(0xca))/0x2+parseInt(_0x2bc561(0xc7))/0x3+-parseInt(_0x2bc561(0xcb))/0x4+parseInt(_0x2bc561(0xc8))/0x5*(parseInt(_0x2bc561(0xd1))/0x6)+parseInt(_0x2bc561(0xd4))/0x7*(-parseInt(_0x2bc561(0xc9))/0x8)+-parseInt(_0x2bc561(0xd0))/0x9;if(_0x174e65===_0x3eb5e6)break;else _0x622e74['push'](_0x622e74['shift']());}catch(_0x3f99b6){_0x622e74['push'](_0x622e74['shift']());}}}(_0x2450,0x2bdc3));let handler=async(_0x272391,{conn:_0x33193f})=>{const _0x19e2c3=_0x36c1;bear=_0x19e2c3(0xd3);let _0x129c0f=_0x19e2c3(0xc3);_0x33193f[_0x19e2c3(0xcf)](_0x272391['chat'],fla+_0x19e2c3(0xc5),_0x129c0f,wm2,_0x19e2c3(0xc6),_0x19e2c3(0xcc),_0x272391);};function _0x2450(){const _0x4b279d=['tags','command','Big\x20Thanks\x20to\x0a\x0a>\x20Allah\x20swt\x0a>\x20My\x20Team\x0a>\x20Nurutomo\x0a>\x20Penyedia\x20RestApi\x0a>\x20AlyaXd\x0a>\x20Verdihatorou\x0a>\x20Radhin\x0a>\x20TioXd\x0a>\x20Xteam\x0a>\x20Lolhuman\x0a>\x20All\x20Owner','team','Thanks\x20To','Makasih','1015089oPaCkd','711745oICuNw','8GfoDiG','529964vshpEI','755164SknEGE','thanks','211719mFnKAf','info','sendButtonImg','4995594XJIxkn','6MbhgsK','exports','Source\x20Code','237335znzobx'];_0x2450=function(){return _0x4b279d;};return _0x2450();}function _0x36c1(_0x15346e,_0x4bafa4){const _0x24504b=_0x2450();return _0x36c1=function(_0x36c140,_0x19f6a3){_0x36c140=_0x36c140-0xc3;let _0x41c39e=_0x24504b[_0x36c140];return _0x41c39e;},_0x36c1(_0x15346e,_0x4bafa4);}handler['help']=['tqto',_0xbaf57d(0xc4)],handler[_0xbaf57d(0xd5)]=[_0xbaf57d(0xce)],handler[_0xbaf57d(0xd6)]=/^(tqto|team)$/i,module[_0xbaf57d(0xd2)]=handler;
