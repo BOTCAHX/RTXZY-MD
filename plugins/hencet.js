74066de20c7962b941485a11f9ca23ad438a2891
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-10-05 10:11:40 +0700

    commit

--- plugins/hencet.js
+++ plugins/hencet.js
@@ -5,7 +5,7 @@ let tio = 'Berem Berem Hencet Berem'
 type: 'audioMessage', 
 ptt: true, contextInfo: {
         externalAdReply: { showAdAttribution: true, title: tio,
- body: wm, sourceUrl: 'https://botcahx-rest-api.up.railway.app', thumbnail: await (await fetch('https://telegra.ph/file/168c4b9765f88c8ab9c8c.png')).buffer()}} 
+ body: wm, sourceUrl: 'https://botcahx.ddns.net', thumbnail: await (await fetch('https://telegra.ph/file/168c4b9765f88c8ab9c8c.png')).buffer()}} 
      })
 
 }
