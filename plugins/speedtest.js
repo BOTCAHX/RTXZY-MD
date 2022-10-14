f0be70e69b81958b4b5cc8273e3fbb3e755c3467
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-10-15 00:22:31 +0700

    update python3

--- plugins/speedtest.js
+++ plugins/speedtest.js
@@ -1,22 +1,21 @@
-let cp = require('child_process')
-let { promisify } = require('util')
+let cp = require ('child_process')
+let { promisify } = require ('util')
 let exec = promisify(cp.exec).bind(cp)
-let handler = async (m) => {
+let handler = async (m, { conn}) => {
+	await conn.reply(m.chat, `Please Wait`, m)
     let o
     try {
-        o = await exec('python speed.py')
+        o = await exec('python3 speed.py --share --secure')
     } catch (e) {
         o = e
     } finally {
         let { stdout, stderr } = o
-        if (stdout.trim()) m.reply(stdout)
+        if (stdout.trim()) conn.sendButton(m.chat, `SPEEDTEST.NET`, stdout, null, [["MENU", ".menu"],["PING", ".ping"]], m)
         if (stderr.trim()) m.reply(stderr)
     }
 }
 handler.help = ['speedtest']
 handler.tags = ['info']
-handler.command = /^(speedtest)$/i
-
-handler.owner = true
+handler.command = /^(speedtest|ookla)$/i
 
 module.exports = handler
