9b36202c9fd429c094f50102d126855e322390a4
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-10-18 07:19:21 +0700

    base update

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -1,44 +1,29 @@
-const { servers, yta, ytv } = require('../lib/y2mate')
-let yts = require('yt-search')
-let fetch = require('node-fetch')
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} dj tiktok`
-  m.reply(wait)
-  let chat = global.db.data.chats[m.chat]
-  let results = await yts(text)
-  let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Konten Tidak ditemukan'
-  let isVideo = /2$/.test(command)
-  let yt = false
-  let yt2 = false
-  let usedServer = servers[0]
-  for (let i in servers) {
-    let server = servers[i]
-    try {
-      yt = await yta(vid.url, server)
-      yt2 = await ytv(vid.url, server)
-      usedServer = server
-      break
-    } catch (e) {
-      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
-    }
-  }
-  if (yt === false) throw 'semua server gagal'
-  if (yt2 === false) throw 'semua server gagal'
-  let { dl_link, thumb, title, filesize, filesizeF } = yt
-  await conn.send2ButtonImg(m.chat, await (await fetch(thumb)).buffer(), `
-*Judul:* ${title}
-*Ukuran File Audio:* ${filesizeF}
-*Ukuran File Video:* ${yt2.filesizeF}
-*Server y2mate:* ${usedServer}
-*Link Sumber:* 
-${vid.url}
-`.trim(), wm, 'Audio', `.yta ${vid.url}`, 'Video', `.yt ${vid.url}`, m)
-}
-handler.help = ['play'].map(v => v + ' <pencarian>')
-handler.tags = ['downloader']
-handler.command = /^(p|play)$/i
+let fetch = require ('node-fetch')
+let { youtubeSearch } = require ('@bochilteam/scraper')
 
+let handler = async (m, { conn, text, usedPrefix }) => {
+  if (!text) throw 'Input Query'
+  let vid = (await youtubeSearch(text)).video[0]
+  if (!vid) throw 'Video/Audio Tidak Ditemukan'
+  let { title, description, thumbnail, videoId, durationH, durationS, viewH, publishedTime } = vid
+  let url = 'https://www.youtube.com/watch?v=' + videoId
+  let ytLink = `https://botcahx2.ddns.net/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
+  let capt = `*Title:* ${title}\n*Published:* ${publishedTime}\n*Duration:* ${durationH}\n*Views:* ${viewH}\n*Description:* ${description}\n*Url:* ${url}`
+  let buttons = [{ buttonText: { displayText: 'Video' }, buttonId: `${usedPrefix}ytv ${url}` }]
+  let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: capt, footer: '_Audio on progress..._', buttons }, { quoted: m })
+  //if (durationS > 4000) return conn.sendMessage(m.chat, { text: `*Download:* ${await shortUrl(ytLink)}\n\n_Duration too long..._` }, { quoted: msg })
+  conn.sendMessage(m.chat, { audio: { url: ytLink }, mimetype: 'audio/mpeg' }, { quoted: msg })
+}
+handler.help = ['play']
+handler.tags = ['downloader']
+handler.command = /^(play)$/i
 handler.exp = 0
 
 module.exports = handler
+
+async function shortUrl(url) {
+  url = encodeURIComponent(url)
+  let res = await fetch(`https://is.gd/create.php?format=simple&url=${url}`)
+  if (!res.ok) throw false
+  return await res.text()
+}
