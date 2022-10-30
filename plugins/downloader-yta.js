1a382dc39496fb827cf398c20e97f934c4f7153c
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-10-30 17:45:50 +0700

    args:salah;v

--- plugins/downloader-yta.js
+++ plugins/downloader-yta.js
@@ -1,16 +1,15 @@
 let fetch = require ('node-fetch')
 let { youtubeSearch } = require ('@bochilteam/scraper')
-let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args[0]) throw 'Url nya mana?'
+let handler = async (m, { conn, text }) => {
+  if (!text) throw 'Url nya mana?'
   m.reply('_Proses..._')
-let text = '${args[0]}'
   let vid = (await youtubeSearch(text)).video[0]
   let { title, description, thumbnail, videoId, durationH, durationS, viewH, publishedTime } = vid
   let url = 'https://www.youtube.com/watch?v=' + videoId
 let ytLink = `https://botcahx2.ddns.net/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
   conn.sendMessage(m.chat, { audio: { url: ytLink }, mimetype: 'audio/mpeg' }, { quoted: m })
 }
-handler.help = ['yta'].map(v => v + ' <url>')
+handler.help = ['ytv'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^(yta|ytaudio)$/i
 
