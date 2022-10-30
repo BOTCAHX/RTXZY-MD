c1a78d774450d034ac5ff9e2109d17196731a17e
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-10-31 03:45:11 +0700

    🥕

--- plugins/_ppcp2.js
+++ plugins/_ppcp2.js
@@ -4,13 +4,13 @@ let handler = async (m, { conn, command }) => {
   if (res.status != 200) throw await res.text()
   let json = await res.json()
   if (!json.status) throw json
-conn.sendButtonImg(m.chat, json.result.female,  'PP Cewenya', wm, 'Next', '.ppcp2', m)
+conn.sendButtonImg(m.chat, json.result.female,  'PP Cewenya', wm, 'Next', '.ppcp', m)
 conn.sendFile(m.chat, json.result.male,'pria.jpg',  'PP Cowonya', m)
 
 }
-handler.help = ['ppcp2']
+handler.help = ['ppcp']
 handler.tags = ['internet']
-handler.command = /^ppcp2$/i
+handler.command = /^ppcp$/i
 
 
 module.exports = handler
