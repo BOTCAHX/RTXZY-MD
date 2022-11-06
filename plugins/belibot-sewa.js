af00713c6831edf4da73f872a6bac2a1e78c4e26
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-11-06 12:00:41 +0700

    to doc

--- plugins/belibot-sewa.js
+++ plugins/belibot-sewa.js
@@ -1,29 +1,34 @@
-let fs = require('fs')
-let handler = async (m, { conn }) => {
-let anu = `
-╭─「 Donasi 」
-│• [ 081395861695 ] - GOPAY
- | • [ 081395861695 ] - TSEL
- | • [ 081395861695 ] - OVO
- | • [ QRISS ] - pm me
-╰────
-*_note_*: jangan pencet button tar fc.
-`
-await conn.relayMessage(m.chat,  {
-    requestPaymentMessage: {
-      currencyCodeIso4217: 'USD',
-      amount1000: 9999,
-      requestFrom: m.sender,
-      noteMessage: {
-      extendedTextMessage: {
-      text: anu,
-      contextInfo: {
-      externalAdReply: {
-      showAdAttribution: true
-      }}}}}}, {})
-}// Tambah sendiri kalo mau
+let fetch = require('node-fetch') 
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+let gh = 'https://github.com/BOTCAHX'
+  let buttonMessage= {
+'document':{'url':gh},
+'mimetype':'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+'fileName':'FILE BOKEP SEMUA BTW',
+'fileLength':'99999999999999',
+'pageCount':'999',
+'contextInfo':{
+'externalAdReply':{
+'showAdAttribution': true, 
+'mediaUrl':gh,
+'mediaType':2,
+'previewType':'pdf',
+'title':'Follow Github Gua',
+'body':'© BOTCAHX',
+'thumbnail':await(await fetch('https://telegra.ph/file/f4063ab3071f5c4af765a.png')).buffer(),
+'sourceUrl':gh}},
+'caption':'Hi, Silahkan pilih opsi di bawah ini.\n*Payment*\n\n*x* Dana: 082221792667\n*x* Pulsa: 082221792667',
+'footer':'©TioXd',
+'buttons':[
+{'buttonId':'.menu','buttonText':{'displayText':'ᴍᴇɴᴜ'},'type':1},
+{'buttonId':'.donasi','buttonText':{'displayText':'ᴅᴏɴᴀsɪ'},'type':1}
+],
+'headerType':6}
+    await conn.sendMessage(m.chat,buttonMessage, { quoted:m})
+}
+
 handler.help = ['sewa']
-handler.tags = ['info']
-handler.command = /^(sewa|belibot|sewabot)$/i
+handler.tags = ['main']
+handler.command = /^(sewa|sewabot|belibot)$/i
 
 module.exports = handler
