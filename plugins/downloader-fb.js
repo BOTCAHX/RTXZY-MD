ce05b551216d0f76b0c7df6306e0cf9064659dc6
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-11-09 18:45:56 +0700

    update fb

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,10 +1,10 @@
 const { facebookdl, facebookdlv2 } = require('@bochilteam/scraper')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example / Gunakan ${usedPrefix}${command} https://www.facebook.com/juankcortavarriaoficial/videos/218237676749570/`
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://fb.watch/azFEBmFRcy/`
     const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
-    for (const { url, isVideo } of result.reverse()) conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `© TioXd`, m)
+    for (const { url, isVideo } of result.reverse()) conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `🔗 *Url:* ${url}`, m)
 }
-handler.help = ['facebook'].map(v => v + ' <url>')
+handler.help = ['facebbok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
 handler.command = /^((facebook|fb)(downloder|dl)?)$/i
