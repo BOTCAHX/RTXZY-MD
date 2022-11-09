e0fb4e271073792f1a0d989278a00190c93cdb6f
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-11-09 20:22:37 +0700

    note

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,10 +1,13 @@
+/**Coba aja dlu gua bingung mau pake scraper suka error makanya 
+Coba pake apikey malesin dlu wkwk**/
+
 let https = require('axios')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://vm.tiktok.com/ZGJAmhSrp/`
 let tio = (await https.get(API('males', '/tiktok', { url: args[0] } ))).data;
 if (tio.status != 200) throw tio.message;
 if (!tio) throw tio.message;
- let hasilnya = `*Title:*${tio.title}\n\n*Author:*${tio.author}`
+ let hasilnya = `*Title:* ${tio.title}\n\n*Author:* ${tio.author}`
   conn.sendButtonVid(m.chat, tio.video, hasilnya, wm, `Back`, `.menu`, m)
         }
 handler.help = ['tiktok'].map(v => v + ' <url>')
