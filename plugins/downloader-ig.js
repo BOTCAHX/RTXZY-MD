fcc465b870d6e6c3092b6f84c2711f03c4b4c7ac
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-11-09 18:52:27 +0700

    change scraper

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,21 +1,14 @@
-let { instagramdl, instagramdlv2 } = require('@bochilteam/scraper')
+let instagramGetUrl = require('instagram-url-direct')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
+    const results = (await instagramGetUrl(args[0])).url_list[0]
 
-    if (!args[0]) throw `*Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_medium=copy_link`
-    if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `*Link salah! Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_medium=copy_link`
-    const results = await instagramdl(args[0]).catch(async _ => await instagramdlv2(args[0]))
-    for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `*${global.wm}*`, m)
+    conn.sendFile(m.chat, results, 'ig.mp4', `*Instagram Downloader*`, m)
 }
-
 handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(ig|igdl|instagram)$/i
-handler.limit = false
-handler.group = false
+
+handler.command = /^(ig(dl)?)$/i
+handler.limit = true
 
 module.exports = handler
-
-async function shortlink(url) {
-isurl = /https?:\/\//.test(url)
-return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''
-}
