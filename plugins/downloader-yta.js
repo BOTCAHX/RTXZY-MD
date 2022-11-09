661423cab10d47f1d1f3324d5740f61c7e519bf7
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-11-09 20:20:32 +0700

    import kebanyakan

--- plugins/downloader-yta.js
+++ plugins/downloader-yta.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
   if (!text) throw 'Url nya mana?'
   m.reply('_Proses..._')
   let vid = (await youtubeSearch(text)).video[0]
-  let { title, description, thumbnail, videoId, durationH, durationS, viewH, publishedTime } = vid
+  let { videoId } = vid
   let url = 'https://www.youtube.com/watch?v=' + videoId
 let ytLink = `https://botcahx2.ddns.net/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
   conn.sendMessage(m.chat, { audio: { url: ytLink }, mimetype: 'audio/mpeg' }, { quoted: m })
