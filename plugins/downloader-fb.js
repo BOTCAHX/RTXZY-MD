90fc2384665caf7b00e756cc568e9e38c438326a
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-11-10 13:32:29 +0700

    ah sudahlah

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,12 +1,12 @@
-const { facebookdl, facebookdlv2 } = require('@bochilteam/scraper')
+const { facebookdl, facebookdlv2, facebookdlv3 } = require('@bochilteam/scraper')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://fb.watch/azFEBmFRcy/`
-    const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
+    const { result } = await facebookdlv3(args[0]).catch(async _ => await facebookdlv3(args[0]))
     for (const { url, isVideo } of result.reverse()) conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `🔗 *Url:* ${url}`, m)
 }
 handler.help = ['facebbok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-
+handler.limit = 10
 handler.command = /^((facebook|fb)(downloder|dl)?)$/i
 
 module.exports = handler
