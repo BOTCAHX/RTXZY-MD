99cd8b8397adfb49005f508c9d7b20fd04127fe3
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-11-15 14:30:17 +0700

    Update quotes-quotes.js

--- plugins/quotes-quotes.js
+++ plugins/quotes-quotes.js
@@ -1,4 +1,4 @@
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
 let fs = require('fs')
 let handler = async (m) => {
     let who
