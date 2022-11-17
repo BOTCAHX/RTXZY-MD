0ba5f8fdc13a0e56f63425f6046fdef18019943d
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-11-17 18:47:37 +0700

    Makin kesini makin kesana

--- plugins/stalker-githubstalk.js
+++ plugins/stalker-githubstalk.js
@@ -1,27 +1,27 @@
 let fetch = require('node-fetch')
 
 let handler = async(m, { conn, text }) => {
-
   if (!text) return conn.reply(m.chat, 'Harap Masukan Username', m)
 
   await m.reply('Searching...')
-    let res = await fetch(`https://hadi-api.herokuapp.com/api/githubstalk?username=${text}`)
-    let json = await res.json()
+    var res = await fetch('https://botcahx.ddns.net/api/info/githubstalk?user=${text}')
+    var json = await res.json()
     if (res.status !== 200) throw await res.text()
     if (!json.status) throw json
-    let thumb = await (await fetch(json.result.avatar)).buffer()
-    let hasil = `*── 「 GITHUB STALK 」 ──*
+    var thumb = await (await fetch(json.result.avatar_url)).buffer()
+    var hasil = `*── 「 GITHUB STALK 」 ──*
 
 ➸ *Bio*: ${json.result.bio}
 ➸ *Perusahaan*: ${json.result.company}
 ➸ *Email:* ${json.result.email}
 ➸ *Twitter:* ${json.result.twiter_username}
-➸ *Repo Publik:* ${json.result.public_repo}
+➸ *Repo Publik:* ${json.result.public_repos}
 ➸ *Gists Publik:* ${json.result.public_gists}
-➸ *Follower:* ${json.result.follower}
+➸ *Follower:* ${json.result.followers}
 ➸ *Following:* ${json.result.following}
 ➸ *Lokasi:* ${json.result.location}
-➸ *Type:* ${json.result.Type}
+➸ *Type:* ${json.result.type}
+➸ *Url:* ${json.result.url}
 `
 
     conn.sendFile(m.chat, thumb, 'githubstalk.jpg', hasil, m)
