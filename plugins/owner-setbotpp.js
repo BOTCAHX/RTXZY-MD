00907898c13126d12d50e7978ab73311fa023706
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-11-19 19:59:00 +0700

    Pp full buat bot v:

--- plugins/owner-setbotpp.js
+++ plugins/owner-setbotpp.js
@@ -1,22 +1,39 @@
-/*
-   Silahkan Di Pakek
-   Tapi Bantu Rapihin :v
-   Buatan: Miaweers
-*/
-
-let handler = async (m, { conn, args }) => {
-    let bot = conn.user.jid // Bot
-    let q = m.quoted ? m.quoted : m
-    let mime = (q.msg || q).mimetype || ''
-    if (/image/.test(mime)) {
-      let img = await q.download()
-      if (!img) throw `Foto tidak ditemukan`
-     conn.updateProfilePicture (bot, img)
-    conn.reply(m.chat, 'Sukses Mengganti Foto Profile Bot!', m)
-	}
-    }
+const fs = require('fs')
+const Jimp = require('jimp') 
+let handler = async (m, { conn }) => {
+var image = m.quoted ? m.quoted : m
+var mime = (image.msg || image).mimetype || ''
+var media = await image.download()
+            const bot = await conn.user.jid // Bot
+            var { img } = await generateProfilePicture(media)
+            await conn.query({
+            tag: 'iq',
+            attrs: {
+            to: bot, 
+            type:'set',
+            xmlns: 'w:profile:picture'
+            },
+            content: [
+            {
+            tag: 'picture',
+            attrs: { type: 'image' },
+            content: img
+            }
+            ]
+            })
+            m.reply(`Succes Update Bot Profile ✅`)
+}
 handler.help = ['setbotpp']
 handler.command = /^(setbotpp)$/i
 handler.owner = true
 
 module.exports = handler
+
+async function generateProfilePicture(buffer) {
+	const jimp_1 = await Jimp.read(buffer);
+	const minz = jimp_1.getWidth() > jimp_1.getHeight() ? jimp_1.resize(720, Jimp.AUTO) : jimp_1.resize(Jimp.AUTO, 720)
+	const jimp_2 = await Jimp.read(await minz.getBufferAsync(Jimp.MIME_JPEG));
+	return {
+	  img: await minz.getBufferAsync(Jimp.MIME_JPEG)
+	}
+}
