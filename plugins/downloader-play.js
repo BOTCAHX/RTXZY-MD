9aea99d58c3114b3208ff6b550844129f23302d9
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-11-19 22:05:33 +0700

    shortlink

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -8,22 +8,22 @@ let handler = async (m, { conn, text, usedPrefix }) => {
   let { title, description, thumbnail, videoId, durationH, durationS, viewH, publishedTime } = vid
   let url = 'https://www.youtube.com/watch?v=' + videoId
   let ytLink = `https://botcahx2.ddns.net/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
-  let capt = `*Title:* ${title}\n*Published:* ${publishedTime}\n*Duration:* ${durationH}\n*Views:* ${viewH}\n*Description:* ${description}\n*Url:* ${url}`
+  let capt = `⭔ Title: ${title}\n\n⭔ Published: ${publishedTime}\n⭔ Duration: ${durationH}\n⭔ Views: ${viewH}\n⭔ Description: ${description}\n⭔ Url:  ${url}`
   let buttons = [{ buttonText: { displayText: 'Video' }, buttonId: `${usedPrefix}ytv ${url}` }]
   let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: capt, footer: '_Audio on progress..._', buttons }, { quoted: m })
-  //if (durationS > 4000) return conn.sendMessage(m.chat, { text: `*Download:* ${await shortUrl(ytLink)}\n\n_Duration too long..._` }, { quoted: msg })
+ if (durationS > 4000) return conn.sendMessage(m.chat, { text: `*Download:* ${await shortUrl(ytLink)}\n\n_Durasi terlalu panjang download manual saja lewat link berikut..._` }, { quoted: msg })
   conn.sendMessage(m.chat, { audio: { url: ytLink }, mimetype: 'audio/mpeg' }, { quoted: msg })
 }
 handler.help = ['play']
 handler.tags = ['downloader']
-handler.command = /^(play)$/i
+handler.command = /^(play|song)$/i
 handler.exp = 0
 
 module.exports = handler
 
 async function shortUrl(url) {
   url = encodeURIComponent(url)
-  let res = await fetch(`https://is.gd/create.php?format=simple&url=${url}`)
+  let res = await fetch(`https://botcahx.ddns.net/api/linkshort/cuttly?link=${url}`)
   if (!res.ok) throw false
   return await res.text()
 }
