7f147f2635bd065467352d30c60e2796acd0094d
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-11-21 06:57:40 +0700

    error blog

--- config.js
+++ config.js
@@ -15,25 +15,25 @@ global.mods = ['6281395861695']   // isi jika perlu
 global.prems = ['6281395861695']  // isi jika perlu
 
 //Nomor Owner & payment//
-global.nameowner = 'nama owner' // isi nama kalian bebas
-global.numberowner = 'nama owner' // isi ini untuk mengendalikan bot, untuk mengubah tampilan owner ada di plugins 
-global.dana = '' // isi dengan nomor dana yg kamu punya 
-global.pulsa = '' // isi dengan nomor pulsa hp kalian
-global.gopay = ''
+global.nameowner = 'TioXd' // isi nama kalian bebas
+global.numberowner = '6281395861695' // isi ini untuk mengendalikan bot, untuk mengubah tampilan owner ada di plugins 
+global.dana = '6281395861695' // isi dengan nomor dana yg kamu punya 
+global.pulsa = '6281395861695' // isi dengan nomor pulsa hp kalian
+global.gopay = '6281395861695'
 
 //Nama Bot & Grouplink//
-global.namebot = 'nama bot lu'
+global.namebot = 'RTXZY-MD'
 global.gc = 'https://chat.whatsapp.com/D6BZVRSymWaFU28t2lxaO0'
-global.web = 'https://github.com/verdihatoru' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
-global.instagram = 'https://instagram.com/verdi_zz'
+global.web = 'https://github.com/BOTCAHX' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.instagram = 'https://instagram.com/prm2.0'
 
 //Tampilan wm Bot dan pesan bot//
 global.lolkey = 'rey2k22' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = '© xziro'
+global.wm = '© R-TXZY-MD'
 global.watermark = wm
-global.wm2 = '⫹⫺ wm'
-global.wm3 = '© wm'
+global.wm2 = '⫹⫺ R-TXZY-MD'
+global.wm3 = '© R-TXZY-MD'
 global.wm4 = '© 2021/2022'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='
 global.wait = '_*Tunggu sedang di proses...*_'
@@ -42,7 +42,7 @@ global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
 global.packname = 'Made with'
-global.author = 'Bot WhatsApp\wm'
+global.author = 'Bot WhatsApp\nR-TXZY-MD'
 
 //Apikey Setting//
 global.APIs = { // API Prefix
