3e3c1f8e939e53edc0bf3229b6f93213552e5f11
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-11-23 09:04:15 +0700

    upd

--- plugins/maker-trigger.js
+++ plugins/maker-trigger.js
@@ -1,19 +1,34 @@
+const uploadImage = require('../lib/uploadImage')
+const uploadFile = require('../lib/uploadFile')
 const { sticker } = require('../lib/sticker')
 const { MessageType } = require('@adiwajshing/baileys')
-let handler = async (m, { conn }) => {
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  let marah = global.API('https://some-random-api.ml', '/canvas/triggered', {
-    avatar: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  })
- let stiker = await sticker(null, marah, global.packname, global.author)
- if (stiker) return conn.sendFile(m.chat, stiker, m, false, { asSticker: true });
-  else throw stiker.toString()
+let fs = require('fs')
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || q.mediaType || ''
+    if (/image/g.test(mime) && !/webp/g.test(mime)) {
+    	let ztick = fs.readFileSync(`./media/sticker/emror.webp`)
+    	try {
+			let img = await q.download?.()
+			let out = await uploadImage(img)
+			const res = `https://botcahx.ddns.net/api/maker/trigger?url=${out}`
+			const stiker = await sticker(false, res, global.packname, global.author)
+	        await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+    	} catch (e) {
+    		console.log(e)
+	        await conn.sendFile(m.chat, ztick, 'stick.webp', '', m)
+    	}
+    } else {
+    	m.reply(`Kirim gambar dengan caption\n*${usedPrefix + command}* atau tag gambar yang sudah dikirim`)
+    }
 }
 
-
 handler.help = ['trigger']
-handler.tags = ['maker']
+handler.tags = ['sticker']
+handler.command = /^(trigger(ed)?)$/i
 
-handler.command = /^(trigger)$/i
+handler.premium = false
+handler.limit = true
 
 module.exports = handler
