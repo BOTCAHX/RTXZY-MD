cc19d198021f8ce00b1fd16e2e0dc871cd0f6f6c
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-11-25 02:08:37 +0700

    dahlah:v

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,41 +1,16 @@
-/**ubah no lu biar ada owner nya
-Isi semua yg perlu di isi tapi jangan ngasal
-error fixs sendiri bg
-By TioXd**/
+let fs = require('fs')
+let handler = async (m, { conn }) => {
+let teks = 'Creator'
+let numberowner = global.numberowner
+let anu = `
+Contact Owner:
+wa.me/${numberowner}
 
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
-let handler = async (m, {conn}) => {
-const vcard = `BEGIN:VCARD
-VERSION:3.0
-N:Sy;Bot;;;
-FN: Tio 
-item.ORG: Creator Bot
-item1.TEL;waid=6281395861695:6281395861695@s.whatsapp.net
-item1.X-ABLabel:Nomor Creator Bot / Owner Bot
-item2.EMAIL;type=INTERNET:Kalo Chat Gak Penting Gak Akan Di Balas.
-item2.X-ABLabel:Email
-item3.ADR:;;🇮🇩INDONESIA;;;;
-item3.X-ABADR:ac
-item3.X-ABLabel:📍Lokasi
-item4.EMAIL;type=INTERNET:Menerima kritik & saran user kok :)
-item4.X-ABLabel:Contact 👤
-item5.URL:${instagram}
-item5.X-ABLabel:Website
-END:VCARD`
-const sentMsg  = await conn.sendMessage(
-    m.chat,
-    { 
-        contacts: { 
-            displayName: 'OWNER GW', 
-            contacts: [{ vcard }] 
-        }
-    }
-)
-conn.sendMessage(m.chat, { text: `Hay kak *@${await m.sender.split('@')[0]}*, itu ownerku, jangan spam yah >_<`, mentions: [m.sender] }, { quoted: sentMsg })
+_Itu adalah nomor owner/creator bot_`
+  conn.sendButtonImg(m.chat, fla + teks, anu, instagram, 'Thanks', 'thanks', m) 
 }
-handler.help = ['owner', 'creator']
+handler.help = ['main']
 handler.tags = ['info']
-
 handler.command = /^(owner|creator)$/i
 
 module.exports = handler
