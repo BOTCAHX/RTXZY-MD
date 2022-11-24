f8c9af6ffce01ddbe3b5fba4b27183d353f3f3f5
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-11-25 01:53:21 +0700

    :

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -8,9 +8,9 @@ let handler = async (m, {conn}) => {
 const vcard = `BEGIN:VCARD
 VERSION:3.0
 N:Sy;Bot;;;
-FN: TioXd
+FN: BOTCAHX 
 item.ORG: Creator Bot
-item1.TEL;waid=6282221792667:6282221792667@s.whatsapp.net
+item1.TEL;waid=6281395861695:6281395861695@s.whatsapp.net
 item1.X-ABLabel:Nomor Creator Bot / Owner Bot
 item2.EMAIL;type=INTERNET:Kalo Chat Gak Penting Gak Akan Di Balas.
 item2.X-ABLabel:Email
