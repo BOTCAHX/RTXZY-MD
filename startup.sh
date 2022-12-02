c5fcd9fb4e53bbe212ac1afd3fdf8b3256f0b887
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-12-02 08:37:51 +0700

    vps installer

---
+++ startup.sh
@@ -0,0 +1,10 @@
+apt update && pkg upgrade
+apt install git -y
+apt install nodejs -y
+apt install ffmpeg -y
+apt install imagemagick -y
+apt install npm
+git clone https://github.com/BOTCAHX/RTXZY-MD
+cd RTXZY-MD
+npm install
+node .
