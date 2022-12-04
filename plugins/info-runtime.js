3eb7c191885982bc1d69aa39f6138ed8c75e2c52
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-12-04 15:28:00 +0700

    day,hour,minute,second

--- plugins/info-runtime.js
+++ plugins/info-runtime.js
@@ -1,12 +1,12 @@
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
     let _uptime = process.uptime() * 1000
-    let uptime = clockString(_uptime)
+    let uptimex = clockString(_uptime)
     let time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
 
 m.reply(`
 *───「 RUNTIME BOT 」───*
 
-Runtime : ${uptime} 
+Runtime : ${uptimex} 
 Waktu : ${time}
 
     `)
@@ -18,8 +18,12 @@ handler.command = /^(uptime|runtime)$/i
 module.exports = handler
 
 function clockString(ms) {
-    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+    let days = Math.floor(ms / (24 * 60 * 60 * 1000));
+    let daysms = ms % (24 * 60 * 60 * 1000);
+    let hours = Math.floor((daysms) / (60 * 60 * 1000));
+    let hoursms = ms % (60 * 60 * 1000);
+    let minutes = Math.floor((hoursms) / (60 * 1000));
+    let minutesms = ms % (60 * 1000);
+    let sec = Math.floor((minutesms) / (1000));
+    return days + " Hari " + hours + " Jam " + minutes + " Menit " + sec + " Detik";
 }
