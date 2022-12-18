a9531af3820363c472e8ffa25eb8708b7ab80fa1
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-12-18 19:32:23 +0700

    ngentod

--- plugins/sticker-gay.js
+++ plugins/sticker-gay.js
@@ -12,7 +12,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     	try {
 			let img = await q.download?.()
 			let out = await uploadImage(img)
-			const res = `https://botcahx.ddns.net/api/maker/rainbow?url=${out}`
+			const res = `https://api.tiodevhost.my.id/api/maker/rainbow?url=${out}`
 			const stiker = await sticker(false, res, global.packname, global.author)
 	        await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
     	} catch (e) {
