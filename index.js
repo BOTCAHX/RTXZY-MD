67eb18ed9352b65bc409cab6e24abf45a9d5aab9
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-12-18 15:49:26 +0700

    .

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("Menyabungkan Sessions...");
+console.log("Connecting sessions...");
 let cluster = require("cluster");
 let { spawn } = require("child_process");
 let path = require("path");
@@ -33,10 +33,10 @@ function start(file) {
 		if (code === 0) return;
 		fs.watchFile(args[0], () => {
 			fs.unwatchFile(args[0]);
-			start("main.js");
+			start(file);
 		});
 	});
 	
 }
 
-start("main.js");
+start('main.js');
