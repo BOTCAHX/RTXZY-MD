04eab82b4b59984e4092fdd674a8f975834d4ef7
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-12-18 19:13:48 +0700

    ngentod

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text }) => {
   let vid = (await youtubeSearch(text)).video[0]
   let { videoId } = vid
   let url = 'https://www.youtube.com/watch?v=' + videoId
-let ytLink = `https://botcahx2.ddns.net/?url=${url}&filter=audioandvideo&quality=highestvideo&contenttype=video/mp4`
+let ytLink = `https://ytdl.tiodevhost.my.id/?url=${url}&filter=audioandvideo&quality=highestvideo&contenttype=video/mp4`
   conn.sendMessage(m.chat, { video: { url: ytLink }, mimetype: 'video/mp4' }, { quoted: m })
 }
 handler.help = ['ytv'].map(v => v + ' <url>')
