eb5c6201884068ac448049388e25d5861d4438a3
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-12-20 06:34:36 +0700

    kontolodon

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -1,14 +1,10 @@
 let handler = async(m, { conn, text, args, usedPrefix, command }) => {
-if (!text) throw `Contoh\n${usedPrefix + command} Bot`
+if (!text) throw `*CONTOH*\n${usedPrefix + command} Bot`
 let teks = encodeURI(text)
-
 if (command == 'attp') {
 conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/attp?apikey=85faf717d0545d14074659ad&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
-
 }
-handler.help = ['attp <teks>']
+handler.command = handler.help = ['attp']
 handler.tags = ['sticker']
-
-handler.command = /^attp$/i
-
+handler.limit = 3
 module.exports = handler
