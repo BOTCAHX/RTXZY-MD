3424e94a8d4dd0a2382b402228616ab2b46d334c
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-12-20 05:17:58 +0700

    jancuk pristel

--- plugins/sticker-ttp.js
+++ plugins/sticker-ttp.js
@@ -4,12 +4,12 @@ const fetch = require('node-fetch')
 let handler = async (m, { conn, text, command }) => {
   let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
   if (/^ttp1?$/i.test(command)) {
-    let stiker = await sticker5(null, API('xteam', '/ttp', { file: '', text: teks }), packname, author)
+    let stiker = await sticker5(null, API('https://api.tiodevhost.my.id', '/api/maker/ttp', { file: '', text: teks }), packname, author)
     if (stiker) return await conn.sendFile(m.chat, stiker, '', '', m)
     throw stiker.toString()
   }
   if (/2$/i.test(command)) {
-    let url = await fetch(API('https://salism3api.pythonanywhere.com', '/text2img/', { text: teks }))
+    let url = await fetch(API('https://api.tiodevhost.my.id', '/api/maker/ttp', { text: teks }))
     if (!url.ok) throw eror
     res = await url.json()
     stick = res.image
@@ -17,12 +17,12 @@ let handler = async (m, { conn, text, command }) => {
     conn.sendFile(m.chat, stiker, '', '', m)
   }
   if (/3$/i.test(command)) {
-    let stiker = await sticker5(null, API('hardianto', '/api/maker/ttp', { text: teks }, 'apikey'), packname, author)
+    let stiker = await sticker5(null, API('https://api.tiodevhost.my.id', '/api/maker/ttp', { text: teks }, 'apikey'), packname, author)
     if (stiker) return await conn.sendFile(m.chat, stiker, '', '', m)
     throw stiker.toString()
   }
   if (/4$/i.test(command)) {
-    let url = await fetch(API('https://salism3api.pythonanywhere.com', '/text2img/', { text: teks, outlineColor: '255,0,0,255', textColor: '0,0,0,255' }))
+    let url = await fetch(API('https://api.tiodevhost.my.id', '/api/maker/ttp', { text: teks, outlineColor: '255,0,0,255', textColor: '0,0,0,255' }))
     if (!url.ok) throw eror
     res = await url.json()
     stick = res.image
@@ -31,8 +31,8 @@ let handler = async (m, { conn, text, command }) => {
     throw stiker.toString()
   }
 }
-handler.help = new Array(4).fill('ttp').map((v, i) => v + (i + 1) + ' <teks>')
+handler.help = ['ttp']
 handler.tags = ['sticker']
-handler.command = /^ttp[1-4]?$/i
+handler.command = /^(ttp)$/i
 
 module.exports = handler
