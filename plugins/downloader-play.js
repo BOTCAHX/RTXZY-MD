cff1673bd09a48d5936eacb22d22b50b4834f7af
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-12-20 07:47:53 +0700

    .

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -1,4 +1,3 @@
-let fetch = require ('node-fetch')
 let { youtubeSearch } = require ('@bochilteam/scraper')
 
 let handler = async (m, { conn, text, usedPrefix }) => {
@@ -14,11 +13,10 @@ let handler = async (m, { conn, text, usedPrefix }) => {
  if (durationS > 9999999999) return conn.sendMessage(m.chat, { text: `*Download:* ${await shortUrl(url)}\n\n_Durasi terlalu panjang download manual saja lewat link berikut..._` }, { quoted: msg })
   conn.sendMessage(m.chat, { audio: { url: ytLink }, mimetype: 'audio/mpeg' }, { quoted: msg })
 }
-handler.help = ['play']
+handler.command = handler.help = ['play', 'song', 'lagu']
 handler.tags = ['downloader']
-handler.command = /^(play|song)$/i
 handler.exp = 0
-
+handler.limit = true
 module.exports = handler
 
 async function shortUrl(url) {
