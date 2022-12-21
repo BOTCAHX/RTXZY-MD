f5aeef0b4c1796248ed7e16ab2e8ca940a50abd0
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-12-21 22:57:44 +0700

    shortlink

--- plugins/tools-shortlink.js
+++ plugins/tools-shortlink.js
@@ -1,8 +1,12 @@
-let fetch = require('node-fetch')
+var fetch = require("node-fetch");
 
-let handler = async (m, { conn, usedPrefix, args }) => {
-	let title = `— *S H O R T E D  U R L* —`
-    let caption = 'Please select the URL type'
+var handler = async (m, {
+                           conn, 
+                           usedPrefix, 
+                           args })
+                       => {
+                  	var title = `— *S H O R T E D  U R L* —`
+                    var caption = 'Please select the URL type'
 const sections = [
    {
 	title: "TYPE URL",
@@ -26,26 +30,26 @@ if (!args[0]) return m.reply('Wheres the link?')
 if (!args[0].startsWith('https://')) throw 'Enter Url With Prefix *https://*'
 if (!args[1]) return conn.sendMessage(m.chat, listMessage, { quoted: m })
 
-let tesk = '🚀 *Result link:* '
-let pros = '_*Converting Link. . .*_'
+var tesk = '🚀 *Result link:* '
+var pros = '_*Converting Link. . .*_'
 //Case Tinyurl 
 if (args[1] == "tinyurl") {
-	let tiny = await (await fetch(`https://api.tiodevhost.my.id/api/linkshort/tinyurl?link=${args[0]}`)).json()
+	var tiny = await (await fetch(`https://api.tiodevhost.my.id/api/linkshort/tinyurl?link=${args[0]}`)).json()
 m.reply(pros).then(_ => conn.reply(m.chat, `${tesk}${tiny.result}`,m))
 }
 //Case Cuttly
 if (args[1] == "cuttly") {
-	let cuttly = await (await fetch(`https://api.tiodevhost.my.id/api/linkshort/cuttly?link=${args[0]}`)).json()
+	var cuttly = await (await fetch(`https://api.tiodevhost.my.id/api/linkshort/cuttly?link=${args[0]}`)).json()
 m.reply(pros).then(_ => conn.reply(m.chat, `${tesk}${cuttly.result}`,m))
 }
 //Case Bitly 
 if (args[1] == "bitly") {
-	let bitly = await (await fetch(`https://api.tiodevhost.my.id/api/linkshort/bitly?link=${args[0]}`)).json()
+	var bitly = await (await fetch(`https://api.tiodevhost.my.id/api/linkshort/bitly?link=${args[0]}`)).json()
 m.reply(pros).then(_ => conn.reply(m.chat, `${tesk}${bitly.result}`,m))
   }
 }
-handler.help = ['short <url> <type>']
-handler.tags = ['internet']
+handler.help = ['short <url> <type>'];
+handler.tags = ['internet'];
 handler.command = /^(short(url)?)$/i
 
-module.exports = handler
+module.exports = handler;
