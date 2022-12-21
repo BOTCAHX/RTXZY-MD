4495b5fa3192663c7725b814319b622c1b54f77d
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-12-21 12:44:57 +0700

    update

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -1,16 +1,16 @@
-let fetch = require ('node-fetch')
-let { youtubeSearch } = require ('@bochilteam/scraper')
-let handler = async (m, { conn, text }) => {
+var { 
+     youtubeSearch } = require('@bochilteam/scraper');
+var handler = async (m, {
+ conn,
+ text }) => {
   if (!text) throw 'Url nya mana?'
   m.reply('_Proses..._')
-  let vid = (await youtubeSearch(text)).video[0]
-  let { videoId } = vid
-  let url = 'https://www.youtube.com/watch?v=' + videoId
-let ytLink = `https://ytdl.tiodevhost.my.id/?url=${url}&filter=audioandvideo&quality=highestvideo&contenttype=video/mp4`
+  var vid = (await youtubeSearch(text)).video[0]
+  var { videoId } = vid
+  var url = 'https://www.youtube.com/watch?v=' + videoId
+var ytLink = `https://ytdl.tiodevhost.my.id/?url=${url}&filter=audioandvideo&quality=highestvideo&contenttype=video/mp4`
   conn.sendMessage(m.chat, { video: { url: ytLink }, mimetype: 'video/mp4' }, { quoted: m })
 }
-handler.help = ['ytv'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.command = /^(ytv|ytvideo|ytmp4)$/i
-
-module.exports = handler
+handler.command = handler.help = ['ytv', 'ytvideo', 'ytmp4'];
+handler.tags = ['downloader'];
+module.exports = handler;
