bd34c118cbfaf0f3c8dbdd35e01d7ea03a71ed7c
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-12-21 13:36:24 +0700

    biar muncul button

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -1,10 +1,15 @@
-   var { 
-   youtubeSearch } = require('@bochilteam/scraper');
+var {
+	youtubeSearch,
+	youtubedl,
+	youtubedlv2,
+	youtubedlv3
+                } = require('@bochilteam/scraper');
    var handler = async (m, { 
     conn,
     text, 
-    usedPrefix }) => {
-  if (!text) throw 'Input Query'
+    usedPrefix
+               }) => {
+  if (!text) throw 'Enter Title'
   var vid = (await youtubeSearch(text)).video[0]
   if (!vid) throw 'Video/Audio Tidak Ditemukan'
   var { title, 
@@ -14,18 +19,19 @@
         durationH, 
         durationS,
         viewH,
-        publishedTime } = vid
+        publishedTime
+                       } = vid
   var url = 'https://www.youtube.com/watch?v=' + videoId
-  var web = `https://ytdl.tiodevhost.my.id/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
+  let web = `https://ytdl.tiodevhost.my.id/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
   var captionvid = `⭔ Title: ${title}\n⭔ Published: ${publishedTime}\n⭔ Duration: ${durationH}\n⭔ Views: ${viewH}\n⭔ Description: ${description}\n⭔ Url:  ${url}`
-  var buttons = [{ buttonText: { displayText: 'Video' }, buttonId: `${usedPrefix}ytv ${url}` }]
-  var msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captionvid, footer: '_Audio on progress..._', buttons }, { quoted: m })
- if (durationS > 18000) return conn.sendMessage(m.chat, { text: `*Link Original:* ${await cut(url)}\n\n_Durasi terlalu panjang_` }, { quoted: msg })
-  conn.sendMessage(m.chat, { audio: { url: web }, mimetype: 'audio/mpeg' }, { quoted: msg })
+  var pesan = await conn.sendButtonImg(m.chat, thumbnail,  captionvid, '_Audio on progress..._', 'Video', '${usedPrefix}ytv ${url}', m, {  
+      quoted: m})
+ if (durationS > 18000) return conn.sendMessage(m.chat, { text: `*Link Original:* ${await cut(url)}\n\n_Durasi terlalu panjang_` }, { quoted: pesan })
+  conn.sendMessage(m.chat, { audio: { url: web }, mimetype: 'audio/mpeg' }, { quoted: pesan })
 }
 handler.command = handler.help = ['play', 'song', 'lagu'];
 handler.tags = ['downloader'];
-handler.exp = 0
+handler.exp = 0;
 handler.limit = true;
 handler.premium = false;
 module.exports = handler;
