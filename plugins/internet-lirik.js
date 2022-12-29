c0185a33b9bc77d6aaca2464a77f8c111b8f5bd5
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-12-29 17:19:44 +0700

    update

--- plugins/internet-lirik.js
+++ plugins/internet-lirik.js
@@ -1,34 +1,26 @@
-let { MessageType } = require('@adiwajshing/baileys')
-let fetch = require('node-fetch')
-let handler = async (m, { conn, args, usedPrefix, DevMode }) => {
-    try {
-        if (!args || !args[0] || args.length < 1) return m.reply(`[❗] Format salah\nContoh : ${usedPrefix}lirik sad!`)
-        let res = await fetch(global.API('bg', '/lirik', { 
-            title: args[0],
-            artist: args[1] || '' 
-        }))
-        let json = await res.json()
-        if (json.status !== true) throw json
-        m.reply(`
-〘 *Lirik/Lyrics* 〙
-_Song Name_ : *${args[0]}*
-Requested by : *${conn.getName(m.sender)}*
-\`\`\`${json.result}\`\`\`
-`.trim())
-    } catch (e) {
-        m.reply('Error!')
-        console.log(e)
-        if (DevMode) {
-            let file = require.resolve(__filename)
-            for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)) {
-                conn.sendMessage(jid, file + ' error\nNo: *' + m.sender.split`@`[0] + '*\nCommand: *' + m.text + '*\n\n*' + e + '*', MessageType.text)
-            }
-        }
-    }
-}
-    
-handler.help = ['lirik', 'lyrics'].map(v => ' [title] [artist]')
-handler.tags = ['internet']
-handler.command = /^(l(irik|yrics))$/i
+var {
+	lyrics,
+	lyricsv2
+} = require('@bochilteam/scraper');
 
-module.exports = handler
+var handler = async (m, {
+	conn,
+	text,
+	usedPrefix,
+	command
+}) => {
+	var teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
+	if (!teks) throw `Use example ${usedPrefix}${command} hallo`
+	var result = await lyricsv2(teks).catch(async _ => await lyrics(teks))
+	m.reply(`
+Lyrics *${result.title}*
+Author ${result.author}
+${result.lyrics}
+Url ${result.link}
+`.trim())
+};
+
+handler.help = ['lirik'].map(v => v + ' <Apa>')
+handler.tags = ['internet'];
+handler.command = /^(lirik|lyrics|lyric)$/i
+module.exports = handler;
