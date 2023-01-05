c4220db379d2d7e0f747f92524422c83dc914467
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-01-05 18:22:31 +0700

    kucoba coba siapa tau work pffttt

--- plugins/main-update.js
+++ plugins/main-update.js
@@ -1,21 +1,25 @@
-let cp = require ('child_process')
-let { promisify } = require ('util')
-let exec = promisify(cp.exec).bind(cp)
-let handler = async (m, { conn}) => {
+var cp = require ('child_process');
+var {
+promisify 
+} = require ("util");
+var exec = promisify(cp.exec).bind(cp)
+var handler = async (m, {
+conn
+}) => {
 	await conn.reply(m.chat, `Looking for resources...`, m)
-    let o
+    var o
     try {
-        o = await exec('git pull')
+        o = await exec('git pull https://github.com/BOTCAHX/RTXZY-MD.git')
     } catch (e) {
         o = e
     } finally {
         let { stdout, stderr } = o
-        if (stdout.trim()) conn.sendButton(m.chat, `root@tio`, stdout, null, [["Back", ".menu"],["Command", ".pay"]], m)
+        if (stdout.trim()) conn.sendButton(m.chat, `root@tio`, stdout, null, [["Back", ".menu"],["Ping", ".ping"]], m)
         if (stderr.trim()) m.reply(stderr)
     }
 }
-handler.help = ['npminfo']
-handler.tags = ['info']
-handler.command = /^(act|updategit|update)$/i
-
-module.exports = handler
+handler.help = ['act'];
+handler.tags = ['info'];
+handler.command = ["act", "updategit", "update"];
+handler.owner = true;
+module.exports = handler;
