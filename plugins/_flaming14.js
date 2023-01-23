100f46db862a912aa9415a9dec0c14fe6b302446
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-01-23 17:26:21 +0700

    Update _flaming14.js

--- plugins/_flaming14.js
+++ plugins/_flaming14.js
@@ -6,7 +6,7 @@ args
    response = args.join(' ').split('|')
   if (!args[0]) throw 'Masukkan Text\nContoh : .coffee BOTCAHX'
   m.reply('_Proses..._')
-  var res = `https://api.botcahx.biz.id/api/photooxy/coffee-cup?text=${response[0]}&apikey=Apikey`
+  var res = `https://api.botcahx.biz.id/api/photooxy/coffee-cup?text=${response[0]}&apikey=Admin`
   conn.sendFile(m.chat, res, 'botcahx.jpg', `© BOTCAHX`, m, false)
 }
 handler.help = ['coffee'].map(v => v + ' <text>')
