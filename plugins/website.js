163d0393c0ea8e5c605e46d8717e553ef705b867
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-01-23 17:39:23 +0700

    Update website.js

--- plugins/website.js
+++ plugins/website.js
@@ -4,8 +4,10 @@ let krtu = `web`
 m.reply(`
 > http://api.tiodevhost.my.id
 > http://ytdl.tiodevhost.my.id
+> http://botcahx.biz.id
 `.trim()) 
 }
 handler.command = /^(web)$/i
-
+handler.tags = ['main']
+handler.help = ['api', 'web']
 module.exports = handler
