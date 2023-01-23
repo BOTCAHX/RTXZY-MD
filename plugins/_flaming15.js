b8910913d14e7c35736793c517603e661ac0ef68
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-01-23 17:25:39 +0700

    b

--- plugins/_flaming15.js
+++ plugins/_flaming15.js
@@ -1,9 +1,12 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, args }) => {
+var fetch = require('node-fetch')
+var handler = async (m, { 
+conn, 
+args 
+}) => {
    response = args.join(' ').split('|')
   if (!args[0]) throw 'Masukkan Text\nContoh : . butterfly BOTCAHX'
   m.reply('_Proses..._')
-  let res = `https://api.tiodevhost.my.id/api/photooxy/butterfly?text=${response[0]}`
+  var res = `https://api.botcahx.biz.id/api/photooxy/butterfly?text=${response[0]}&apikey=Admin`
   conn.sendFile(m.chat, res, 'botcahx.jpg', `© BOTCAHX`, m, false)
 }
 handler.help = ['butterfly'].map(v => v + ' <text>')
