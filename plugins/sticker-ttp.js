8fb0f51c7428741c830cca530a7f595b1a18c324
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-01-25 04:48:15 +0700

    update

--- plugins/sticker-ttp.js
+++ plugins/sticker-ttp.js
@@ -1,19 +1,30 @@
-const { sticker5 } = require('../lib/sticker');
-const fetch = require('node-fetch');
+var { 
+sticker5 
+} = require('../lib/sticker')
+var fs = require('fs')
+var handler = async (m, {
+ conn, 
+ args, 
+ text, 
+ usedPrefix, 
+ command
+ }) => {
+    text = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.quoted && m.quoted.caption ? m.quoted.caption : m.quoted && m.quoted.description ? m.quoted.description : ''
+    if (!text) throw `Example : ${usedPrefix + command} Lagi Ruwet`
+    const res = `https://api.botcahx.biz.id/api/maker/ttp?text=${encodeURIComponent(text.substring(0, 151))}&apikey=Admin`
+    var error = fs.readFileSync(`./media/sticker/emror.webp`)
+    try {
+        if (command == 'ttp' || command == 'ttp') {
+            var stiker = await sticker5(res, { packname })
+            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+        }
+    } catch (e) {
+        console.log(e)
+        await conn.sendFile(m.chat, error, 'error.webp', '', m)
+    }
+}
 
-var handler = async (m, { 
-conn, 
-text, 
-command 
-}) => {
-  var teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-  if (/^ttp?$/i.test(command)) {
-    let stiker = await sticker5(null, API('https://api.tiodevhost.my.id', '/api/maker/ttp', { file: '', text: teks }), packname, author)
-    if (stiker) return await conn.sendFile(m.chat, stiker, '', '', m)
-    throw stiker.toString()
-  }
-};
-handler.command = handler.help = ['ttp'];
-handler.tags = ['sticker'];
-handler.limit = true;
-module.exports = handler;
+handler.command = handler.help = ['ttp']
+handler.tags = ['sticker']
+handler.limit = true
+module.exports = handler
