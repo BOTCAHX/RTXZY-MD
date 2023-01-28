5beb72289e305583cf7fd2374d70418461bbf536
VyZens <110511365+VyZens@users.noreply.github.com>
2023-01-28 22:04:44 +0700

    Anu om

--- plugins/nsfw-all.js
+++ plugins/nsfw-all.js
@@ -1,6 +1,6 @@
 
 let handler = async (m, { conn, command }) => {
-let nyenye = `https://zenzapis.xyz/api/morensfw/${command}?apikey=80b5045dce`
+let nyenye = `https://api.zahwazein.xyz/api/morensfw/${command}?apikey=8dd8336cbd`
     conn.sendButtonImg(m.chat, nyenye, 'Nih', wm2, 'Next', `.${command}`, m) 
 }
 handler.help = ['ahegao', 'ass', 'bdsm', 'blowjob', 'cum', 'cuckold', 'ero', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'hentaigif', 'jahy', 'maid', 'manga', 'neko', 'orgy', 'panties', 'pussy','sfwneko', 'tentacles', 'thighs', 'yuri', 'zettairyouiki']
