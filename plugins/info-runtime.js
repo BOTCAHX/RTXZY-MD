fc33f0b1f9bbbdc90676facd1ebf943415ec235a
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-01-30 21:53:06 +0700

    kontol

--- plugins/info-runtime.js
+++ plugins/info-runtime.js
@@ -1,21 +1,22 @@
-var handler = async (m, { 
-conn, 
-usedPrefix,
-command
-}) => {
-    var _uptime = process.uptime() * 1000
-    var uptimer = clockString(_uptime)
-    var time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
+let handler = async (m, { conn, usedPrefix, command }) => {
+    let _uptime = process.uptime() * 1000
+    let tio = clockString(_uptime)
+    let time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
 
 m.reply(`
 *───「 RUNTIME BOT 」───*
 
-Runtime : ${uptimer} 
-Waktu : ${time}`)
-};
-handler.command = handler.help = ['runtime', 'uptime'];
-handler.tags = ['info'];
-module.exports = handler;
+Runtime : ${tio} 
+Waktu : ${time}
+
+    `)
+}
+handler.help = ['runtime']
+handler.tags = ['info']
+handler.command = /^(uptime|runtime)$/i
+
+module.exports = handler
+
 function clockString(ms) {
     let days = Math.floor(ms / (24 * 60 * 60 * 1000));
     let daysms = ms % (24 * 60 * 60 * 1000);
@@ -24,5 +25,5 @@ function clockString(ms) {
     let minutes = Math.floor((hoursms) / (60 * 1000));
     let minutesms = ms % (60 * 1000);
     let sec = Math.floor((minutesms) / (1000));
-    return days + " D " + hours + " H " + M + " Minute " + S + " Second ";
+    return days + " Day " + hours + " Hour " + minutes + " Minute " + sec + " Second ";
 }
