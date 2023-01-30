f3fb1cbbfa6f2815b71c074774e15da6552a4c01
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-01-30 07:36:26 +0700

    baru sadar gak fungsi

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -24,7 +24,7 @@ var {
   var url = 'https://www.youtube.com/watch?v=' + videoId
   let web = `https://ytdl.tiodevhost.my.id/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
   var captionvid = `⭔ Title: ${title}\n⭔ Published: ${publishedTime}\n⭔ Duration: ${durationH}\n⭔ Views: ${viewH}\n⭔ Description: ${description}\n⭔ Url:  ${url}`
-  var pesan = await conn.sendButtonImg(m.chat, thumbnail,  captionvid, '_Audio on progress..._', 'Video', '${usedPrefix}ytv ${url}', m, {  
+  var pesan = await conn.sendButtonImg(m.chat, thumbnail,  captionvid, '_Audio on progress..._', 'Video', '#ytv ${url}', m, {  
       quoted: m})
  if (durationS > 18000) return conn.sendMessage(m.chat, { text: `*Link Original:* ${await cut(url)}\n\n_Durasi terlalu panjang_` }, { quoted: pesan })
   conn.sendMessage(m.chat, { audio: { url: web }, mimetype: 'audio/mpeg' }, { quoted: pesan })
