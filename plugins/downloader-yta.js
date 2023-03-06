e51cfc34b231b6b7944981dcd90d3ecd482b7730
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-03-06 12:46:14 +0700

    fix delay download

--- plugins/downloader-yta.js
+++ plugins/downloader-yta.js
@@ -1,22 +1,31 @@
-
-var {
-	youtubeSearch,
-	youtubedl,
-	youtubedlv2,
-	youtubedlv3
-                } = require('@bochilteam/scraper');
-  var handler = async (m, { 
-                        conn,
-                        text 
-                        }) => {
-       if (!text) throw 'Url nya mana?'
-       m.reply('_Proses..._')
-      var vid = (await youtubeSearch(text)).video[0]
-var { videoId } = vid
-var url = 'https://www.youtube.com/watch?v=' + videoId
-var ytLink = `https://ytdl.tiodevhost.my.id/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
-  conn.sendMessage(m.chat, { audio: { url: ytLink }, mimetype: 'audio/mpeg' }, { quoted: m })
-}
+var fetch = require("node-fetch");
+var handler = async (m, { 
+usedPrefix, 
+text,
+command
+ }) => { 
+if (!text) throw `Contoh:\n${usedPrefix + command} https://www.youtube.com/watch?v=iu7P4QnQP2k`
+m.reply(wait)
+var anu = await fetch(`https://api.botcahx.xyz/api/download/ytmp3?url=${text}&apikey=Admin`)
+var con = await anu.json()
+var capt = `• Title : ${con.title}\n• Size: ${con.size}\n• ID: ${con.id}\n• URL: ${text}
+`
+fdoc = {
+  key : {
+  remoteJid: 'status@broadcast',
+  participant : '0@s.whatsapp.net'
+  },
+  message: {
+  documentMessage: {
+  title: "Created By BOTCAHX", 
+                            }
+                          }
+                        }
+conn.sendButtonImg(m.chat, con.thumbnail, capt, `Media sedang dikirim...`, `Video`, `${usedPrefix}ytmp4 ${text}`, fdoc)
+var parse = con.download
+conn.sendFile(m.chat, parse, 'result.mp3', '', fdoc)
+    };  
 handler.command = handler.help = ['yta', 'ytaudio', 'ytmp3'];
+handler.limit = true;
 handler.tags = ['downloader'];
 module.exports = handler;
