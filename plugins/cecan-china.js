9ad64d31bd6c49420f88bc570ee5c0d871903042
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-03-06 12:53:54 +0700

    cecan

---
+++ plugins/cecan-china.js
@@ -0,0 +1,7 @@
+var handler = async(m,{ conn }) => {
+var bingciling = 'https://api.botcahx.xyz/api/cecan/china?apikey=Admin'
+conn.sendButtonImg(m.chat, bingciling, `Chinese Girls`, wm, 'Next', '.china', m) 
+};
+handler.command = handler.help = ['cecanchina'];
+handler.tags = ['asupan'];
+module.exports = handler;
