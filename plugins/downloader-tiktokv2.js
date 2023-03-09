28c9ced40aa6ac178e13f1df91788778c6bca108
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-03-09 08:14:56 +0700

    mokad

--- plugins/downloader-tiktokv2.js
+++ plugins/downloader-tiktokv2.js
@@ -1,16 +1,38 @@
-var fetch = require("node-fetch");
+var nodeF = require("node-fetch");
 var handler = async (m, {
- text, 
- usedPrefix, 
- command
- }) => {
-    if (!text) throw `Contoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
+	conn,
+	args,
+	usedPrefix,
+	command
+}) => {
+if (!args[0]) throw `Masukan URL!\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
+if (!args[0].match(/tiktok/gi)) throw `URL Tidak Ditemukan!`
+m.reply('*Please wait..*.')
+ var tioxd = await nodeF(`https://api.botcahx.biz.id/api/dowloader/tikok?url=${args[0]}&apikey=Admin`)
+if (!tioxd.ok) throw await tioxd.text()
+var tiodl = await tioxd.json()
+if (!tiodl.status) throw tiodl
+var { 
+video, 
+video2, 
+username,
+description,
+audio
+} = tiodl.result
+await conn.sendFile(m.chat, video, 'tiovid.mp4', `
+*Deskripsi*: ${description}
+\n*Username*: ${username}`, m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 
-var anu = await fetch(`https://api.botcahx.xyz/api/download/tiktok?url=${text}&apikey=Admin`)
-var con = await anu.json()
-var parse = con.result.video.no_watermark
-conn.sendFile(m.chat, parse, 'tiktok.mp4', 'Nih..', m)
-    };  
-handler.command = handler.help = ['tiktokv2'];
-handler.tags = ['internet'];
+conn.sendFile(m.chat, audio, 'tok2.opus', null, m)
+};
+handler.command = handler.help = ['tiktokdl2'];
+handler.tags = ['downloader'];
+handler.limit = true;
+handler.group = false;
+handler.premium = false;
+handler.owner = false;
+handler.admin = false;
+handler.botAdmin = false;
+handler.fail = null;
+handler.private = false;
 module.exports = handler;
