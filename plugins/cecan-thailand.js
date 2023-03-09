1d34b8418bcecba1ea9cda68b3f92a88ef1c6441
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-03-09 08:11:29 +0700

    Delete cecan-thailand.js

--- plugins/cecan-thailand.js
+++
@@ -1,7 +0,0 @@
-var handler = async(m,{ conn }) => {
-var wikwik = 'https://api.botcahx.xyz/api/cecan/thailand?apikey=Admin'
-conn.sendButtonImg(m.chat, wikwik, `Thailand Girls`, wm, 'Next', '.cecanthailand', m) 
-};
-handler.command = handler.help = ['cecanthailand'];
-handler.tags = ['asupan'];
-module.exports = handler;
