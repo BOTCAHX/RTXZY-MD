3477e7af27441109a21f3131892f0d16cf57b700
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-03-09 08:11:56 +0700

    Delete cecan-malaysia.js

--- plugins/cecan-malaysia.js
+++
@@ -1,7 +0,0 @@
-var handler = async(m,{ conn }) => {
-var upin = 'https://api.botcahx.xyz/api/cecan/malaysia?apikey=Admin'
-conn.sendButtonImg(m.chat, upin, `Malaysia Girls`, wm, 'Next', '.cecanmalaysia', m) 
-};
-handler.command = handler.help = ['cecanmalaysia'];
-handler.tags = ['asupan'];
-module.exports = handler;
