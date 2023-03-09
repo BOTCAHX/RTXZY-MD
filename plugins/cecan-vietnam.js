6a6d58693889e659587c166fb73d6a77d4ef5d9c
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-03-09 08:10:18 +0700

    Delete cecan-vietnam.js

--- plugins/cecan-vietnam.js
+++
@@ -1,7 +0,0 @@
-var handler = async(m,{ conn }) => {
-var nguyen = 'https://api.botcahx.xyz/api/cecan/vietnam?apikey=Admin'
-conn.sendButtonImg(m.chat, nguyen, `Vietnam Girls`, wm, 'Next', '.cecanvietname', m) 
-};
-handler.command = handler.help = ['cecanvietname'];
-handler.tags = ['asupan'];
-module.exports = handler;
