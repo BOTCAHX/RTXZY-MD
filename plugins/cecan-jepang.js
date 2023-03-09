b545b78d8362ec9021362988b5372b0c0d35037f
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-03-09 08:12:46 +0700

    Delete cecan-jepang.js

--- plugins/cecan-jepang.js
+++
@@ -1,7 +0,0 @@
-var handler = async(m,{ conn }) => {
-var kimochi = 'https://api.botcahx.xyz/api/cecan/japan?apikey=Admin'
-conn.sendButtonImg(m.chat, kimochi, `Japan Girls`, wm, 'Next', '.cecanjapan', m) 
-};
-handler.command = handler.help = ['cecanjapan'];
-handler.tags = ['asupan'];
-module.exports = handler;
