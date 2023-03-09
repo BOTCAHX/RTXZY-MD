67271986a8119462911356e81fe62621b46a0e59
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-03-09 08:12:23 +0700

    Delete cecan-korea.js

--- plugins/cecan-korea.js
+++
@@ -1,7 +0,0 @@
-var handler = async(m,{ conn }) => {
-var batues = 'https://api.botcahx.xyz/api/cecan/korea?apikey=Admin'
-conn.sendButtonImg(m.chat, batues, `Plastic Girls`, wm, 'Next', '.cecankorea', m) 
-};
-handler.command = handler.help = ['cecankorea'];
-handler.tags = ['asupan'];
-module.exports = handler;
