383407e8b902374ba7b359b46a14cea6c87ec6c3
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-03-09 08:10:50 +0700

    Delete cecan-china.js

--- plugins/cecan-china.js
+++
@@ -1,7 +0,0 @@
-var handler = async(m,{ conn }) => {
-var bingciling = 'https://api.botcahx.xyz/api/cecan/china?apikey=Admin'
-conn.sendButtonImg(m.chat, bingciling, `Chinese Girls`, wm, 'Next', '.cecanchina', m) 
-};
-handler.command = handler.help = ['cecanchina'];
-handler.tags = ['asupan'];
-module.exports = handler;
