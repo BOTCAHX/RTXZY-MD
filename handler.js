fa95f3087e07aafb03aee40cbd61035f5a77e3d1
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-03-14 11:27:27 +0700

    logs

--- handler.js
+++ handler.js
@@ -678,14 +678,12 @@ module.exports = {
                 }
             }
 
-            // try {
-            //     require('./lib/print')(m, this)
-            // } catch (e) {
-            //     console.log(m, m.quoted, e)
-            // }
-            if (opts['autoread']) await this.chatRead(m.chat, m.isGroup ? m.sender : undefined, m.id || m.key.id).catch(() => { })
-            let quequeIndex = this.msgqueque.indexOf(m.id || m.key.id)
-            if (opts['queque'] && m.text && quequeIndex !== -1) this.msgqueque.splice(quequeIndex, 1)
+            try {
+                 require('./lib/print')(m, this)
+             } catch (e) {
+                 console.log(m, m.quoted, e)
+             }
+            if (opts['autoread']) await this.readMessages([m.key]) //this.chatRead(m.chat, m.isGroup ? m.sender : undefined, m.id || m.key.id).catch(() => { })
         }
     },
      async participantsUpdate({ id, participants, action }) {
