067bb3cfae3f8608a39ac1f972626751a19066ad
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-04-02 19:08:11 +0700

    ttp

--- plugins/sticker-ttp.js
+++ plugins/sticker-ttp.js
@@ -1,30 +1,23 @@
-var { 
-sticker5 
-} = require('../lib/sticker')
-var fs = require('fs')
-var handler = async (m, {
- conn, 
- args, 
+const { sticker5 } = require('../lib/sticker');
+const api = require('api-dylux');
+async function handler(m, {
  text, 
  usedPrefix, 
  command
- }) => {
-    text = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.quoted && m.quoted.caption ? m.quoted.caption : m.quoted && m.quoted.description ? m.quoted.description : ''
-    if (!text) throw `Example : ${usedPrefix + command} Lagi Ruwet`
-    const res = `https://api.botcahx.biz.id/api/maker/ttp?text=${encodeURIComponent(text.substring(0, 151))}&apikey=Admin`
-    var error = fs.readFileSync(`./media/sticker/emror.webp`)
-    try {
-        if (command == 'ttp' || command == 'ttp') {
-            var stiker = await sticker5(res, { packname })
-            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
-        }
-    } catch (e) {
-        console.log(e)
-        await conn.sendFile(m.chat, error, 'error.webp', '', m)
-    }
-}
-
-handler.command = handler.help = ['ttp']
-handler.tags = ['sticker']
-handler.limit = true
-module.exports = handler
+ }) {   
+  if (!text) throw `Tolong berikan Text!\n\n*Example:* ${ usedPrefix + command} hai guys`
+  await m.reply(wait)
+  api.ttp(text)
+    .then((res) => {
+      return sticker5(res.result, { packname });
+    })
+    .then((stikk) => { 
+    conn.sendFile(m.chat, stikk, 'ttp.webp', '', m);
+    })
+    .catch((err) => {
+      console.error(err);
+    });     
+}        
+handler.command = handler.help = ['ttp','ttpsticker'];
+handler.tags = ['sticker'];
+module.exports = handler;
