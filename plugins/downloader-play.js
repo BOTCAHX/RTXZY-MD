3a348216f49c34aa814523c344caee7d2f7c4bfc
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-04-07 10:44:28 +0700

    without api

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -1,124 +1,86 @@
-/*
 var {
-	youtubeSearch,
-	youtubedl,
-	youtubedlv2,
-	youtubedlv3
-                } = require('@bochilteam/scraper');
-   var handler = async (m, { 
+    youtubeSearch,
+    youtubedl,
+    youtubedlv2,
+    youtubedlv3
+} = require('@bochilteam/scraper');
+var hxz = require('hxz-api');
+var handler = async (m, {
     conn,
-    text, 
+    text,
     usedPrefix
-               }) => {
-  if (!text) throw 'Enter Title'
-  var vid = (await youtubeSearch(text)).video[0]
-  if (!vid) throw 'Video/Audio Tidak Ditemukan'
-  var { title, 
-        description, 
-        thumbnail, 
-        videoId, 
-        durationH, 
-        durationS,
-        viewH,
-        publishedTime
-                       } = vid
-  var url = 'https://www.youtube.com/watch?v=' + videoId
-  let web = `https://ytdl.tiodevhost.my.id/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
-  var captionvid = `⭔ Title: ${title}\n⭔ Published: ${publishedTime}\n⭔ Duration: ${durationH}\n⭔ Views: ${viewH}\n⭔ Description: ${description}\n⭔ Url:  ${url}\nID: ${videoId}`
-  var pesan = await conn.sendButtonImg(m.chat, thumbnail,  captionvid, '_Audio on progress..._', 'Video', '.ytv ${url}', m, {  
-      quoted: m})
- if (durationS > 18000000) return conn.sendMessage(m.chat, { text: `*Link Original:* ${await cut(url)}\n\n_Durasi terlalu panjang_` }, { quoted: pesan })
-  conn.sendMessage(m.chat, { audio: { url: web }, mimetype: 'audio/mpeg' }, { quoted: pesan })
+}) => {
+    if (!text) throw 'Enter Title / Link From YouTube!'
+    try {
+        var vid = (await youtubeSearch(text)).video[0]
+        if (!vid) throw 'Video/Audio Tidak Ditemukan'
+        var {
+            title,
+            description,
+            thumbnail,
+            videoId,
+            durationH,
+            durationS,
+            viewH,
+            publishedTime
+        } = vid
+        if (durationS >= 3600) { 
+            m.reply('Video is longer than 1 hour!')
+        } else {
+            var url = 'https://www.youtube.com/watch?v=' + videoId
+            var cvr
+            try {
+                cvr = await hxz.youtube(url)
+            } catch (e) {
+                conn.reply(m.chat, wait, m)
+                cvr = await hxz.youtube(url) 
+            }
+            var sce = cvr.mp3
+            var tmb = thumbnail
+            var captionvid = `∘ Title: ${title}\n∘ Published: ${publishedTime}\n∘ Duration: ${durationH}\n∘ Second: ${durationS}\n∘ Views: ${viewH}\n∘ Url:  ${url}\n∘ Description: ${description}`
+            var pesan = await conn.sendMessage(m.chat, {
+                text: captionvid,
+                contextInfo: {
+                    externalAdReply: {
+                        title: "",
+                        body: "Powered by " + wm,
+                        thumbnailUrl: tmb,
+                        sourceUrl: url,
+                        mediaType: 1,
+                        showAdAttribution: true,
+                        renderLargerThumbnail: true
+                    }
+                }
+            })
+            conn.sendMessage(m.chat, {
+                audio: {
+                    url: sce
+                },
+                mimetype: 'audio/mpeg',
+                contextInfo: {
+                    externalAdReply: {
+                        title: title,
+                        body: "",
+                        thumbnailUrl: tmb,
+                        sourceUrl: url,
+                        mediaType: 1,
+                        showAdAttribution: true,
+                        renderLargerThumbnail: true
+                    }
+                }
+            }, {
+                quoted: pesan
+            })
+        }
+    } catch (e) {
+       //m.reply('Error: ' + e)
+       //var eror = e.toString() 
+       conn.reply(m.chat, `*Error:* ` + eror, m)
+    }
 }
-handler.command = handler.help = ['play','song'];
+handler.command = handler.help = ['play', 'song'];
 handler.tags = ['downloader'];
 handler.exp = 0;
 handler.limit = true;
 handler.premium = false;
 module.exports = handler;
-async function cut(url) {
-  url = encodeURIComponent(url)
-  let res = await fetch(`https://api.botcahx.biz.id/api/linkshort/cuttly?link=${url}&apikey=Admin`)
-  if (!res.ok) throw false
-  return await res.text()
-}
-*/
-
-var {
-	youtubeSearch,
-	youtubedl,
-	youtubedlv2,
-	youtubedlv3
-                } = require('@bochilteam/scraper');
-   var handler = async (m, { 
-    conn,
-    text, 
-    usedPrefix
-               }) => {
-  if (!text) throw 'Enter Title'
-  var vid = (await youtubeSearch(text)).video[0]
-  if (!vid) throw 'Video/Audio Tidak Ditemukan'
-  var { title, 
-        description, 
-        thumbnail, 
-        videoId, 
-        durationH, 
-        durationS,
-        viewH,
-        publishedTime
-                       } = vid
-  var url = 'https://www.youtube.com/watch?v=' + videoId
-
- /*let vide = `https://ytdl.tiodevhost.my.id/?url=${url}&filter=audioandvideo&quality=highestvideo&contenttype=video/mp4`*/
-
-  let web = `https://ytdl.tiodevhost.my.id/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
-  var tmb = thumbnail
-  var captionvid = `  ∘ Title: ${title}
-  ∘ Published: ${publishedTime}
-  ∘ Duration: ${durationH}
-  ∘ Second: ${durationS}
-  ∘ Views: ${viewH}  
-  ∘ Url:  ${url}
-  ∘ Description: ${description}`
-var pesan = await conn.sendMessage(m.chat, {
-text: captionvid,
-contextInfo: {
-externalAdReply: {
-title: "",
-body: "Powered by",
-thumbnailUrl: tmb ,
-sourceUrl: web,
-mediaType: 1,
-showAdAttribution: true,
-renderLargerThumbnail: true
-}}})
-
-if (durationS > 18000) return conn.sendMessage(m.chat, { text: `*Link Original:* ${await cut(url)}\n\n_Durasi terlalu panjang..._\n*Duration Limit!*` }, { quoted: pesan })
-  conn.sendMessage(m.chat, { audio: { url: web }, mimetype: 'audio/mpeg', contextInfo: {
-externalAdReply: {
-title: title,
-body: "",
-thumbnailUrl: tmb,
-sourceUrl: web,
-mediaType: 1,
-showAdAttribution: true,
-renderLargerThumbnail: true
-}}} , { quoted: pesan })
-/*await delay(10000);
-conn.sendMessage(m.chat, { video: { url: vide }, mimetype: 'video/mp4' }, { quoted: pesan })*/
-}
-handler.command = handler.help = ['play','song','ds'];
-handler.tags = ['downloader'];
-handler.exp = 0;
-handler.limit = true;
-handler.premium = false;
-module.exports = handler;
-async function cut(url) {
-  url = encodeURIComponent(url)
-  let res = await fetch(`https://api.botcahx.biz.id/api/linkshort/bitly?link=${url}&apikey=Admin`)
-  if (!res.ok) throw false
-  return await res.text()
-}
-/*async function delay(ms) {
-   await new Promise(resolve => setTimeout(resolve, ms));
-}*/
