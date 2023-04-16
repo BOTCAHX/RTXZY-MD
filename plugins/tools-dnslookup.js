22da45b31af7ca22ca97fd792978c0615885406f
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-04-17 00:32:23 +0700

    update

--- plugins/tools-dnslookup.js
+++ plugins/tools-dnslookup.js
@@ -4,22 +4,26 @@ by: TioXd
 */
 
 const fetch = require('node-fetch');
+
 const handler = async (m, { text, usedPrefix, command }) => {
 if (!text) throw `Masukkan Domain/Sub Domain!\n\n*Contoh:* api.botcahx.biz.id`;
+
     if (text.includes('https://') || text.includes('http://')) throw `Tolong masukkan domain/sub domain secara lengkap. Contoh: api.botcahx.biz.id`;
   try {
     const res = await fetch(`https://api.hackertarget.com/dnslookup/?q=${text}`)
       .then(response => response.text())
       .then(data => {
-        m.reply(`*INI ADALAH HASIL DNS LOOKUP UNTUK*\n${text}:\n\n${data});
+        m.reply(`*Ini Adalah Hasil Dns Lookup Untuk ${text}:*\n${data}`);
         console.log(data);
       })
       .catch(error => console.error(error));
+
   } catch (error) {
     console.log(error);
     m.reply('*Invalid data!*');
   }
 };
+
 handler.command = ['dnslookup', 'hackertarget', 'lookup'];
 handler.help = ['dnslookup', 'hackertarget', 'lookup'];
 handler.tags = ['internet'];
