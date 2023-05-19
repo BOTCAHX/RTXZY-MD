c013a7a55c495c1a1956307601b300582385b1ca
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-05-20 00:33:15 +0700

    openai

---
+++ plugins/_openai-ai.js
@@ -0,0 +1,16 @@
+var fetch = require('node-fetch');
+var handler = async (m, {
+ text, 
+ usedPrefix, 
+ command
+ }) => {
+if (!text) throw `Masukkan pertanyaan!\n\n*Contoh:* Siapa presiden Indonesia? `
+await m.reply(wait)
+  var apii = await fetch(`https://botcahx.cyclic.app/openai?text=${text}`)
+  var js = await apii.json()
+  await m.reply(js.result)
+}      
+handler.command = handler.help = ['ai','openai','chatgpt'];
+handler.tags = ['internet'];
+handler.premium = false
+module.exports = handler;
