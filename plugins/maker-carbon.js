eee9c3bc104fd26d05dfc77ac6ffaecdbcea9928
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-05-19 22:23:41 +0700

    carbon

--- plugins/maker-carbon.js
+++ plugins/maker-carbon.js
@@ -1,10 +1,28 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, text }) => {
-  if (!text) throw 'Masukkan Text\nContoh : .carbon require("node-fetch")'
-  m.reply('_Proses..._')
-  let res = `https://api.lolhuman.xyz/api/carbon?apikey=BrunoSobrino&code=${text}&language=python`
-  conn.sendFile(m.chat, res, 'carbon.jpg', `© BOTCAHX`, m, false)
+const carbon = require('carbon-now-scraper');
+const fs = require('fs');
+let handler = async (m, { conn, text, command}) => {
+
+  let inputText = m.quoted ? m.quoted.text : text;
+  if (!inputText) {
+    return conn.reply(m.chat, `Masukkan pesan:\n${command} hello world`, m);
+  }
+
+  var randomColor = ['#ef1a11', '#89cff0', '#660000', '#87a96b', '#e9f6ff', '#ffe7f7', '#ca86b0', '#83a3ee', '#abcc88', '#80bd76', '#6a84bd', '#5d8d7f', '#530101', '#863434', '#013337', '#133700', '#2f3641', '#cc4291', '#7c4848', '#8a496b', '#722f37', '#0fc163', '#2f3641', '#e7a6cb', '#64c987', '#e6e6fa', '#ffffff', '#00000000'];
+  const apiColor = randomColor[Math.floor(Math.random() * randomColor.length)];
+  conn.reply(m.chat, '*Tunggu few seconds...*', m)
+
+  const code = inputText
+  const output = "./tmp/carbon.png"
+  const options = {
+    lang: "auto",
+    theme: "dracula",
+    background: apiColor,
+    "width-adjustment": false
+  }
+  await carbon(code, output, options)
+  await conn.sendMessage(m.chat, { image: await fs.readFileSync(output), caption: `*Done*`},{quoted:m})
 }
+
 handler.help = ['carbon'].map(v => v + ' <text>')
 handler.tags = ['maker']
 handler.command = /^(carbon)$/i
