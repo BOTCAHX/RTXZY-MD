4539a6a1ab1fea298352434ad97baf35e0fa1075
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-05-19 21:35:50 +0700

    asupan

---
+++ plugins/downloader-asupan.js
@@ -0,0 +1,26 @@
+let handler = async(m, { conn }) => {
+  await conn.sendFile(m.chat, pickRandom(asupan), 'asupan.mp4', '', m)
+}
+handler.help = ['asupan']
+handler.tags = ['asupan']
+handler.command = /^asupan$/i
+handler.owner = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
+const asupan = [
+"https://api.botcahx.live/api/asupan/rikagusriani?apikey=${btc}",
+"https://api.botcahx.live/api/asupan/santuy?apikey=${btc}",
+"https://api.botcahx.live/api/asupan/ukhty?apikey=${btc}",
+"https://api.botcahx.live/api/asupan/bocil?apikey=${btc}",
+"https://api.botcahx.live/api/asupan/gheayubi?apikey=${btc}",
+"https://api.botcahx.live/api/asupan/gheayubi?apikey=${btc}",
+"https://api.botcahx.live/api/asupan/euni?apikey=${btc}"
+]
