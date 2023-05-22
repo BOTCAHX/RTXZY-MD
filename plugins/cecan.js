20138980542e29c2fb5118f0116b4b012eb1c152
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2023-05-22 12:13:45 +0700

    Update cecan.js

--- plugins/cecan.js
+++ plugins/cecan.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command }) => {
-let buffer = await fetch(`https://api.betabotz.me/api/cecan/${command}?apikey=${lann}`).then(res => res.buffer())
+let buffer = await fetch(`https://api.betabotz.org/api/cecan/${command}?apikey=${lann}`).then(res => res.buffer())
 conn.sendFile(m.chat, buffer, 'hasil.jpg', "Random " + command, m)
 }
 handler.command = handler.help = ['china', 'indonesia', 'japan', 'vietnam', 'thailand', 'korea', 'malaysia']
