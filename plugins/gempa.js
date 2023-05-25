e3d91f66522f515e624c60ca2f18d3f860094278
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-05-26 01:35:54 +0700

    Update gempa.js

--- plugins/gempa.js
+++ plugins/gempa.js
@@ -1,27 +1,24 @@
-var fetch = require("node-fetch");
-const link = 'https://data.bmkg.go.id/DataMKG/TEWS/'
-var handler = async (m, {
-  conn,
-  text, 
-  usedPrefix, 
-  command 
-  }) => {
-	try {
-		let res = await fetch(link+'autogempa.json')
-		let anu = await res.json()
-		anu = anu.Infogempa.gempa
-		let txt = `*${anu.Wilayah}*\n\n`
-		txt += `Tanggal : ${anu.Tanggal}\n`
-		txt += `Waktu : ${anu.Jam}\n`
-		txt += `Potensi : *${anu.Potensi}*\n\n`
-		txt += `Magnitude : ${anu.Magnitude}\n`
-		txt += `Kedalaman : ${anu.Kedalaman}\n`
-		txt += `Koordinat : ${anu.Coordinates}${anu.Dirasakan.length > 3 ? `\nDirasakan : ${anu.Dirasakan}` : ''}`
-		await conn.sendMessage(m.chat, { image: { url: link+anu.Shakemap }, caption: txt }, { quoted: m })
-	} catch (e) {
-		console.log(e)
-		m.reply(`[!] Fitur Error.`)
-	}
+var axios = require('axios');
+var handler = async (m, { conn }) => {
+try {
+var dataGempa = [];
+  var response = await axios.get(`https://api.botcahx.live/api/search/gempa?apikey=${btc}`);
+  var res = response.data;
+  var gambar = res.result.Map;
+  dataGempa.push({
+    waktu: res.result.Waktu,
+    lintang: res.result.Lintang,
+    bujur: res.result.Bujur,
+    magnitude: res.result.Magnitudo,
+    kedalaman: res.result.Kedalaman,
+    wilayah: res.result.Wilayah
+  });
+  var caption = `Waktu : ${dataGempa[0].waktu}\nLintang : ${dataGempa[0].lintang}\nBujur : ${dataGempa[0].bujur}\nMagnitude : ${dataGempa[0].magnitude}\nKedalaman : ${dataGempa[0].kedalaman}\nWilayah : ${dataGempa[0].wilayah}`;
+  conn.sendFile(m.chat, gambar, 'map.png', caption, m);
+} catch(e) {
+  console.log(e);
+  conn.reply(m.chat, 'Terjadi kesalahan saat mengambil data gempa', m);
+}
 };
 handler.command = handler.help = ['infogempa', 'gempa'];
 handler.tags = ['information'];
