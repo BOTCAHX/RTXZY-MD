533958c987391e4bc1757ddca55d011c58d15e40
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-05-26 23:39:28 +0700

    Create image-anime.js

---
+++ plugins/image-anime.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+var handler = async (m, { conn, args, usedPrefix, command }) => {
+    m.reply(wait);
+    let url = `https://api.botcahx.live/api/anime/${command}?apikey=${btc}`;
+    let response = await axios.get(url, { responseType: 'arraybuffer' });
+    conn.sendFile(m.chat, response.data, "", "", m);
+}
+handler.help = ['umaru','kaneki','megumin','yotsuba','shinomiya','yumeko','tejina','chiho','toukachan','akira','itori','kurumi','sagiri','eba','deidara','itachi','madara','asuna','ayuzawa','chitoge','emilia','hestia','inori','ana','miku','kaori','shizuka','doraemon','kaga','koturi','mikasa','akiyama','gremory','isuzu','shina','kagura','shinka','tsunade','sasuke','sakura','rize','nezuko','boruto','naruto','erza','minato','elaina','yuri','shota','waifu','loli','hinata']
+handler.command = /^(umaru|kaneki|megumin|yotsuba|shinomiya|yumeko|tejina|chiho|toukachan|akira|itori|kurumi|sagiri|eba|deidara|itachi|madara|asuna|ayuzawa|chitoge|emilia|hestia|inori|ana|miku|kaori|shizuka|doraemon|kaga|koturi|mikasa|akiyama|gremory|isuzu|shina|kagura|shinka|tsunade|sasuke|sakura|rize|nezuko|boruto|naruto|erza|minato|elaina|yuri|shota|waifu|loli|hinata)$/i
+handler.tags = ['image']
+handler.limit = true;
+module.exports = handler;
