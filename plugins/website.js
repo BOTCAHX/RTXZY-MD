fb8413162de6ca23b2f968454bed0dd283dbd71a
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-06-09 13:47:00 +0700

    Update website.js

--- plugins/website.js
+++ plugins/website.js
@@ -3,8 +3,8 @@ let handler = async m => {
 let krtu = `web`
 m.reply(`
 > https://api.botcahx.live
-> https://ytdl.tiodevhost.my.id
 > https://tiodevhost.my.id
+
 `.trim()) 
 }
 handler.command = /^(web)$/i
