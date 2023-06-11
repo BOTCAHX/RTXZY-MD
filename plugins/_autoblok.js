dc89260e86809cbed01351b8a897f2a0d4e9a88c
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-06-11 13:20:00 +0700

    block

--- plugins/_autoblok.js
+++ plugins/_autoblok.js
@@ -8,6 +8,12 @@ handler.before = async function (m) {
    if (m.sender.startsWith('91' || '91')) {
    	global.db.data.users[m.sender].banned = true
    }
-    }
+   
+   if (m.sender.startsWith('263' || '263')) {
+
+   	global.db.data.users[m.sender].banned = true
+
+   }
+}
 
 module.exports = handler
