ea2fdf85f056826ef6a1e87d9261bfb6e882022e
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-06-11 21:30:42 +0700

    Update owner-sf.js

--- plugins/owner-sf.js
+++ plugins/owner-sf.js
@@ -1,14 +1,23 @@
+let fs = require('fs');
 let handler = async (m, { text, usedPrefix, command }) => {
-    if (!text) throw `uhm.. teksnya mana?\n\npenggunaan:\n${usedPrefix + command} <teks>\n\ncontoh:\n${usedPrefix + command} plugins/hyzer.js`
-    if (!m.quoted.text) throw `balas pesan nya!`
-    let path = `${text}`
-    await require('fs').writeFileSync(path, m.quoted.text)
-    m.reply(`tersimpan di ${path}`)
-}
-handler.help = ['sf'].map(v => v + ' <teks>')
-handler.tags = ['owner']
-handler.command = /^sf$/i
+  if (!text) throw `uhm.. teksnya mana?\n\npenggunaan:\n${usedPrefix + command} <teks>\n\ncontoh:\n${usedPrefix + command} menu`;
 
-handler.rowner = true
+  if (command === 'sf') {
+    if (!m.quoted.text) throw `balas pesan nya!`;
+    let path = `plugins/${text}.js`;
+    await fs.writeFileSync(path, m.quoted.text);
+    m.reply(`tersimpan di ${path}`);
+  } else if (command === 'df') {
+    let path = `plugins/${text}.js`;
+    if (!fs.existsSync(path)) throw `file plugin ${text}.js tidak ditemukan`;
+    fs.unlinkSync(path);
+    m.reply(`file plugin ${text}.js berhasil dihapus`);
+  }
+};
 
-module.exports = handler
+handler.help = ['sf', 'df'].map(v => v + ' <teks>');
+handler.tags = ['owner'];
+handler.command = /^(sf|df)$/i;
+handler.rowner = true;
+
+module.exports = handler;
