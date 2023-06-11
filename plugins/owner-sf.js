7eced21d8593340977a3c671a1bff706230920a3
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-06-12 01:40:30 +0700

    Update owner-sf.js

--- plugins/owner-sf.js
+++ plugins/owner-sf.js
@@ -1,5 +1,5 @@
 let handler = async (m, { text, usedPrefix, command }) => {
-    if (!text) throw `uhm.. teksnya mana?\n\npenggunaan:\n${usedPrefix + command} <teks>\n\ncontoh:\n${usedPrefix + command} plugins/hyzer.js`
+    if (!text) throw `uhm.. teksnya mana?\n\npenggunaan:\n${usedPrefix + command} <teks>\n\ncontoh:\n${usedPrefix + command} plugins/menu.js`
     if (!m.quoted.text) throw `balas pesan nya!`
     let path = `${text}`
     await require('fs').writeFileSync(path, m.quoted.text)
