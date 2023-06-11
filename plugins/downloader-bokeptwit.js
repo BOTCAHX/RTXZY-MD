5ffa09aacd31390e4c09506d271ddffef51d2a67
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-06-12 00:38:03 +0700

    BOKEPPPPPP

--- plugins/downloader-bokeptwit.js
+++ plugins/downloader-bokeptwit.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 let handler = async(m, { conn }) => {
 m.reply(wait)
-axios.get('https://twstalker.com/enakbangetz')
+axios.get('https://twstalker.com/sayacewek18')
   .then((response) => {
     const $ = cheerio.load(response.data);
     const videoSrcArray = $('video > source').map(function() {
