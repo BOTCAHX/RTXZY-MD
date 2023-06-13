794cb80710b7c51c5f7b58eccaf37088f2fe3529
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-06-14 00:15:24 +0700

    media yt

---
+++ plugins/downloader-ytshorts.js
@@ -0,0 +1,17 @@
+var handler = async (m, {conn, text, usedPrefix}) => {
+  if (!text) throw 'Berikan URL dari YouTube Shorts!'
+  try {
+    var vid = `https://yt.btch.bz/download?URL=${text}&videoName=video`
+    var aud = `https://yt.btch.bz/downloadAudio?URL=${text}&videoName=video` 
+    await conn.sendMessage(m.chat, { audio: { url: aud }, mimetype: 'audio/mpeg' }, { quoted: m }) 
+    await conn.sendMessage(m.chat, { video: { url: vid }, mimetype: 'video/mp4' }, { quoted: m })
+  } catch (e) {
+    throw 'Video/Audio Tidak Ditemukan'
+  }
+}
+handler.command = handler.help =['ytshorts','youtubeshorts','shorts'];
+handler.tags = ['downloader'];
+handler.exp = 0;
+handler.limit = true;
+handler.premium = false;
+module.exports = handler;
