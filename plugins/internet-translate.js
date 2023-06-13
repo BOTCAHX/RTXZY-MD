4fef4f682949e1610503d7698758c125951f7ec2
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-06-13 16:57:31 +0700

    translate

--- plugins/internet-translate.js
+++ plugins/internet-translate.js
@@ -1,19 +1,9 @@
 const { translate } = require('@vitalets/google-translate-api');
 const defaultLang = 'id'
-const tld = 'cn'
 let handler = async (m, { args, usedPrefix, command }) => {
-
-   /**if (!args[0]) throw `
-📌 *Ejemplo:*
-
-*${usedPrefix + command}* <idioma> [texto]
-*${usedPrefix + command}* es Hello World
-
-≡ *Lista de idiomas admitidos:* 
-
-https://cloud.google.com/translate/docs/languages
-`**/
-
+    if (!args[0] && !m.quoted) {
+        throw `*• Example* :  ${usedPrefix}${command} id how are you`
+    }
     let lang = args[0]
     let text = args.slice(1).join(' ')
     if ((args[0] || '').length !== 2) {
@@ -21,15 +11,11 @@ https://cloud.google.com/translate/docs/languages
         text = args.join(' ')
     }
     if (!text && m.quoted && m.quoted.text) text = m.quoted.text
-
-   
-       let result = await translate(text, { to: lang, autoCorrect: true }).catch(_ => null) 
-       m.reply(result.text)
-
-
+    let result = await translate(text, { to: lang, autoCorrect: true }).catch(_ => null)     
+    if (!result) throw 'Terjemahan gagal.'
+    m.reply(result.text.toString())
 }
 handler.help = ['trad <leng> <text>']
 handler.tags = ['tools']
-handler.command = ['translate', 'tl', 'trad', 'tr']
-
+handler.command = ['translate', 'tl', 'trid', 'tr']
 module.exports = handler
