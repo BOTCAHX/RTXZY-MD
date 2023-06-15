f04d1c44427cc576d506258867358c0b89a02472
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-06-15 15:57:11 +0700

    biar gak double send :v

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,31 +1,42 @@
-var fetch = require("node-fetch");
-var handler = async (m, {
-	conn,
-	args,
-	usedPrefix,
-	command
-}) => {
-if (!args[0]) throw `Masukan URL!\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
-if (!args[0].match(/tiktok/gi)) throw `URL Tidak Ditemukan!`
-m.reply(wait)
- var apis = await fetch(`https://api.botcahx.live/api/dowloader/tiktok?url=${args[0]}&apikey=${btc}`)
-if (!apis.ok) throw await apis.text()
-var jsons = await apis.json()
-if (!jsons.status) throw jsons
-var { 
-video, 
-video2, 
-username,
-description,
-audio
-} = jsons.result
-await conn.sendFile(m.chat, video, 'tiktok.mp4', `
-*Deskripsi*: ${description}
-\n*Username*: ${username}`, m)
-conn.sendFile(m.chat, audio, 'tikmp3.opus', null, m)
+const https = require('https');
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) {
+      throw `Masukan URL!\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
+    }
+try {
+    if (!args[0].match(/tiktok/gi)) {
+      throw `URL Tidak Ditemukan!`
+    }
+    m.reply('*Mohon tunggu..*')
+    const api = await https.get(`https://api.botcahx.live/api/dowloader/tiktok?url=${args[0]}&apikey=${btc}`, response => {
+      let data = '';
+      response.on('data', chunk => {
+        data += chunk;
+      });
+      response.on('end', async () => {
+        const res = JSON.parse(data);
+        var { 
+          video, 
+          video2, 
+          username,
+          description,
+          audio
+        } = res.result
+        conn.sendFile(m.chat, video, null, `Username: ${username}\nDeskripsi: ${description}\nAudio: ${audio}`, m);
+      });
+    }).on('error', error => {
+      console.log(error);
+      throw error.message
+    });
+  } catch (e) {
+    console.log(e)
+    throw `Terjadi Kesalahan!`
+  }
 };
-handler.help = ['tiktok']
-handler.command = /^(tiktok|tt|tiktokdl|tiktoknowm|dltt)$/i
+
+handler.help = ['tiktok'];
+handler.command = /^(tiktok|tt|tiktokdl|tiktoknowm)$/i
 handler.tags = ['downloader'];
 handler.limit = true;
 handler.group = false;
@@ -35,4 +46,5 @@ handler.admin = false;
 handler.botAdmin = false;
 handler.fail = null;
 handler.private = false;
+
 module.exports = handler;
