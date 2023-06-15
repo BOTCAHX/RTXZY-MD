49af3abf249c233d1b4673b00ba2931ac4f0f253
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-06-15 23:32:29 +0700

    + cecan

--- plugins/cecan.js
+++ plugins/cecan.js
@@ -1,8 +1,19 @@
 let fetch = require('node-fetch')
+
 let handler = async (m, { conn, command }) => {
-let buffer = await fetch(`https://api.betabotz.org/api/cecan/${command}?apikey=${lann}`).then(res => res.buffer())
-conn.sendFile(m.chat, buffer, 'hasil.jpg', "Random " + command, m)
+    let api1 = `https://api.botcahx.live/api/cecan/${command}?apikey=${btc}`
+    let api2 = `https://api.betabotz.org/api/cecan/${command}?apikey=${lann}`
+    let buffer = await fetch(api1)
+        .then(res => res.buffer())
+        .catch(async (err) => {
+            console.log(`API 1 failed with error: ${err}. Trying API 2...`)
+            buffer = await fetch(api2).then(res => res.buffer())
+            return buffer
+        })
+    conn.sendFile(m.chat, buffer, 'hasil.jpg', `Random ${command}`, m)
 }
-handler.command = handler.help = ['china', 'indonesia', 'japan', 'vietnam', 'thailand', 'korea', 'malaysia']
-handler.tags = ['internet']
+
+handler.command = ['china','vietnam','thailand','indonesia','korea','japan','malaysia','justinaxie','jeni','jiso','ryujin','rose','hijaber']
+handler.tags = ['downloader']
+
 module.exports = handler
