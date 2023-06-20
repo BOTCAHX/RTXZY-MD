dd7a74473e114acf896a3475f2a1559ef8e56d4d
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-06-20 23:14:04 +0700

    Update info-speedtest.js

--- plugins/info-speedtest.js
+++ plugins/info-speedtest.js
@@ -11,7 +11,20 @@ let handler = async (m, { conn}) => {
     } finally {
         let { stdout, stderr } = o
         if (stdout.trim()) 
-        conn.sendMessage(m.chat,{ image :{ url : "https://telegra.ph/file/ec8cf04e3a2890d3dce9c.jpg" } , caption : stdout }, { quoted: m })
+        conn.relayMessage(m.chat, {
+extendedTextMessage:{
+                text: stdout, 
+                contextInfo: {
+                     externalAdReply: {
+                        title: "",
+                        mediaType: 1,
+                        previewType: 0,
+                        renderLargerThumbnail: true,
+                        thumbnailUrl: 'https://telegra.ph/file/ec8cf04e3a2890d3dce9c.jpg',
+                        sourceUrl: ''
+                    }
+                }, mentions: [m.sender]
+}}, {})
         if (stderr.trim()) m.reply(stderr)
     }
 }
