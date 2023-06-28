264e46750b3116e4c30e64511b80a4937cf0ae7c
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-06-28 20:31:53 +0700

    Update _openai-ai.js

--- plugins/_openai-ai.js
+++ plugins/_openai-ai.js
@@ -5,12 +5,17 @@ var handler = async (m, {
  command
  }) => {
 if (!text) throw `Masukkan pertanyaan!\n\n*Contoh:* Siapa presiden Indonesia? `
-await m.reply(wait)
+try {
+  await m.reply(wait)
   var apii = await fetch(`https://api.botcahx.live/api/search/openai-chat?text=${text}&apikey=${btc}`)
   var res = await apii.json()
   await m.reply(res.message)
-}      
+} catch (err) {
+  console.error(err)
+  throw "Terjadi kesalahan dalam menjawab pertanyaan"
+}
+}
 handler.command = handler.help = ['ai','openai','chatgpt'];
-handler.tags = ['tools'];
+handler.tags = ['info'];
 handler.premium = false
 module.exports = handler;
