57009e595c2fcbce0acd4d91eeee31a2b7fd5093
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-07-06 02:07:40 +0700

    sewabot

--- plugins/info-sewa.js
+++ plugins/info-sewa.js
@@ -1,21 +1,10 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-  
-var caption = `Hi, Silahkan pilih opsi di bawah ini.\n*Payment*\n\n*x* Dana: 082221792667\n*x* Pulsa: 082221792667`
-conn.relayMessage(m.chat, 
-{ liveLocationMessage: {
-  degreesLatitude: 35.685506276233525,
-  degreesLongitude: 139.75270667105852,
-  accuracyInMeters: 0,
-degreesClockwiseFromMagneticNorth: 2,
-caption: caption,
-sequenceNumber: 2,
-timeOffset: 3,
-contextInfo: m,
-}}, {})
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, command }) => {
+    let buffer = await fetch(`https://telegra.ph/file/1c53a85e78e81e2aaad9c.jpg`).then(res => res.buffer())
+    conn.sendFile(m.chat, buffer, 'hasil.jpg', `*Jika telah melakukan pembayaran silahkan kirimkan bukti pembayaran ke WhatsApp Admin*\n*Nomor:* https://wa.me/6282221792667`, m)
 }
 
-handler.help = ['sewa']
+handler.help = handler.command = ['donasi','donate','sewa','sewabot','belibot']
 handler.tags = ['main']
-handler.command = /^(sewa|sewabot|belibot)$/i
-
 module.exports = handler
