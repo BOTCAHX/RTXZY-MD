2cab12802b1a025bb01bae160d827c3e950742e5
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-07-06 02:44:24 +0700

    update

--- config.js
+++ config.js
@@ -1,24 +1,16 @@
-/**Penjelasan: Untuk menggunakan script ini, kamu akan perlu mengganti nomor owner dan wm terlebih dahulu. Kamu juga harus memiliki api key dari kedua website ini https://api.botcahx.live dan https://api.betabotz.org. Setelah memiliki api key, kamu bisa menggantinya dengan menyalin key tersebut dari profilmu dan paste di variabel global.btc. Contohnya, global.btc = 'xzRhejka'. Setelah itu, kamu bisa melanjutkan dengan mengubah nomor owner dan wm.
-**/
-
-
 global.owner = ['6281395861695']  
 global.mods = ['6281395861695'] 
 global.prems = ['6281395861695']
 global.nameowner = 'Tio'
 global.numberowner = '6281395861695' 
 global.mail = 'support@tioprm.my.id' 
-global.namebot = 'BOTCAHX'
 global.gc = 'https://chat.whatsapp.com/Ln2vHjRrRayAbzalRMB56r'
 global.instagram = 'https://instagram.com/prm2.0'
 global.wm = '© Tio'
-global.watermark = wm
 global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
-global.benar = 'Benar ✅\n'
-global.salah = 'Salah ❌\n'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
-global.packname = 'Made with'
+global.packname = 'Made With'
 global.author = 'Bot WhatsApp'
 
 //INI WAJIB DI ISI!//
