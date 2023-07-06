1a94ac3abdd5e9d0b16bb34aa432e3c7d268af65
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-07-07 00:32:54 +0700

    remini tools

---
+++ plugins/tools-remini.js
@@ -0,0 +1,29 @@
+const fetch = require('node-fetch');
+const uploadImage = require('../lib/uploadImage.js');
+
+async function handler(m, { conn, usedPrefix, command }) {
+  try {
+    const q = m.quoted ? m.quoted : m;
+    const mime = (q.msg || q).mimetype || q.mediaType || '';
+    if (/^image/.test(mime) && !/webp/.test(mime)) {
+      const img = await q.download();
+      const out = await uploadImage(img);
+      const api = await fetch(`https://api.botcahx.live/api/tools/remini?url=${out}&apikey=${btc}`);
+       const image = await api.buffer();
+       conn.sendFile(m.chat, image, 'remini.jpg', wm, m);
+    } else {
+      m.reply(`Kirim gambar dengan caption *${usedPrefix + command}* atau tag gambar yang sudah dikirim.`);
+    }
+  } catch (e) {
+    console.error(e);
+    m.reply(`Identifikasi gagal. Silakan coba lagi.`);
+  }
+}
+
+handler.help = ['remini'];
+handler.tags = ['tools'];
+handler.command = ['remini'];
+handler.premium = false;
+handler.limit = false;
+
+module.exports = handler;
