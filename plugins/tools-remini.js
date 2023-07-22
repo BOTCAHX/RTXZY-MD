911f50cf5170511ce3073585879d1b73dfc07117
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-07-22 15:45:08 +0700

    update remini result

--- plugins/tools-remini.js
+++ plugins/tools-remini.js
@@ -9,8 +9,9 @@ async function handler(m, { conn, usedPrefix, command }) {
       const img = await q.download();
       const out = await uploadImage(img);
       const api = await fetch(`https://api.botcahx.live/api/tools/remini?url=${out}&apikey=${btc}`);
-       const image = await api.buffer();
-       conn.sendFile(m.chat, image, 'remini.jpg', wm, m);
+      const image = await api.json();
+      const { url } = image 
+       conn.sendFile(m.chat, url, null, wm, m);
     } else {
       m.reply(`Kirim gambar dengan caption *${usedPrefix + command}* atau tag gambar yang sudah dikirim.`);
     }
