e6de4b75ecb0ec09c278a1ba415d6111fe84ca56
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-07-29 07:58:51 +0700

    replace module yt search

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -1,61 +1,57 @@
-var { youtubeSearch } = require('@bochilteam/scraper');
+const youtube = require("yt-search");
+const fetch = require('node-fetch');
+const FormData = require('form-data');
+const { fromBuffer } = require('file-type');
+
 var handler = async (m, {
     conn,
     text,
     usedPrefix
 }) => {
-    if (!text) throw 'Enter Title / Link From YouTube!'
+    if (!text) throw 'Enter Title / Link From YouTube!';
     try {
-        var vid = (await youtubeSearch(text)).video[0]
-        if (!vid) throw 'Video/Audio Tidak Ditemukan'
-        var {
-            title,
-            description,
-            thumbnail,
-            videoId,
-            durationH,
-            durationS,
-            viewH,
-            publishedTime
-        } = vid
-        if (durationS >= 3600) { 
-            m.reply('Video is longer than 1 hour!')
+        var search = await youtube(text);
+        var convert = search.videos[0];
+        if (!search) throw 'Video/Audio Tidak Ditemukan';
+        if (convert.seconds >= 3600) {
+            return conn.reply(m.chat, 'Video is longer than 1 hour!', m);
         } else {
-            var url = 'https://www.youtube.com/watch?v=' + videoId
-            var cvr
+            var audioUrl
             try {
-                cvr = `https://yt.tioo.eu.org/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mp3`
+                audioUrl = `https://yt.btch.bz/downloadAudio?URL=${convert.url}&videoName=mp3`
             } catch (e) {
                 conn.reply(m.chat, wait, m)
-                cvr = `https://yt.btch.bz/downloadAudio?URL=${url}&videoName=music`
-            }
-            var tmb = thumbnail
-            var captionvid = `∘ Title: ${title}\n∘ Published: ${publishedTime}\n∘ Duration: ${durationH}\n∘ Second: ${durationS}\n∘ Views: ${viewH}\n∘ Url:  ${url}\n∘ Description: ${description}`
+                audioUrl = `https://yt.tioo.eu.org/?url=${convert.url}&filter=audioonly&quality=highestaudio&contenttype=audio/mp3`
+            } 
+            var build = await fetch(convert.image);
+            var buffer = await build.buffer();
+            var image = await uploader(buffer);
+            var caption = `∘ Title : ${convert.title}\n∘ Ext : Search\n∘ ID : ${convert.videoId}\n∘ Duration : ${convert.timestamp}\n∘ Viewers : ${convert.views}\n∘ Upload At : ${convert.ago}\n∘ Author : ${convert.author.name}\n∘ Channel : ${convert.author.url}\n∘ Url : ${convert.url}\n∘ Description : ${convert.description}\n∘ Thumbnail : ${image}`;
             var pesan = conn.relayMessage(m.chat, {
                 extendedTextMessage:{
-                text: captionvid, 
+                text: caption, 
                 contextInfo: {
                      externalAdReply: {
                         title: "Powered by",
                         mediaType: 1,
                         previewType: 0,
                         renderLargerThumbnail: true,
-                        thumbnailUrl: tmb,
-                        sourceUrl: cvr
+                        thumbnailUrl: image,
+                        sourceUrl: audioUrl
                     }
                 }, mentions: [m.sender]
-}}, {})
- conn.sendMessage(m.chat, {
+                }}, {})
+            conn.sendMessage(m.chat, {
                 audio: {
-                    url: cvr
+                    url: audioUrl
                 },
                 mimetype: 'audio/mpeg',
                 contextInfo: {
                     externalAdReply: {
-                        title: title,
+                        title: convert.title,
                         body: "",
-                        thumbnailUrl: tmb,
-                        sourceUrl: url,
+                        thumbnailUrl: image,
+                        sourceUrl: audioUrl,
                         mediaType: 1,
                         showAdAttribution: true,
                         renderLargerThumbnail: true
@@ -63,15 +59,29 @@ var handler = async (m, {
                 }
             }, {
                 quoted: m
-            })
+            });
         }
-    } catch (e) { 
-       conn.reply(m.chat, `*Error:* ` + eror, m)
+    } catch (e) {
+        conn.reply(m.chat, `*Error:* ` + e, m);
     }
-}
-handler.command = handler.help = ['play', 'song', 'ds', 'ytmp3','yta'];
+};
+
+handler.command = handler.help = ['play', 'song', 'ds', 'ytmp3', 'yta'];
 handler.tags = ['downloader'];
 handler.exp = 0;
 handler.limit = true;
 handler.premium = false;
 module.exports = handler;
+
+async function uploader(buffer) {
+  const { ext } = await fromBuffer(buffer);
+  let form = new FormData();
+  form.append('file', buffer, 'tmp.' + ext);
+  let res = await fetch('https://cdn.btch.bz/upload', {
+    method: 'POST',
+    body: form
+  });
+  let img = await res.json();
+  if (img.error) throw img.error;
+  return 'https://cdn.btch.bz' + img[0].src;
+}
