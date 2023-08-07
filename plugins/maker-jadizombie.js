1cb90feecf6f7a8a58d0c226226af79852711c08
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-08-07 23:00:08 +0700

    fix jadizombie

--- plugins/maker-jadizombie.js
+++ plugins/maker-jadizombie.js
@@ -1,6 +1,5 @@
+const uploadImage = require('../lib/uploadImage');
 const fetch = require("node-fetch");
-const FormData = require('form-data');
-const { fromBuffer } = require('file-type');
 let handler = async (m, { 
 conn, 
 usedPrefix, 
@@ -12,7 +11,7 @@ command
     await conn.reply(m.chat, wait, m)
 		try {
 			const img = await q.download?.()
-			let out = await uploader(img)
+			let out = await uploadImage(img)
 			let old = new Date()
 			let res = await fetch(`https://api.botcahx.live/api/maker/jadizombie?url=${out}&apikey=${btc}`)
 			let convert = await res.json()
@@ -33,16 +32,3 @@ handler.tags = ['maker'];
 handler.premium = false;
 handler.limit = 5;
 module.exports = handler;
-
-async function uploader(buffer) {
-  const { ext } = await fromBuffer(buffer);
-  let form = new FormData();
-  form.append('file', buffer, 'tmp.' + ext);
-  let res = await fetch('https://cdn.btch.bz/upload', {
-    method: 'POST',
-    body: form
-  });
-  let img = await res.json();
-  if (img.error) throw img.error;
-  return 'https://cdn.btch.bz' + img[0].src;
-}
