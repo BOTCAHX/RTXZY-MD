9aafc77b050389aa136ea70fe5729fd9e93327ec
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-08-07 12:24:56 +0700

    yt url

--- plugins/downloader-ytshorts.js
+++ plugins/downloader-ytshorts.js
@@ -1,8 +1,8 @@
 var handler = async (m, {conn, text, usedPrefix}) => {
   if (!text) throw 'Berikan URL dari YouTube Shorts!'
   try {
-    var vid = `https://yt.btch.bz/download?URL=${text}&videoName=video`
-    var aud = `https://yt.btch.bz/downloadAudio?URL=${text}&videoName=video` 
+    var vid = `https://yt.tioo.eu.org/?url=${text}&filter=audioandvideo&quality=highestvideo&contenttype=video/mp4`
+    var aud = `https://yt.tioo.eu.org/?url=${text}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg` 
     await conn.sendMessage(m.chat, { audio: { url: aud }, mimetype: 'audio/mpeg' }, { quoted: m }) 
     await conn.sendMessage(m.chat, { video: { url: vid }, mimetype: 'video/mp4' }, { quoted: m })
   } catch (e) {
