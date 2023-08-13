9863b1427ddb2a84cbf0d57c8d3fee41e3b9fd46
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-08-13 19:32:40 +0700

    fix simsimi

--- plugins/fun-simsimi.js
+++ plugins/fun-simsimi.js
@@ -1,25 +1,11 @@
-/*let fetch = require('node-fetch')
-let handler = async (m, { text }) => {
-  let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "id" }, ''))
-  let json = await res.json()
-  if (json.success) m.reply(json.success)
-  else throw json
-}
-handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
-handler.tags = ['fun']
-handler.command = /^((sim)?simi|simih)$/i
-
-module.exports = handler*/
-
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  let res = await fetch(`http://api.brainshop.ai/get?bid=175444&key=DxrtWTGGOUvgKqya&uid=175444&msg=${encodeURIComponent(text)}`)
+  let res = await fetch(`https://api.botcahx.live/api/search/simsimi?query=${encodeURIComponent(text)}&apikey=${btc}`)
   let json = await res.json()
-  m.reply(json.cnt)
+  m.reply(json.result.success)
 }
 handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
 handler.tags = ['fun']
 handler.command = /^((sim)?simi|simih)$/i
 
 module.exports = handler
-
