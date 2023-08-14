d0c72198255c78449f9e51dfae0d0415f4ef85b8
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-08-14 23:21:58 +0700

    update YouTube video downloader

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -1,74 +1,46 @@
-var { youtubeSearch } = require('@bochilteam/scraper');
-var fetch = require('node-fetch')
-var handler = async (m, {
+const youtube = require("yt-search");
+let handler = async (m, {
     conn,
     text,
     usedPrefix
 }) => {
-    if (!text) throw 'Enter Title / Link From YouTube!'
+    if (!text) throw 'Enter Title / Link From YouTube!';
     try {
-        var vid = (await youtubeSearch(text)).video[0]
-        if (!vid) throw 'Video/Audio Tidak Ditemukan'
-        var {
-            title,
-            description,
-            thumbnail,
-            videoId,
-            durationH,
-            durationS,
-            viewH,
-            publishedTime
-        } = vid
-        if (durationS >= 3600) { 
-            m.reply('Video is longer than 1 hour!')
+        var search = await youtube(text);
+        var convert = search.videos[0];
+        if (!convert) throw 'Video/Audio Tidak Ditemukan';
+        if (convert.seconds >= 3600) {
+            return conn.reply(m.chat, 'Video is longer than 1 hour!', m);
         } else {
-            var url = 'https://www.youtube.com/watch?v=' + videoId
-            var cvr
+            var videoUrl
             try {
-                cvr = await fetch(`https://yt.nxr.my.id/yt2?url=${url}&type=video`)
+                videoUrl = `https://yt.tioo.eu.org/?url=${convert.url}&filter=audioandvideo&quality=highestvideo&contenttype=video/mp4`
             } catch (e) {
                 conn.reply(m.chat, wait, m)
-                cvr = await fetch(`https://yt.nxr.my.id/yt2?url=${url}&type=video`)
-            }
-            var sce = await cvr.json()
-            var mcs = sce.data.url
-            var tmb = thumbnail
-            var captionvid = `∘ Title: ${title}\n∘ Published: ${publishedTime}\n∘ Duration: ${durationH}\n∘ Second: ${durationS}\n∘ Views: ${viewH}\n∘ Url:  ${url}\n∘ Description: ${description}`
+                videoUrl = `https://yt.tioo.eu.org/?url=${convert.url}&filter=audioandvideo&quality=highestvideo&contenttype=video/mp4`
+            }             
+            var caption = `∘ Title : ${convert.title}\n∘ Ext : Search\n∘ ID : ${convert.videoId}\n∘ Duration : ${convert.timestamp}\n∘ Viewers : ${convert.views}\n∘ Upload At : ${convert.ago}\n∘ Author : ${convert.author.name}\n∘ Channel : ${convert.author.url}\n∘ Url : ${convert.url}\n∘ Description : ${convert.description}\n∘ Thumbnail : ${convert.image}`;
             var pesan = conn.relayMessage(m.chat, {
                 extendedTextMessage:{
-                text: captionvid, 
+                text: caption, 
                 contextInfo: {
                      externalAdReply: {
-                        title: "Powered by",
+                        title: "Powered by ",
                         mediaType: 1,
                         previewType: 0,
                         renderLargerThumbnail: true,
-                        thumbnailUrl: tmb,
-                        sourceUrl: mcs
+                        thumbnailUrl: convert.image,
+                        sourceUrl: videoUrl
                     }
                 }, mentions: [m.sender]
-}}, {})
- conn.sendMessage(m.chat, {
-      video: {
-        url: mcs,
-        mimetype: 'video/webm',
-        attributes: [
-          {
-            name: 'controls',
-            value: 'true'
-          },
-          {
-            name: 'autoplay',
-            value: 'true'
-          }
-        ]
-      }
-    }, { quoted: m })
+                }}, {})
+                conn.sendMessage(m.chat, { video: { url: videoUrl }, mimetype: 'video/mp4' }, { quoted: m })
         }
-    } catch (e) { 
-       conn.reply(m.chat, `*Error:* ` + eror, m)
+    } catch (e) {
+        conn.reply(m.chat, `*Error:* ` + e, m);
     }
-}
+};
+
 handler.command = handler.help = ['ytv','ytmp4'];
 handler.tags = ['downloader'];
 handler.exp = 0;
