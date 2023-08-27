2ab77bad52b979f5d8b26d094b081804f3016b7b
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-08-27 11:37:48 +0700

    memilih random dengan array

--- plugins/downloader-twitter.js
+++ plugins/downloader-twitter.js
@@ -7,12 +7,12 @@ var handler = async (m, {
 }) => {
 if (!args[0]) throw `Masukan URL!\n\ncontoh:\n${usedPrefix + command} https://twitter.com/gofoodindonesia/status/1229369819511709697`
 if (!args[0].match(/twitter/gi)) throw `URL Tidak Ditemukan!`
-m.reply('wait')
+m.reply(wait)
 try {
         const api = await fetch(`https://api.botcahx.live/api/dowloader/twitter?url=${args[0]}&apikey=${btc}`)
         const res = await api.json()
-        for (let i of res.result) {
-            conn.sendFile(m.chat, i.url, null, `*Twitter Downloader*`, m)
+        for (let i of res.result.url) {
+            conn.sendFile(m.chat, i.hd, null, `*Twitter Downloader*`, m)
         }
     } catch (e) {
         throw `*Server Down!*`
