4d4e78d6e9f3a5895f59697aa4c072e0c3d75a1e
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-08-30 15:34:54 +0700

    add ulang

---
+++ plugins/sticker-emoji.js
@@ -0,0 +1,107 @@
+var { 
+sticker5 
+} = require('../lib/sticker')
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+  if (!text) throw `🚩 *Contoh:* ${usedPrefix + command} 🗿`
+     await conn.reply(m.chat, wait, m)
+     try {
+        if (command == 'stikapple') {
+        const res = `https://api.botcahx.live/api/emoji/apple?emoji=${encodeURIComponent(text)}&apikey=${btc}`
+            var stiker = await sticker5(res, { packname })
+            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+        }
+        else if (command == 'stikgoogle') {
+        const res = `https://api.botcahx.live/api/emoji/google?emoji=${encodeURIComponent(text)}&apikey=${btc}`
+            var stiker = await sticker5(res, { packname })
+            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+        }
+        else if (command == 'stiksamsung') {
+        const res = `https://api.botcahx.live/api/emoji/samsung?emoji=${encodeURIComponent(text)}&apikey=${btc}`
+            var stiker = await sticker5(res, { packname })
+            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+        }
+        else if (command == 'stikmicrosoft') {
+        const res = `https://api.botcahx.live/api/emoji/microsoft?emoji=${encodeURIComponent(text)}&apikey=${btc}`
+            var stiker = await sticker5(res, { packname })
+            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+        }
+        else if (command == 'stikwhatsapp') {
+        const res = `https://api.botcahx.live/api/emoji/whatsapp?emoji=${encodeURIComponent(text)}&apikey=${btc}`
+            var stiker = await sticker5(res, { packname })
+            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+        }
+        else if (command == 'stiktwitter') {
+        const res = `https://api.botcahx.live/api/emoji/twitter?emoji=${encodeURIComponent(text)}&apikey=${btc}`
+            var stiker = await sticker5(res, { packname })
+            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+        }
+        else if (command == 'stikfacebook') {
+        const res = `https://api.botcahx.live/api/emoji/facebook?emoji=${encodeURIComponent(text)}&apikey=${btc}`
+            var stiker = await sticker5(res, { packname })
+            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+        }
+        else if (command == 'stikskype') {
+        const res = `https://api.botcahx.live/api/emoji/skype?emoji=${encodeURIComponent(text)}&apikey=${btc}`
+            var stiker = await sticker5(res, { packname })
+            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+        }
+        else if (command == 'stikjoypixels') {
+        const res = `https://api.botcahx.live/api/emoji/joypixels?emoji=${encodeURIComponent(text)}&apikey=${btc}`
+            var stiker = await sticker5(res, { packname })
+            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+        }
+        else if (command == 'stikopenmoji') {
+        const res = `https://api.botcahx.live/api/emoji/openmoji?emoji=${encodeURIComponent(text)}&apikey=${btc}`
+            var stiker = await sticker5(res, { packname })
+            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+        }
+        else if (command == 'stikemojipedia') {
+        const res = `https://api.botcahx.live/api/emoji/emojipedia?emoji=${encodeURIComponent(text)}&apikey=${btc}`
+            var stiker = await sticker5(res, { packname })
+            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+        }
+        else if (command == 'stiklg') {
+        const res = `https://api.botcahx.live/api/emoji/lg?emoji=${encodeURIComponent(text)}&apikey=${btc}`
+            var stiker = await sticker5(res, { packname })
+            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+        }
+        else if (command == 'stikjoypixels') {
+        const res = `https://api.botcahx.live/api/emoji/joypixels?emoji=${encodeURIComponent(text)}&apikey=${btc}`
+            var stiker = await sticker5(res, { packname })
+            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+        }
+        else if (command == 'stikhtc') {
+        const res = `https://api.botcahx.live/api/emoji/htc?emoji=${encodeURIComponent(text)}&apikey=${btc}`
+            var stiker = await sticker5(res, { packname })
+            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+        }
+        else if (command == 'stikmozilla') {
+        const res = `https://api.botcahx.live/api/emoji/mozilla?emoji=${encodeURIComponent(text)}&apikey=${btc}`
+            var stiker = await sticker5(res, { packname })
+            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+        }
+        else if (command == 'stiksoftbank') {
+        const res = `https://api.botcahx.live/api/emoji/softbank?emoji=${encodeURIComponent(text)}&apikey=${btc}`
+            var stiker = await sticker5(res, { packname })
+            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+        }
+        else if (command == 'stikdocomo') {
+        const res = `https://api.botcahx.live/api/emoji/docomo?emoji=${encodeURIComponent(text)}&apikey=${btc}`
+            var stiker = await sticker5(res, { packname })
+            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+        }
+        else if (command == 'stikkddi') {
+        const res = `https://api.botcahx.live/api/emoji/kddi?emoji=${encodeURIComponent(text)}&apikey=${btc}`
+            var stiker = await sticker5(res, { packname })
+            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+        }
+    } catch (e) {
+        console.log(e)
+        throw '*🚩 Stiker tidak di temukan!*'
+    }
+}
+
+handler.command = handler.help = ['stikapple', 'stikkddi', 'stikdocomo', 'stiksoftbank', 'stikhtc', 'stikmozilla', 'stiklg', 'stikjoypixels', 'stikopenmoji', 'stikemojipedia', 'stikjoypixels', 'stikopenmoji', 'stikjoypixels', 'stikfacebook', 'stikskype', 'stikwhatsapp', 'stiktwitter', 'stiksamsung', 'stikmicrosoft', 'stikapple', 'stikapple']
+handler.tags = ['sticker'] 
+handler.limit = true
+module.exports = handler
