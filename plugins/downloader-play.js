ae359db549ab51e5fecab622f19c3366b8c0a424
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-09-01 22:11:38 +0700

    kombek

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -1,83 +1,67 @@
-let ytdl = require('ytdl-core');
-let fs = require('fs');
-let ffmpeg = require('fluent-ffmpeg');
-let search = require ('yt-search');
-
-let handler = async (m, { conn, text }) => {
-  if (!text) return m.reply('*example*: .play eula song');
-  try {
-    let results = await search(text);
-    let videoId = results.videos[0].videoId;
-    let info = await ytdl.getInfo(videoId);
-    let title = info.videoDetails.title.replace(/[^\w\s]/gi, '');
-    let thumbnailUrl = `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`;
-    let url = info.videoDetails.video_url;
-    let duration = parseInt(info.videoDetails.lengthSeconds);
-    let uploadDate = new Date(info.videoDetails.publishDate).toLocaleDateString();
-    let views = info.videoDetails.viewCount;
-    let minutes = Math.floor(duration / 60);
-    let description = results.videos[0].description;
-    let seconds = duration % 60;
-    let durationText = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;       
-    let audio = ytdl(videoId, { quality: 'highestaudio' });
-    let inputFilePath = 'tmp/' + title + '.webm';
-    let outputFilePath = 'tmp/' + title + '.mp3';
-    let viewsFormatted = formatViews(views);
-    let infoText = `◦ *Title*: ${title}\n◦ *Duration*: ${durationText}\n◦ *Upload*: ${uploadDate}\n◦ *Views*: ${viewsFormatted}\n◦ *ID*: ${videoId}\n◦ *Description*: ${description}
-  `;
-    const pesan = conn.relayMessage(m.chat, {
+const youtube = require("yt-search");
+var handler = async (m, {
+    conn,
+    text,
+    usedPrefix
+}) => {
+    if (!text) throw 'Enter Title / Link From YouTube!';
+    try {
+        var search = await youtube(text);
+        var convert = search.videos[0];
+        if (!convert) throw 'Video/Audio Tidak Ditemukan';
+        if (convert.seconds >= 3600) {
+            return conn.reply(m.chat, 'Video is longer than 1 hour!', m);
+        } else {
+            var audioUrl
+            try {
+                audioUrl = `https://aemt.me/youtube?url=${convert.url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
+            } catch (e) {
+                conn.reply(m.chat, wait, m)
+                audioUrl = `https://aemt.me/youtube?url=${convert.url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
+            } 
+            var caption = `∘ Title : ${convert.title}\n∘ Ext : Search\n∘ ID : ${convert.videoId}\n∘ Duration : ${convert.timestamp}\n∘ Viewers : ${convert.views}\n∘ Upload At : ${convert.ago}\n∘ Author : ${convert.author.name}\n∘ Channel : ${convert.author.url}\n∘ Url : ${convert.url}\n∘ Description : ${convert.description}\n∘ Thumbnail : ${convert.image}`;
+            var pesan = conn.relayMessage(m.chat, {
                 extendedTextMessage:{
-                text: infoText, 
+                text: caption, 
                 contextInfo: {
                      externalAdReply: {
-                        title: wm,
-                        body: "",
+                        title: "Powered by",
                         mediaType: 1,
                         previewType: 0,
                         renderLargerThumbnail: true,
-                        thumbnailUrl: thumbnailUrl,
-                        sourceUrl: "https://youtube.com"
+                        thumbnailUrl: convert.image,
+                        sourceUrl: audioUrl
                     }
                 }, mentions: [m.sender]
-}}, {});
-
-    audio.pipe(fs.createWriteStream(inputFilePath)).on('finish', async () => {
-      ffmpeg(inputFilePath)
-        .toFormat('mp3')
-        .on('end', async () => {
-          let thumbnailData = await conn.getFile(thumbnailUrl);
-          let buffer = fs.readFileSync(outputFilePath);
-          conn.sendFile(m.chat, buffer, `${title}.mp3`, '', m);
-          fs.unlinkSync(inputFilePath);
-          fs.unlinkSync(outputFilePath);
-        })
-        .on('error', (err) => {
-          console.log(err);
-          m.reply(`There was an error converting the audio: ${err.message}`);
-          fs.unlinkSync(inputFilePath);
-          fs.unlinkSync(outputFilePath);
-        })
-        .save(outputFilePath);
-    });
-  } catch (e) {
-    console.log(e);
-    m.reply(`An error occurred while searching for the song: ${e.message}`);
-  }
+                }}, {})
+            conn.sendMessage(m.chat, {
+                audio: {
+                    url: audioUrl
+                },
+                mimetype: 'audio/mpeg',
+                contextInfo: {
+                    externalAdReply: {
+                        title: convert.title,
+                        body: "",
+                        thumbnailUrl: convert.image,
+                        sourceUrl: audioUrl,
+                        mediaType: 1,
+                        showAdAttribution: true,
+                        renderLargerThumbnail: true
+                    }
+                }
+            }, {
+                quoted: m
+            });
+        }
+    } catch (e) {
+        conn.reply(m.chat, `*Error:* ` + eror, m);
+    }
 };
 
 handler.command = handler.help = ['play', 'song', 'ds', 'ytmp3', 'yta'];
 handler.tags = ['downloader'];
+handler.exp = 0;
+handler.limit = true;
 handler.premium = false;
-handler.limit = false;
-
-module.exports = handler
-
-function formatViews(views) {
-  if (views >= 1000000) {
-    return (views / 1000000).toFixed(1) + 'M';
-  } else if (views >= 1000) {
-    return (views / 1000).toFixed(1) + 'K';
-  } else {
-    return views.toString();
-  }
-}
+module.exports = handler;
