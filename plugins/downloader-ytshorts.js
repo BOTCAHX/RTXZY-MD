6c122de008539867058ea4bb6777566252d32cb4
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-09-01 22:14:11 +0700

    YouTube shorts

--- plugins/downloader-ytshorts.js
+++ plugins/downloader-ytshorts.js
@@ -1,8 +1,8 @@
 var handler = async (m, {conn, text, usedPrefix}) => {
   if (!text) throw 'Berikan URL dari YouTube Shorts!'
   try {
-    var vid = `https://yt.tioo.eu.org/?url=${text}&filter=audioandvideo&quality=highestvideo&contenttype=video/mp4`
-    var aud = `https://yt.tioo.eu.org/?url=${text}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg` 
+    var vid = `https://aemt.me/youtube?url=${text}&filter=audioandvideo&quality=highestvideo&contenttype=video/mp4`
+    var aud = `https://aemt.me/youtube?url=${text}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg` 
     await conn.sendMessage(m.chat, { audio: { url: aud }, mimetype: 'audio/mpeg' }, { quoted: m }) 
     await conn.sendMessage(m.chat, { video: { url: vid }, mimetype: 'video/mp4' }, { quoted: m })
   } catch (e) {
