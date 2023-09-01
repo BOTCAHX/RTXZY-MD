d2043d3fb62eb8b1ffd0dd09259aaa9a4904a158
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-09-01 22:13:02 +0700

    youtube video url

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -1,39 +1,14 @@
-const { youtube } = require("btch-downloader");
-let handler = async (m, {
-    conn,
-    text,
-    usedPrefix
-}) => {
-    if (!text) throw 'Link From YouTube!';
-    try {
-        var convert = await youtube(text);
-        if (!convert) throw 'Video/Audio Tidak Ditemukan';
-            let api = await fetch(`https://api.botcahx.live/api/dowloader/yt?url=${text}&apikey=${btc}`);
-            let json = await api.json();
-            var caption = `∘ Title : ${convert.title}\n∘ Size : ${convert.size}\n∘ Quality : ${convert.quality}`;
-            var pesan = conn.relayMessage(m.chat, {
-                extendedTextMessage:{
-                text: caption, 
-                contextInfo: {
-                     externalAdReply: {
-                        title: "Powered by ",
-                        mediaType: 1,
-                        previewType: 0,
-                        renderLargerThumbnail: true,
-                        thumbnailUrl: convert.thumb,
-                        sourceUrl: text
-                    }
-                }, mentions: [m.sender]
-                }}, {})
-                conn.sendMessage(m.chat, { video: { url: json.result.mp4.result }, mimetype: 'video/mp4' }, { quoted: m })
-    } catch (e) {
-        conn.reply(m.chat, `*Error:* ` + e, m);
-    }
-};
+const youtube = require("yt-search");
+let handler = async (m, { conn, text }) => {
+  if (!text) throw 'Url nya mana?'
+  m.reply('_Proses..._')
+  var search = await youtube(text);
+  var convert = search.videos[0];
+  let url = `https://aemt.me/youtube?url=${convert.url}&filter=audioandvideo&quality=highestvideo&contenttype=video/mp4`
+  conn.sendMessage(m.chat, { video: { url: url }, mimetype: 'video/mp4' }, { quoted: m })
+}
+handler.help = ['ytmp4'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(ytv|ytvideo|ytmp4)$/i
 
-handler.command = handler.help = ['ytv','ytmp4'];
-handler.tags = ['downloader'];
-handler.exp = 0;
-handler.limit = true;
-handler.premium = false;
-module.exports = handler;
+module.exports = handler
