5df8512ca31ba6a2113e235039c655f5712c0c32
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-09-20 03:55:03 +0700

    limiter uploader

--- plugins/tools-uploader.js
+++ plugins/tools-uploader.js
@@ -1,34 +1,23 @@
-const fetch = require('node-fetch');
-const FormData = require('form-data');
-const { fromBuffer } = require('file-type');
+const uploadImage = require('../lib/uploadImage')
 
 let handler = async (m) => {
-  let q = m.quoted ? m.quoted : m;
-  let mime = (q.msg || q).mimetype || '';
-  if (!mime) throw 'Tidak ada media yang ditemukan';
-  let media = await q.download();
-  let format = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
-  let link = format ? await uploader(media) : null;
-  m.reply(`${link?.result?.url}\n${media.length} Byte(s)`);
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw 'Tidak ada media yang ditemukan'
+  let media = await q.download()
+  let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
+  
+  let fileSizeLimit = 5 * 1024 * 1024 // 5MB 🗿
+  if (media.length > fileSizeLimit) {
+    throw 'Ukuran media tidak boleh melebihi 5MB'
+  }
+  
+  let link = await (isTele ? uploadImage : uploadImage)(media)
+  m.reply(`${link}\n${media.length} Byte(s)`);
 }
 
-handler.help = ['tourl <reply image>'];
-handler.tags = ['owner'];
+handler.help = ['tourl <reply image>']
+handler.tags = ['sticker']
 handler.command = /^(upload|tourl)$/i
-handler.limit = true
-module.exports = handler;
 
-async function uploader(buffer) {
-  let { ext } = await fromBuffer(buffer);
-  let bodyForm = new FormData(); bodyForm.append("file", buffer, "file." + ext);
-
-  let res = await fetch("https://aemt.me/api/upload.php", {
-    method: "post",
-    body: bodyForm,
-  });
-  return {
-    status: res.status,
-    creator: "AEMT",
-    result: (await res.json()).result,
-  };
-}
+module.exports = handler
