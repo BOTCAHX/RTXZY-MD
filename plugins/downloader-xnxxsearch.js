b1fa70c2ae90e33ab62d516df19b0b785b1dc591
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-10-02 08:40:22 +0700

    teks

--- plugins/downloader-xnxxsearch.js
+++ plugins/downloader-xnxxsearch.js
@@ -4,7 +4,7 @@ var handler = async (m, { text, usedPrefix, command }) => {
   if (!text) {
     throw `Contoh:\n${usedPrefix + command} boobs`;
   }
-  
+  try {
   const search = await fetch(
     `https://api.botcahx.live/api/search/xnxx?query=${text}&apikey=${btc}`
   );
@@ -19,6 +19,9 @@ var handler = async (m, { text, usedPrefix, command }) => {
   
   await conn.sendMessage(m.chat, { react: { text: `⏱️`, key: m.key }});
   await conn.sendMessage(m.chat, { image: { url: hasil.result[0].thumb }, caption: teks }, { quoted: m });
+} catch (e) {
+throw `Can't find data!`
+}
  };
 
 handler.command = ['xnxxsearch'];
