6e60302703b4250ff2b45470d8a0abea7ef37922
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-10-04 21:11:00 +0700

    fix ytv

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -1,5 +1,6 @@
-const { youtube } = require("btch-downloader");
+const ytdl = require('ytdl-core');
 const fetch = require('node-fetch');
+
 let handler = async (m, {
     conn,
     text,
@@ -7,34 +8,20 @@ let handler = async (m, {
 }) => {
     if (!text) throw 'Link From YouTube!';
     try {
-        var convert = await youtube(text);
-        if (!convert) throw 'Video/Audio Tidak Ditemukan';
-            let api = await fetch(`https://api.botcahx.live/api/dowloader/yt?url=${text}&apikey=${btc}`);
-            let json = await api.json();
-            var caption = `∘ Title : ${convert.data.title}\n∘ Size : ${convert.data.medias[3].formattedSize}\n∘ Duration : ${convert.data.duration}\n`;
-            var pesan = conn.relayMessage(m.chat, {
-                extendedTextMessage:{
-                text: caption, 
-                contextInfo: {
-                     externalAdReply: {
-                        title: "Powered by ",
-                        mediaType: 1,
-                        previewType: 0,
-                        renderLargerThumbnail: true,
-                        thumbnailUrl: convert.data.thumbnail,
-                        sourceUrl: text
-                    }
-                }, mentions: [m.sender]
-                }}, {})
-                conn.sendMessage(m.chat, { video: { url: json.result.mp4.result }, mimetype: 'video/mp4' }, { quoted: m })
+        const { videoDetails } = await ytdl.getInfo(text);
+        const { title, description, publishDate, author } = videoDetails;
+        let api = await fetch(`https://api.botcahx.live/api/dowloader/yt?url=${text}&apikey=${btc}`).then(res => res.json());
+        var caption = `∘ Title : ${title}\n∘ Author : ${author}\n∘ Publish : ${publishDate}\n∘ Description : ${description}`;
+        conn.sendMessage(m.chat, { video: { url: api.result.mp4.result }, mimetype: 'video/mp4', caption: caption }, { quoted: m });
     } catch (e) {
-        conn.reply(m.chat, `*Error:* ` + e, m);
+        conn.reply(m.chat, `*Error:* ${e}`, m);
     }
 };
 
-handler.command = handler.help = ['ytv','ytmp4'];
+handler.command = handler.help = ['ytv', 'ytmp4'];
 handler.tags = ['downloader'];
 handler.exp = 0;
 handler.limit = true;
 handler.premium = false;
-module.exports = handler
+
+module.exports = handler;
