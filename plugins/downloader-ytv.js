781dbdcd85b0848a6b9ca43c28b2a2eca1c55911
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-10-06 16:47:27 +0700

    mencoba limiter :v

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -1,17 +1,20 @@
 const ytdl = require('ytdl-core');
 const fetch = require('node-fetch');
 
-let handler = async (m, {
-    conn,
-    text,
-    usedPrefix
-}) => {
+let handler = async (m, { conn, text, usedPrefix }) => {
     if (!text) throw 'Link From YouTube!';
     try {
         const { videoDetails } = await ytdl.getInfo(text);
         const { title, description, publishDate, author } = videoDetails;
         let api = await fetch(`https://api.botcahx.live/api/dowloader/yt?url=${text}&apikey=${btc}`).then(res => res.json());
-        var caption = `∘ Title : ${title}\n∘ Author : ${author}\n∘ Publish : ${publishDate}\n∘ Description : ${description}`;
+        var caption = `∘ Title : ${title}\n∘ Author : ${author}\n∘ Publish : ${publishDate}\n∘ Description : ${description}`;        
+        let size = api.result.mp4.size;
+        let fileSize = parseInt(size) * 1024 * 1024; 
+        
+        if (fileSize > 50 * 1024 * 1024) {
+            throw 'File berukuran terlalu besar';
+        }
+        
         conn.sendMessage(m.chat, { video: { url: api.result.mp4.result }, mimetype: 'video/mp4', caption: caption }, { quoted: m });
     } catch (e) {
         conn.reply(m.chat, `*Error:* ${e}`, m);
