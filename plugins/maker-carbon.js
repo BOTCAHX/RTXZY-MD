6191b3ab876f8fde76ab6bc35cdca6259925f7c7
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-10-09 09:34:19 +0700

    removed features involving puppeteer

--- plugins/maker-carbon.js
+++ plugins/maker-carbon.js
@@ -1,33 +1,23 @@
-/*
-Untuk pengguna fitur ini memerlukan puppeteer jadi silahkan install puppeteer terlebih dahulu 
-*/
-const carbon = require('carbon-now-scraper');
-const fs = require('fs');
-let handler = async (m, { conn, text, command}) => {
+const fetch = require('node-fetch');
 
-  let inputText = m.quoted ? m.quoted.text : text;
-  if (!inputText) {
-    return conn.reply(m.chat, `Masukkan pesan:\n${command} hello world`, m);
-  }
-
-  var randomColor = ['#ef1a11', '#89cff0', '#660000', '#87a96b', '#e9f6ff', '#ffe7f7', '#ca86b0', '#83a3ee', '#abcc88', '#80bd76', '#6a84bd', '#5d8d7f', '#530101', '#863434', '#013337', '#133700', '#2f3641', '#cc4291', '#7c4848', '#8a496b', '#722f37', '#0fc163', '#2f3641', '#e7a6cb', '#64c987', '#e6e6fa', '#ffffff', '#00000000'];
-  const apiColor = randomColor[Math.floor(Math.random() * randomColor.length)];
-  conn.reply(m.chat, '*Tunggu few seconds...*', m)
-
-  const code = inputText
-  const output = "./tmp/carbon.png"
-  const options = {
-    lang: "auto",
-    theme: "dracula",
-    background: apiColor,
-    "width-adjustment": false
-  }
-  await carbon(code, output, options)
-  await conn.sendMessage(m.chat, { image: await fs.readFileSync(output), caption: `*Done*`},{quoted:m})
+let handler = async (m, { conn, args }) => {
+   let text
+    if (args.length >= 1) {
+        text = args.slice(0).join(" ")
+    } else if (m.quoted && m.quoted.text) {
+        text = m.quoted.text
+    } else throw "Input teks atau reply teks yang ingin di jadikan carbon!"
+   if (!text) return m.reply('masukan text') 
+   try {
+   let img = await fetch(`https://api.botcahx.live/api/maker/carbon?text=${text}&apikey=${btc}`).then(res => res.json());
+   await conn.sendFile(m.chat, img.result, 'img.jpeg', '', m)
+   } catch (e) {
+   throw `✖️ *Server sedang error!`
+   }
 }
 
-handler.help = ['carbon'].map(v => v + ' <text>')
+handler.help = ['carbon']
 handler.tags = ['maker']
-handler.command = /^(carbon)$/i
-
+handler.command = /^(carbon|carbonara)$/i
+handler.limit = true
 module.exports = handler
