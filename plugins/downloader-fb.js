b525b906fb7925a6632f80b1c12d768eaf194e0e
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-10-11 20:28:07 +0700

    catch error

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,17 +1,22 @@
 const fetch = require('node-fetch');
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Gunakan contoh ${usedPrefix}${command} https://fb.watch/mcx9K6cb6t/?mibextid=8103lRmnirLUhozF`;
-    const res = await fetch(`https://api.botcahx.live/api/dowloader/fbdown?url=${args[0]}&apikey=${btc}`);
-    const json = await res.json();
-    let urls = json.result.url.urls;
-    if (!Array.isArray(urls)) {
-        throw `Tidak dapat mendapatkan URL video dari tautan yang diberikan`;
-    }
-    for (let url of urls) {
-        if (url.sd) {
-            conn.sendFile(m.chat, url.sd, 'fb.mp4', `*Facebook Downloader*`, m);
-            break;
+let handler = async (m, { conn, args, usedPrefix, command }) => {  
+        if (!args[0]) throw `Gunakan contoh ${usedPrefix}${command} https://fb.watch/mcx9K6cb6t/?mibextid=8103lRmnirLUhozF`;
+        try {
+        const res = await fetch(`https://api.botcahx.live/api/dowloader/fbdown?url=${args[0]}&apikey=${btc}`);
+        const json = await res.json();
+        let urls = json.result.url.urls;
+        if (!Array.isArray(urls)) {
+            throw `Tidak dapat mendapatkan URL video dari tautan yang diberikan`;
         }
+        for (let url of urls) {
+            if (url.sd) {
+                conn.sendFile(m.chat, url.sd, 'fb.mp4', `*Facebook Downloader*`, m);
+                break;
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        throw 'Terjadi kesalahan pada saat melakukan proses download';
     }
 }
 handler.help = ['facebook'].map(v => v + ' <url>');
