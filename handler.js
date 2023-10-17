703a54021375a0b56ba9dc38fd295f455406fe1a
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-10-17 21:30:18 +0700

    jid

--- handler.js
+++ handler.js
@@ -718,6 +718,7 @@ module.exports = {
                             this.sendMessage(id, {
                             text: text,
                             contextInfo: {
+			    mentionedJid: [user],
                             externalAdReply: {  
                             title: action === 'add' ? 'Selamat Datang' : 'Selamat tinggal',
                             body: global.wm,
