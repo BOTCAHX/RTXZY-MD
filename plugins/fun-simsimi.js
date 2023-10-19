34eb9251e046e0a6400a4f9096b706e952b618b7
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-10-19 23:43:04 +0700

    simi

--- plugins/fun-simsimi.js
+++ plugins/fun-simsimi.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
   let res = await fetch(`https://api.botcahx.live/api/search/simsimi?query=${encodeURIComponent(text)}&apikey=${btc}`)
   let json = await res.json()
-  m.reply(json.result.success)
+  m.reply(json.result)
 }
 handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
 handler.tags = ['fun']
