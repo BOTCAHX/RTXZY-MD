cfcaab2f5370543379ae2c071c8377fa56021295
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-10-20 08:24:11 +0700

    fix group add

--- plugins/group-add.js
+++ plugins/group-add.js
@@ -1,48 +1,23 @@
-let fetch = require('node-fetch')
-
-let handler = async (m, { conn, text, participants, usedPrefix, command }) => {
-  if (m.quoted) {
-    await conn.groupAdd(m.chat, [m.quoted.sender]).catch(_ => _)
-  }
-  if (!text) throw `uhm.. nomornya mana?\n\ncontoh:\n\n${usedPrefix + command + ' ' + global.owner[0]}`
-  let _participants = participants.map(user => user.jid)
-  let users = (await Promise.all(
-    text.split(',')
-      .map(v => v.replace(/[^0-9]/g, ''))
-      .filter(v => v.length > 4 && v.length < 20 && !_participants.includes(v + '@s.whatsapp.net'))
-      .map(async v => [
-        v,
-        await conn.isOnWhatsApp(v + '@s.whatsapp.net')
-      ])
-  )).filter(v => v[1]).map(v => v[0] + '@c.us')
-  let response = await conn.groupAdd(m.chat, users)
-  if (response[users] == 408) throw `_Gagal!_\n\nNomor tersebut telah keluar baru² ini\nHanya bisa masuk lewat *${usedPrefix}link* grup`
-  let pp = await conn.getProfilePicture(m.chat).catch(_ => false)
-  let jpegThumbnail = pp ? await (await fetch(pp)).buffer() : false
-  for (let user of response.participants.filter(user => Object.values(user)[0].code == 403)) {
-    let [[jid, {
-      invite_code,
-      invite_code_exp
-    }]] = Object.entries(user)
-    let teks = `Mengundang @${jid.split`@`[0]} menggunakan invite...`
-    m.reply(teks, null, {
-      contextInfo: {
-        mentionedJid: conn.parseMention(teks)
-      }
-    })
-    await conn.sendGroupV4Invite(m.chat, jid, invite_code, invite_code_exp, false, 'Invitation to join my WhatsApp group', jpegThumbnail ? {
-      jpegThumbnail
-    } : {})
-  }
+let handler = async (m, { conn, text, participants }) => {
+    let _participants = participants.map(user => user.id)
+    let users = (await Promise.all(
+        text.split(',')
+            .map(v => v.replace(/[^0-9]/g, ''))
+            .filter(v => v.length > 4 && v.length < 20 && !_participants.includes(v + '@s.whatsapp.net'))
+            .map(async v => [
+                v,
+                await conn.onWhatsApp(v + '@s.whatsapp.net')
+            ])
+    )).filter(v => v[1][0]?.exists).map(v => v[0] + '@c.us')
+    const response = await conn.groupParticipantsUpdate(m.chat, users, 'add')
+    m.reply(`Succes add person`, null, { mentions: response })
 }
-handler.help = ['add', '+'].map(v => v + ' nomor,nomor')
+handler.help = ['add 628xx']
 handler.tags = ['admin']
-handler.command = /^(add|\+)$/i
+handler.command = /^(add)$/i
 
-handler.group = true
 handler.admin = true
+handler.group = true
 handler.botAdmin = true
-handler.limit = true
-handler.owner = true
 
 module.exports = handler
