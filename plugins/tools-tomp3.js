237e7d4f72804355c4bd383f6b68a4032f97d7c8
Smartbotz <100168412+Smartbotz@users.noreply.github.com>
2023-10-25 22:26:33 +0700

    Update tools-tomp3.js

--- plugins/tools-tomp3.js
+++ plugins/tools-tomp3.js
@@ -1,22 +1,17 @@
 const { toPTT } = require('../lib/converter')
-const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, usedPrefix, command }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-  if (!/video|audio/.test(mime)) throw `Balas video atau voice note yang ingin diubah ke mp3 dengan caption *${usedPrefix + command}*`
-  let media = await q.download()
-  let audio = await toPTT(media, 'mp4')
- 
-//conn.sendFile(m.chat, audio,  0,0,m,true, {ptt:true })
- conn.sendMessage(m.chat, {audio:audio}, {
-    quoted: m,
-    ptt: true
-  })
+    if (!/video|audio/.test(mime)) throw `Balas video/audio dengan perintah *${usedPrefix + command}*`
+    let media = await q.download()
+    if (!media) throw 'Media tidak dapat diunduh'
+    let audio = await toAudio(media, 'mp4')
+    if (!audio.data) throw 'Gagal melakukan konversi.'
+    conn.sendMessage(m.chat, { audio: audio.data, mimetype: 'audio/mpeg' }, { quoted: m })
 }
 handler.help = ['tomp3 (reply)']
 handler.tags = ['tools']
-
-handler.command = /^to(mp3|a(udio)?)$/i
+handler.command = /^to(a(udio)?)$/i
 
 module.exports = handler
