efff97f5fe43c3547e1c6960cb1ab6972e924cc9
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-10-28 16:12:52 +0700

    catch error

--- plugins/fun-simsimi.js
+++ plugins/fun-simsimi.js
@@ -1,8 +1,13 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
+if (!text) throw `Masukan pertanyaan!`
+try {
   let res = await fetch(`https://api.botcahx.live/api/search/simsimi?query=${encodeURIComponent(text)}&apikey=${btc}`)
   let json = await res.json()
   m.reply(json.result)
+} catch (e) {
+throw `Internal server eror!`
+  }
 }
 handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
 handler.tags = ['fun']
