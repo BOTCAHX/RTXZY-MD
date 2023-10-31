091ce1fbfa34b3f6e4c07ead635696bd19a598a6
Smartbotz <100168412+Smartbotz@users.noreply.github.com>
2023-10-31 19:42:25 +0700

    Update tools-enc.js

--- plugins/tools-enc.js
+++ plugins/tools-enc.js
@@ -5,8 +5,8 @@ if (!text) throw `[!] Masukan textnya`
 let res = JavaScriptObfuscator.obfuscate(text)
 conn.reply(m.chat, res.getObfuscatedCode(), m)
 }
-handler.help = ['encrypt']
+handler.help = ['enc']
 handler.tags = ['tools']
-handler.command = /^enc(rypt)?$/i
+handler.command = /^enc$/i
 
 module.exports = handler
