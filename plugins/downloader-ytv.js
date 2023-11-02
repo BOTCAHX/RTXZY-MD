abcad69291093af7afdfdbbf0f4621fb31a9f788
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-11-02 09:01:36 +0700

    youtube video

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -1,30 +1,75 @@
-const ytdl = require('ytdl-core');
-const fetch = require('node-fetch');
+const fs = require("fs");
+const ytdl = require("ytdl-core");
 
-let handler = async (m, { conn, text, usedPrefix }) => {
-    if (!text) throw 'Link From YouTube!';
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    conn["youtubedl"] = conn["youtubedl"] || {};
+    if (m.sender in conn["youtubedl"]) {
+        return;
+    }
+    if (!args[0]) {
+        return m.reply(`Example: *${usedPrefix + command}* https://www.youtube.com/watch?v=IolEgdwvjEU`);
+    }
+    const isValid = await ytdl.validateURL(args[0]);
+    if (!isValid) {
+        return m.reply("*your link not suported.*");
+    }
+
+    const _filename = `./tmp/${Math.random().toString(36).substring(2, 7)}.mp4`;
+    const writer = fs.createWriteStream(_filename);
+
+    conn["youtubedl"][m.sender] = true;
     try {
-        const { videoDetails } = await ytdl.getInfo(text);
-        const { title, description, publishDate, author } = videoDetails;
-        let api = await fetch(`https://api.botcahx.live/api/dowloader/yt?url=${text}&apikey=${btc}`).then(res => res.json());
-        var caption = `∘ Title : ${title}\n∘ Author : ${author}\n∘ Publish : ${publishDate}\n∘ Description : ${description}`;        
-        let size = api.result.mp4.size;
-        let fileSize = parseInt(size) * 1024 * 1024; 
-        
-        if (fileSize > 50 * 1024 * 1024) {
-            throw 'File berukuran terlalu besar';
-        }
-        
-        conn.sendMessage(m.chat, { video: { url: api.result.mp4.result }, mimetype: 'video/mp4', caption: caption }, { quoted: m });
-    } catch (e) {
-        conn.reply(m.chat, `*Error:* ${e}`, m);
+        const { formats, videoDetails } = await ytdl.getInfo(args[0]);
+        const { title, description, publishDate, author, isFamilySafe } = videoDetails;
+        const { user } = author;
+        return new Promise(async (resolve, reject) => {
+            ytdl(args[0], {
+                quality: "lowest",
+            }).pipe(writer);
+            writer.on("error", () => {
+                m.reply("Failed sending video");
+                delete conn["youtubedl"][m.sender];
+                resolve();
+            });
+            writer.on("close", async () => {
+                try {
+                    await conn.sendMessage(
+                        m.chat,
+                        {
+                            video: {
+                                stream: fs.createReadStream(_filename),
+                            },
+                            caption: `┌  • *Y o u t u b e - M P 4*\n│  ◦ *Title:* ${title}\n│  ◦ *Published:* ${publishDate}\n└  ◦ *Author:* ${user}`,
+                        },
+                        { quoted: m }
+                    );
+                } catch {
+                    await conn.sendMessage(
+                        m.chat,
+                        {
+                            document: {
+                                stream: fs.createReadStream(_filename),
+                            },
+                            fileName: `${title}.mp4`,
+                            mimetype: "video/mp4",
+                            caption: `┌  • *Y o u t u b e - M P 4*\n│  ◦ *Title:* ${title}\n│  ◦ *Published:* ${publishDate}\n└  ◦ *Author:* ${user}`,
+                        },
+                        { quoted: m }
+                    );
+                }
+                fs.unlinkSync(_filename);
+                delete conn["youtubedl"][m.sender];
+                resolve();
+            });
+        });
+    } catch {
+        m.reply("*Failed get a video!*");
     }
 };
 
-handler.command = handler.help = ['ytv', 'ytmp4'];
-handler.tags = ['downloader'];
-handler.exp = 0;
-handler.limit = true;
-handler.premium = false;
+handler.help = ["ytmp4"].map((v) => v + ' url');
+handler.tags = ["downloader"];
+handler.command = /^yt(v|mp4)?$/i;
+handler.register = true;
 
 module.exports = handler;
