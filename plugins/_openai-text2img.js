937d374dbc8468f28c0141380fe3dc8c8eeb33d6
Smartbotz <100168412+Smartbotz@users.noreply.github.com>
2023-11-05 22:15:35 +0700

    Update cmd _openai-text2img.js

--- plugins/_openai-text2img.js
+++ plugins/_openai-text2img.js
@@ -11,7 +11,7 @@ let handler = async (m, { text, usedPrefix, command }) => {
   }
 };
 
-handler.command = handler.help = ['text2img'];
+handler.command = handler.help = ['text2img', 'diffusion', 'stablediffusion', 'diff'];
 handler.tags = ['tools'];
 handler.limit = true;
 module.exports = handler;
