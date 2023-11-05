7e3e11ca018420a998c7ac5fa93a53a76dfdeda6
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-11-05 23:13:04 +0700

    Update _openai-dalle.js

--- plugins/_openai-dalle.js
+++ plugins/_openai-dalle.js
@@ -2,10 +2,13 @@ let fetch = require('node-fetch')
 let handler = async (m, { text, command, conn }) => {
 
   if (!text) throw 'Masukan teks untuk diubah menjadi gambar'
-
+  try { 
   let response = await fetch(`https://api.botcahx.live/api/search/openai-image?text=${encodeURIComponent(text)}&apikey=${btc}`)
   let image = await response.buffer()
   conn.sendFile(m.chat, image, 'aiimg.jpg',  wm, m)
+  } catch (e) {
+    throw `Error: ${eror}`
+  }
 
 }
 handler.command = handler.help = ['aiimg','aiimage','ai-image','dalle']
