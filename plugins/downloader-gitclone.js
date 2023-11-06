0db0cdc3ceee64bdea9a099a2d54fc9119f6cc23
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-11-06 12:20:40 +0700

    Update downloader-gitclone.js

--- plugins/downloader-gitclone.js
+++ plugins/downloader-gitclone.js
@@ -2,17 +2,16 @@ let regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
 let fetch = require('node-fetch')
 let handler = async (m, { args, usedPrefix, command }) => {
 
-    if (!args[0]) throw 'link githubnya mana? contoh: https://github.com/Bintang73/botst4rz'
+    if (!args[0]) throw 'link githubnya mana? contoh: https://github.com/BOTCAHX/RTXZY-MD'
 
     if (!regex.test(args[0])) throw 'link salah!'
 
     let [, user, repo] = args[0].match(regex) || []
     repo = repo.replace(/.git$/, '')
     let url = `https://api.github.com/repos/${user}/${repo}/zipball`
-    let filename = (await fetch(url, {method: 'HEAD'})).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
-    // 'attachment; filename=Nurutomo-wabot-aq-v2.5.1-251-g836cccd.zip'
-    m.reply(`*Mohon tunggu, sedang mengirim repository..*`)
-    conn.sendFile(m.chat, url, filename, null, m)
+    	let filename = (await fetch(url, { method: 'HEAD' })).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
+		m.reply(wait)
+		await conn.sendMessage(m.chat, { document: { url: url }, mimetype: 'application/zip', fileName: filename.replace('.zip.zip','.zip')}, { quoted : m })
 
 }
 handler.help = ['gitclone <url>']
