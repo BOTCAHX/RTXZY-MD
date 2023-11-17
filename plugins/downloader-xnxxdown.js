1650a9dfde6b43901bdf4209d63346676c50b622
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-11-17 23:40:53 +0700

    Update downloader-xnxxdown.js

--- plugins/downloader-xnxxdown.js
+++ plugins/downloader-xnxxdown.js
@@ -5,12 +5,15 @@ var handler = async (m, {
  command
  }) => {
 if (!text) throw 'Masukkan Query Link!'
- 
+ try {
 let anu = await fetch(`https://api.botcahx.live/api/download/xnxxdl?url=${text}&apikey=${btc}`)
 let hasil = await anu.json() 
 
 conn.sendMessage(m.chat, { video: { url: hasil.result.url }, fileName: 'xnxx.mp4', mimetype: 'video/mp4' }, { quoted: m })
-  }                                                    
+ } catch (e) {
+ throw `*Server error!*`
+ }
+}
 handler.command = handler.help = ['xnxxdown'];
 handler.tags = ['internet'];
 module.exports = handler;
