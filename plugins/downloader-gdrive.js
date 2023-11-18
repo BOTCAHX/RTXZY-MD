ea418b08009ff334273c86f9d0060ec06e731b6a
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-11-19 02:18:22 +0700

    Update downloader-gdrive.js

--- plugins/downloader-gdrive.js
+++ plugins/downloader-gdrive.js
@@ -1,36 +1,14 @@
-const fetch = require('node-fetch')
-async function GDriveDl(url) {
-	let id = (url.match(/\/?id=(.+)/i) || url.match(/\/d\/(.*?)\//))?.[1]
-	if (!id) throw 'ID Not Found'
-	let res = await fetch(`https://drive.google.com/uc?id=${id}&authuser=0&export=download`, {
-		method: 'post',
-		headers: {
-			'accept-encoding': 'gzip, deflate, br',
-			'content-length': 0,
-			'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-			'origin': 'https://drive.google.com',
-			'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
-			'x-client-data': 'CKG1yQEIkbbJAQiitskBCMS2yQEIqZ3KAQioo8oBGLeYygE=',
-			'x-drive-first-party': 'DriveWebUi',
-			'x-json-requested': 'true' 
-		}
-	})
-	let { fileName, sizeBytes, downloadUrl } =  JSON.parse((await res.text()).slice(4))
-	if (!downloadUrl) throw 'Link Download Limit!'
-	let data = await fetch(downloadUrl)
-	if (data.status !== 200) throw data.statusText
-	return {
-		downloadUrl, fileName,
-		fileSize: (sizeBytes / 1024 / 1024).toFixed(2),
-		mimetype: data.headers.get('content-type')
-	}
-}
+let fetch = require('node-fetch')
 
-const handler = async (m, { conn, args }) => {
-	if (!args[0]) throw 'Input URL'
-	if (!args[0].match(/drive\.google/i)) throw 'Invalid URL'
-	let res = await GDriveDl(args[0])
-	await conn.sendMessage(m.chat, { document: { url: res.downloadUrl }, fileName: res.fileName, mimetype: res.mimetype }, { quoted: m })
+let handler = async (m, { text, usedPrefix, command }) => {
+    if (!text) throw `*Example:* ${usedPrefix + command} https://drive.google.com/file/d/1thDYWcS5p5FFhzTpTev7RUv0VFnNQyZ4/view?usp=drivesdk`
+    m.reply(wait)   
+    try {     
+        let json = await fetch(`https://api.botcahx.live/api/download/gdrive?url=${text}&apikey=${btc}`).then(res => res.json());                  
+        conn.sendMessage(m.chat, { document: { url: json.result.data }, fileName: json.result.fileName, mimetype: json.result.mimetype }, { quoted: m })
+    } catch (e) {     
+        throw `Error: ${eror}`
+    }
 }
 handler.command = handler.help = ['gdrive','gdrivedl']
 handler.tags = ['downloader']
