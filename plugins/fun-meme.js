2433e848e4f72ea67449b94db738dfb42ba6f8cf
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-11-26 00:02:54 +0700

    Update fun-meme.js

--- plugins/fun-meme.js
+++ plugins/fun-meme.js
@@ -1,9 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
-let tio = await fetch('https://raw.githubusercontent.com/HasamiAini/wabot_takagisan/main/whatsapp%20bot%20takagisan/whatsapp%20bot%20takagisan/lib/memeindo.json')
-let json = await tio.json();
-let url = json[Math.floor(Math.random() * json.length)]
-await conn.sendFile(m.chat, url.image, 'file.jpg', wm, m)
+try { 
+let img = await fetch(`https://api.botcahx.live/api/random/meme?apikey=${btc}`).then(result => result.buffer())
+await conn.sendFile(m.chat, img, 'file.jpg', wm, m)
+} catch (e) {
+throw `Error ${eror}`
+ }
 }
 handler.command = /^(meme)$/i
 handler.tags = ['fun']
