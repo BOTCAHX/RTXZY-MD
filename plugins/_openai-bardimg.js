59b92ba0dd8292b35f0959ffd2341c1c83383f0e
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-12-18 19:10:52 +0700

    test aja dlu

--- plugins/_openai-bardimg.js
+++ plugins/_openai-bardimg.js
@@ -2,26 +2,26 @@ const fetch = require('node-fetch');
 const uploader = require('../lib/uploadImage');
 
 let handler = async (m, { conn, text, command, usedPrefix }) => {
-  try {
-    let q = m.quoted ? m.quoted : m;
-    let mime = (q.msg || q).mimetype || q.mediaType || '';
-    if (/image/g.test(mime) && !/webp/g.test(mime)) {
-      let buffer = await q.download();
-      await m.reply(wait);
-      let media = await uploader(buffer);
-      let json = await (await fetch(`https://api.botcahx.eu.org/api/search/bard-img?url=${media}&text=${text}&apikey=${btc}`)).json();  
-      conn.sendMessage(m.chat, { text: json.result }, { quoted: m });
-    } else {
-      throw `Reply image with command ${usedPrefix + command} pertanyaan`;
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || q.mediaType || '' 
+  if (/image/g.test(mime) && !/webp/g.test(mime)) {
+    let buffer = await q.download()
+    await m.reply(wait)    
+    try {
+      let media = await uploader(buffer)
+      let json = await (await fetch(`https://api.botcahx.eu.org/api/search/bard-img?url=${media}&text=${text}&apikey=${btc}`)).json()  
+      conn.sendMessage(m.chat, { text: json.result }, { quoted: m })
+    } catch (err) {
+      throw `${eror}`
     }
-  } catch (e) {
-    console.error(e);
-    throw `${eror}`
+  } else {
+    throw `Reply image with command ${usedPrefix + command} pertanyaan`
   }
-};
-handler.help = ['bardimg'];
-handler.tags = ['tools'];
-handler.command = /^(bardimg|bardimage)$/i;
+}
+
+handler.help = ['bardimg']
+handler.tags = ['tools']
+handler.command = /^(bardimg|bardimage)$/i
 handler.limit = true;
 
-module.exports = handler;
+module.exports = handler
