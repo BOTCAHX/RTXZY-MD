35b7e823cda31a2197a3ab8036575b2a516a5b37
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-12-20 11:59:05 +0700

    biar gak spam error

--- plugins/sticker-tts.js
+++ plugins/sticker-tts.js
@@ -1,44 +1,48 @@
-let gtts = require('node-gtts')
-let fs = require('fs')
-let path = require('path')
-let { spawn } = require('child_process')
+let gtts = require('node-gtts');
+let fs = require('fs');
+let path = require('path');
+let { spawn } = require('child_process');
 
-const defaultLang = 'id'
+const defaultLang = 'id';
 let handler = async (m, { conn, args }) => {
+  try {
+    let lang = args[0];
+    let text = args.slice(1).join(' ');
+    if ((args[0] || '').length !== 2) {
+      lang = defaultLang;
+      text = args.join(' ');
+    }
+    if (!text && m.quoted && m.quoted.text) text = m.quoted.text;
 
-  let lang = args[0]
-  let text = args.slice(1).join(' ')
-  if ((args[0] || '').length !== 2) {
-    lang = defaultLang
-    text = args.join(' ')
+    let res = await tts(text, lang);
+    conn.sendFile(m.chat, res, 'tts.opus', null, m, true);
+  } catch (e) {
+    m.reply('*Contoh:* .tts hello world');
   }
-  if (!text && m.quoted && m.quoted.text) text = m.quoted.text
-
-  let res
-  try { res = await tts(text, lang) }
-  catch (e) {
-    m.reply(e + '')
-    res = await tts(text)
-  } finally {
-    conn.sendFile(m.chat, res, 'tts.opus', null, m, true)
-  }
-}
-handler.help = ['tts <teks>']
-handler.tags = ['tools']
-handler.command = /^tts$/i
-module.exports = handler
+};
+handler.help = ['tts <teks>'];
+handler.tags = ['tools'];
+handler.command = /^tts$/i;
+module.exports = handler;
 
 function tts(text, lang = 'id') {
-  console.log(lang, text)
+  console.log(lang, text);
   return new Promise((resolve, reject) => {
-    try {
-      let tts = gtts(lang)
-      let filePath = path.join(__dirname, '../tmp', (1 * new Date) + '.wav')
-      tts.save(filePath, text, () => {
-        resolve(fs.readFileSync(filePath))
-        fs.unlinkSync(filePath)
-      })
-    } catch (e) { reject(e) }
-  })
+    let tts = gtts(lang);
+    let filePath = path.join(__dirname, '../tmp', (1 * new Date) + '.wav');
+    tts.save(filePath, text, (err, result) => {
+      if (err) reject(err);
+      else {
+        fs.readFile(filePath, (err, data) => {
+          if (err) reject(err);
+          else {
+            fs.unlink(filePath, err => {
+              if (err) console.log(err);
+            });
+            resolve(data);
+          }
+        });
+      }
+    });
+  });
 }
-//fitur mengubah teks menjadi audio/ppt
