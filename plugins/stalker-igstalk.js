7dcb5b776de5303edcb4fd9b288864b8c3de69b7
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-12-28 20:37:28 +0700

    igstalk fix

--- plugins/stalker-igstalk.js
+++ plugins/stalker-igstalk.js
@@ -4,9 +4,21 @@ let handler = async (m, { text, usedPrefix, command }) => {
     try {
         let api = await fetch(`https://api.botcahx.eu.org/api/stalk/ig?username=${text}&apikey=${btc}`)
         let x = await api.json()
-        let caption = `▢ *Username*: ${x.result.username}\n▢ *Full Name*: ${x.result.fullName}\n▢ *Bio*: ${x.result.bio}\n▢ *Followers*: ${x.result.followers}\n▢ *Following*: ${x.result.following}\n▢ *Post*: ${x.result.postsCount}
-        `
-        return conn.sendFile(m.chat, x.result.profilePicHD, 'pp.png', caption, m)
+        let caption = `▢ *Username*: ${x.result.username}
+▢ *Full Name*: ${x.result.full_name}
+▢ *Followers*: ${x.result.followers}
+▢ *Followings*: ${x.result.followings}
+▢ *Biography*: ${x.result.biography}
+▢ *External URL*: ${x.result.external_url}
+▢ *Business Account*: ${x.result.is_business_account ? 'Yes' : 'No'}
+▢ *Professional Account*: ${x.result.is_professional_account ? 'Yes' : 'No'}
+▢ *Private Account*: ${x.result.is_private ? 'Yes' : 'No'}
+▢ *Verified Account*: ${x.result.is_verified ? 'Yes' : 'No'}
+▢ *Verified by MV4B*: ${x.result.is_verified_by_mv4b ? 'Yes' : 'No'}
+▢ *Pronouns*: ${x.result.pronouns.join(', ')}
+▢ *Post Count*: ${x.result.post_count}
+▢ *EIMU ID*: ${x.result.eimu_id}`
+        return conn.sendFile(m.chat, x.result.profile_pic_url_hd, 'pp.png', caption, m)
     } catch (e) {
         m.reply('Sistem Sedang Bermasalah!')
     }
