a1ac18074b95744b4c88c5313e037cb3e5349932
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-12-28 22:50:57 +0700

    quotes

---
+++ plugins/quotes-katabijak.js
@@ -0,0 +1,28 @@
+const fetch = require('node-fetch');
+
+let handler = async (m, { conn }) => {
+const res = await fetch(`https://api.botcahx.eu.org/api/random/bijak&apikey=${btc}`).then(result => result.json())
+
+
+let anu =`─────〔 *Kata Bijak* 〕─────
+
+${res.result}
+`
+m.reply(anu) 
+}
+handler.help = ['katabijak']
+handler.tags = ['quotes']
+handler.command = /^(katabijak)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.register = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
