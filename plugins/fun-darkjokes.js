d53022a16579aed11715b2e9f22ca4c37172b9b7
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-12-28 23:07:33 +0700

    Create fun-darkjokes.js

---
+++ plugins/fun-darkjokes.js
@@ -0,0 +1,14 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, text }) => {
+try { 
+let img = await fetch(`https://api.botcahx.eu.org/api/wallpaper/darkjokes?apikey=${btc}`).then(result => result.buffer())
+await conn.sendFile(m.chat, img, 'file.jpg', wm, m)
+} catch (e) {
+throw `Error ${eror}`
+ }
+}
+handler.command = /^(darkjokes)$/i
+handler.tags = ['fun']
+handler.help = ['darkjokes']
+handler.limit = true
+module.exports = handler
