bde7f4f7c044e2e9ca70ec23901b9e7d8f9ec620
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2023-12-31 11:27:37 +0700

    pinterest

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -1,12 +1,27 @@
-let xfar = require('xfarr-api')
+let fetch = require('node-fetch');
 let handler = async (m, { usedPrefix, command, conn, args }) => {
-	 	  if (!args[0]) throw `Gunakan format: ${usedPrefix}${command} naruto`
-xfar.Pinterest(args[0]).then(async data => {
-let pincpt = `🔗Link media : ${data.url}`
-conn.sendFile(m.chat,data.url, 'pin.jpg', pincpt,m)})
+  if (!args[0]) throw `*🚩 Example:* ${usedPrefix}${command} Zhao Lusi`;
+  m.reply(wait)
+  try {
+    let response = await fetch(`https://api.botcahx.eu.org/api/search/pinterest?text1=${args[0]}&apikey=${btc}`);
+    let data = await response.json();   
+    let old = new Date()
+    let limit = Math.min(5, data.result.length);
+    for(let i = 1; i < limit; i++) { 
+      await sleep(3000);
+      conn.sendFile(m.sender, data.result[i], 'pin.jpg', `🍟 *Fetching* : ${((new Date - old) * 1)} ms`, m);
+    }
+  } catch (e) {
+    throw `${eror}`;
+  }
 }
-handler.help = ['pinterest <keyword>']
-handler.tags = ['internet', 'downloader']
-handler.command = /^(pinterest)$/i
 
-module.exports = handler
+handler.help = ['pinterest <keyword>'];
+handler.tags = ['internet', 'downloader'];
+handler.command = /^(pinterest)$/i;
+
+module.exports = handler;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
