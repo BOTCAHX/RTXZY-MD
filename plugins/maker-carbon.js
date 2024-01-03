5e145d47a32f3250bbbc6c5203ac6137476e80a0
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-01-04 05:40:29 +0700

    .

--- plugins/maker-carbon.js
+++ plugins/maker-carbon.js
@@ -9,6 +9,7 @@ let handler = async (m, { conn, args }) => {
     } else throw "Input teks atau reply teks yang ingin di jadikan carbon!"
    if (!text) return m.reply('masukan text') 
    try {
+   m.reply(wait)
    let img = await fetch(`https://api.botcahx.eu.org/api/maker/carbon?text=${text}&apikey=${btc}`).then(res => res.json());
    await conn.sendFile(m.chat, img.result, 'img.jpeg', '', m)
    } catch (e) {
