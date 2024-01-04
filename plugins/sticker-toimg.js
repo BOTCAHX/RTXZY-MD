e4be595acbca8fc5a5aae97785fa59c23237ec8a
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-01-05 05:11:48 +0700

    fix to image

--- plugins/sticker-toimg.js
+++ plugins/sticker-toimg.js
@@ -1,23 +1,31 @@
+const { spawn } = require('child_process')
+const { format } = require('util')
 
-let { webp2png } = require('../lib/webp2mp4')
-let handler = async (m, { conn, usedPrefix, command }) => {
-  if (!m.quoted) throw `balas stiker dengan caption *${usedPrefix + command}*`
-  let mime = m.quoted.mimetype || ''
-  if (!/webp/.test(mime)) throw `balas stiker dengan caption *${usedPrefix + command}*`
-  try {
-  let media = await m.quoted.download()
-  let out = Buffer.alloc(0)
-  if (/webp/.test(mime)) {
-    out = await webp2png(media)
-  }
-  await conn.sendFile(m.chat, out, 'out.png', '*DONE*', m, false, {
-    thumbnail: Buffer.alloc(0)
-  })
-  } catch (e) {
-  return `*Media does not support!*`
-  }
+let handler = async (m, { conn, command, usedPrefix }) => {
+	let q = m.quoted
+	if (q && /sticker/.test(q.mtype)) {
+		if (q.isAnimated) return m.reply(`Gunakan *${usedPrefix}togif* untuk stiker bergerak`)
+		let img = await m.quoted.download()
+		try {
+			let bufs = []
+			const [_spawnprocess, ..._spawnargs] = [...(global.support.gm ? ['gm'] : global.support.magick ? ['magick'] : []), 'convert', 'webp:-', 'png:-']
+			let im = spawn(_spawnprocess, _spawnargs)
+			im.on('error', e => m.reply(format(e)))
+			im.stdout.on('data', chunk => bufs.push(chunk))
+			im.stdin.write(img)
+			im.stdin.end()
+			im.on('exit', async () => {
+				await conn.sendFile(m.chat, Buffer.concat(bufs), 'image.png', '*DONE*', m)
+			})
+		} catch (e) {
+			console.log(e)
+			await conn.sendMsg(m.chat, { image: img, jpegThumbnail: img, caption: '*DONE*' }, { quoted: m })
+		}
+	} else return m.reply('Reply / tag Sticker')
 }
-handler.help = ['toimg (reply)']
+
+handler.help = ['toimg']
 handler.tags = ['sticker']
-handler.command = ['toimg']
+handler.command = /^(toim(g|age))$/i
+
 module.exports = handler
