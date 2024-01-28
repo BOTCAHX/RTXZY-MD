04bd4f5d2b2af7c5bf962a92d2b735d83d3a3c6e
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-01-29 06:58:23 +0700

    bio

---
+++ plugins/_autobio.js
@@ -0,0 +1,22 @@
+module.exports = {
+  before: async function all(m) {
+    if (m.message) {
+      setInterval(async () => {
+        let uptime = process.uptime() * 1000		       
+        let bio = `🤖 Aktif Selama: ${clockString(uptime)} | 💌 by: ${wm}`  
+        await this.updateProfileStatus(bio).catch(_ => _)
+      }, 60000)
+    }
+  }
+}
+
+function clockString(ms) {
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':')
+}
+
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)]
+}
