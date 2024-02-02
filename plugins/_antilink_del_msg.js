e6d7fb0c31dd43296094823b821d3d85139d6ac6
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-02-03 04:31:30 +0700

    Update and rename _antililink_del.js to _antilink_del_msg.js

---
+++ plugins/_antilink_del_msg.js
@@ -0,0 +1,23 @@
+/** // Untuk delete pesan link Group
+
+let handler = async (m, { conn, command, text }) => {
+  await conn.sendMessage(m.chat, { delete: m.key})
+}
+handler.customPrefix = /(https?:\/\/)?(www\.)?(chat.whatsapp.com\/invite\/)?([0-9A-Za-z]{20,24})/ig
+handler.command = new RegExp()
+
+module.exports = handler
+
+**/
+
+/**  // Untuk delete semua pesan berawalan https
+
+let handler = async (m, { conn, command, text }) => {
+  await conn.sendMessage(m.chat, { delete: m.key})
+}
+handler.customPrefix = /(https?:\/\/)?[\da-z\.-]+\.[a-z\.]{2,6}(\/([^\s]*))?/ig
+handler.command = new RegExp()
+
+module.exports = handler
+
+**/
