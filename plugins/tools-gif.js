a375d226bcc52e6056a12dd4422d89d90d662125
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-02-09 18:25:30 +0700

    Update tools-gif.js

--- plugins/tools-gif.js
+++ plugins/tools-gif.js
@@ -1,17 +1,40 @@
-let { webp2mp4 } = require('../lib/webp2mp4')
-let handler = async (m, { conn, usedPrefix, command }) => {
-    if (!m.quoted) throw `balas stiker dengan caption *${usedPrefix + command}*`
-    let mime = m.quoted.mimetype || ''
-    if (!/webp/.test(mime)) throw `balas stiker dengan caption *${usedPrefix + command}*`
-    let media = await m.quoted.download()
-    let out = Buffer.alloc(0)
-    if (/webp/.test(mime)) {
-        out = await webp2mp4(media)
+ const fs = require('fs');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const converter = async bufferImage => {
+  try {
+    let pathFile = "./tmp/" + ~~(Math.random() * 1000000 + 1) + ".webp";
+    fs.writeFileSync(pathFile, bufferImage);
+    await exec(`convert ${pathFile} ${pathFile}.gif`);
+    await exec(`ffmpeg -i ${pathFile}.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" ${pathFile}.mp4`);
+    if (!fs.existsSync(pathFile + ".gif") || !fs.existsSync(pathFile + ".mp4")) {
+      throw new Error("Failed convert file!");
     }
-    await conn.sendFile(m.chat, out, 'out.gif', wm, m, false, { mimetype: 'video/gif', thumbnail: Buffer.alloc(0) })
-}
-handler.help = ['togif (reply)']
-handler.tags = ['sticker']
-handler.command = /^(togif|tovideo)$/i
+    let videoBuffer = fs.readFileSync(pathFile + ".mp4");
+    fs.unlinkSync(pathFile);
+    fs.unlinkSync(pathFile + ".gif");
+    fs.unlinkSync(pathFile + ".mp4");
+    return videoBuffer;
+  } catch(error) {
+    throw error;
+  }
+};
+
+let handler = async (m, { conn, command, usedPrefix }) => {
+	m.reply(wait)
+	if (m.quoted && /sticker/.test(m.quoted.mtype) && !m.quoted.isAnimated) {
+		let img = await m.quoted.download()
+		await conn.sendMessage(m.chat, { image: img, jpegThumbnail: img }, { quoted: m })
+	} else if (m.quoted && /sticker/.test(m.quoted.mtype) && m.quoted.isAnimated) {		
+		let img = await m.quoted.download()
+		let out = await converter(img)
+		await conn.sendMessage(m.chat, { video: out, gifPlayback: /gif/i.test(m.text), gifAttribution: ~~(Math.random() * 2) }, { caption: '*DONE*', quoted: m });
+	} else throw `Balas sticker dengan caption *${usedPrefix + command}*`
+};
+
+handler.command = handler.help = ['togif','tovideo'];
+handler.tags = ['sticker'];
+handler.limit = true;
 
 module.exports = handler
