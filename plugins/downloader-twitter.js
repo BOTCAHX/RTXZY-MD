c843ad2c525b36d63877809842d1bdb3ee9be847
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-02-18 14:37:17 +0700

    Fix Downloader Twitter

--- plugins/downloader-twitter.js
+++ plugins/downloader-twitter.js
@@ -1,24 +1,33 @@
-let fetch = require("node-fetch");
+const fetch = require("node-fetch");
+
 let handler = async (m, {
-	conn,
-	args,
-	usedPrefix,
-	command
+    conn,
+    args,
+    usedPrefix,
+    command
 }) => {
-if (!args[0]) throw `Masukan URL!\n\ncontoh:\n${usedPrefix + command} https://twitter.com/gofoodindonesia/status/1229369819511709697`
-if (!args[0].match(/https?:\/\/(www\.)?(twitter\.com|x\.com)/gi)) throw "URL Tidak Ditemukan!";
-m.reply(wait)
-  try {
-       const api = await fetch(`https://api.botcahx.eu.org/api/download/twitter2?url=${args[0]}&apikey=${btc}`)
-        const res = await api.json()
-        for (let i of res.result.urls) {
-        conn.sendFile(m.chat, i.url, null, `*Twitter Downloader*`, m)
+    if (!args[0]) throw `Masukkan URL!\n\ncontoh:\n${usedPrefix + command} https://twitter.com/gofoodindonesia/status/1229369819511709697`;
+    if (!args[0].match(/https?:\/\/(www\.)?(twitter\.com|x\.com)/gi)) throw "URL Tidak Ditemukan!";
+    m.reply(wait);
+    try {
+        const api = await fetch(`https://api.botcahx.eu.org/api/download/twitter2?url=${args[0]}&apikey=${btc}`);
+        const res = await api.json();
+        const mediaURLs = res.result.mediaURLs;
+        
+        const capt = `*Username: ${res.result.user_name} ${res.result.user_screen_name}*\n*Title: ${res.result.text}*\n*Replies: ${res.result.replies}*\n*Retweet: ${res.result.retweets}*`;
+        
+        for (const url of mediaURLs) {
+            const response = await fetch(url);
+            const buffer = await response.buffer();  
+            await delay(3000)        
+            conn.sendFile(m.chat, buffer, null, capt, m);           
         }
-     } catch (e) {
-        throw `*Server Down!*`
+    } catch (e) {
+        throw '*Server Down!*';
     }
 };
-handler.command = handler.help = ['twitter','twitdl','twitterdl','xcom'];
+
+handler.command = handler.help = ['twitter', 'twitdl', 'twitterdl'];
 handler.tags = ['downloader'];
 handler.limit = true;
 handler.group = false;
@@ -28,4 +37,9 @@ handler.admin = false;
 handler.botAdmin = false;
 handler.fail = null;
 handler.private = false;
+
 module.exports = handler;
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
