5dd8ffcaa3f00791e583255a363fb549005e1b4a
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-05-17 21:43:31 +0700

    douyinslide & tiktokslide

---
+++ plugins/downloader-ttslide-douyinslide.js
@@ -0,0 +1,51 @@
+ const fetch = require('node-fetch');
+
+let handler = async (m, {
+  conn,
+  text,
+  usedPrefix,
+  command
+}) => {
+  if (command == 'tiktokslide' || command == 'ttslide') {
+    if (!text) throw `Masukkan URL!\n\ncontoh: ${usedPrefix + command} https://vt.tiktok.com/ZSY8Me4jL/`;
+    try {
+      const api = await fetch(`https://api.botcahx.eu.org/api/download/tiktokslide?url=${text}&apikey=${btc}`);
+      const res = await api.json();
+      for (let i of res.result.images) {
+        await sleep(3000);
+        conn.sendMessage(m.chat,{ image :{ url : i } , caption : `*Title*: ${res.result.title}` }, { quoted: m });
+        await sleep(5000);
+        conn.sendMessage(m.chat, { audio: { url: res.result.audio[0] }, mimetype: 'audio/mpeg' }, { quoted: m });         
+      }
+    } catch (e) {
+      console.log(e);
+      throw `🚩 *Terjadi kesalahan!*`;
+    }
+  }
+  if (command == 'douyinslide' || command == 'douyinfoto') { 
+    if (!text) throw `Masukkan URL!\n\ncontoh: ${usedPrefix + command} https://v.douyin.com/i2bPkLLo/`;
+    try {
+      const api = await fetch(`https://api.botcahx.eu.org/api/download/douyinslide?url=${text}&apikey=${btc}`);
+      const res = await api.json();
+      for (let i of res.result.images) {
+        await sleep(3000);
+        conn.sendMessage(m.chat,{ image :{ url : i } , caption : `*Title*: ${res.result.title}` }, { quoted: m });
+        await sleep(5000);
+        conn.sendMessage(m.chat, { audio: { url: res.result.audio[0] }, mimetype: 'audio/mpeg' }, { quoted: m });         
+      }
+    } catch (e) {
+      console.log(e);
+      throw `🚩 *Terjadi kesalahan!*`;
+    }
+  }
+};
+
+handler.command = handler.help = ['douyinslide', 'douyinfoto','ttslide','tiktokslide'];
+handler.tags = ['downloader'];
+handler.limit = true;
+
+module.exports = handler;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
