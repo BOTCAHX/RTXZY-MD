e2615f60831c41bce93746ad290db3fb1df07d54
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-06-15 15:12:16 +0700

    Update downloader-soundcloud.js

--- plugins/downloader-soundcloud.js
+++ plugins/downloader-soundcloud.js
@@ -4,16 +4,15 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 	if (!text) throw `*Usage : ${usedPrefix + command} url*\n\nExample: ${usedPrefix + command} https://soundcloud.com/issabella-marchelina/sisa-rasa-mahalini-official-audio?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing`
 	if (!(text.includes('http://') || text.includes('https://'))) throw `url invalid, please input a valid url. Try with add http:// or https://`
 	try {
-		let res = await fetch(`https://api.botcahx.eu.org/api/dowloader/soundcloud?url=${text}&apikey=${btc}`)
+		let res = await fetch(`https://api.botcahx.eu.org/api/download/soundcloud?url=${text}&apikey=${btc}`)
 		let anu = await res.json()
 		anu = anu.result
 		let ini_txt = `*${anu.title}*\n\n`
-		ini_txt += `⭔ Duration : ${anu.duration}\n`
-		ini_txt += `⭔ Quality : *${anu.quality}*`
+		
 		await conn.sendFile(m.chat, anu.thumbnail, 'scloud.jpg', ini_txt, m)
 		conn.sendMessage(m.chat, {
                 audio: {
-                    url: anu.download
+                    url: anu.url
                 },
                 mimetype: 'audio/mpeg',
                 contextInfo: {
@@ -21,7 +20,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
                         title: anu.title,
                         body: "",
                         thumbnailUrl: anu.thumbnail,
-                        sourceUrl: anu.download,
+                        sourceUrl: anu.url,
                         mediaType: 1,
                         showAdAttribution: true,
                         renderLargerThumbnail: true
