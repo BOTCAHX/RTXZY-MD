bd856ec92f86840f18daea98d9a93e8703388b40
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-07-09 20:58:12 +0700

    Update _openai-blackbox.js

--- plugins/_openai-blackbox.js
+++ plugins/_openai-blackbox.js
@@ -1,5 +1,6 @@
-var fetch = require('node-fetch');
-var handler = async (m, {
+let fetch = require('node-fetch');
+
+let handler = async (m, {
  text, 
  usedPrefix, 
  command
@@ -7,12 +8,11 @@ var handler = async (m, {
 if (!text) throw `Masukkan pertanyaan!\n\n*Contoh:* buatkan saya code express.js`
 try {
   await m.reply(wait)
-  var apii = await fetch(`https://api.botcahx.eu.org/api/search/blackbox-chat?text=${text}&apikey=${btc}`)
-  var res = await apii.json()
+  let res = await (await fetch(`https://api.botcahx.eu.org/api/search/blackbox-chat?text=${text}&apikey=${btc}`)).json()
   await m.reply(res.message)
 } catch (err) {
   console.error(err)
-  throw "Terjadi kesalahan dalam menjawab pertanyaan"
+  throw eror
 }
 }
 handler.command = handler.help = ['blackbox','blackboxai','aicoding'];
