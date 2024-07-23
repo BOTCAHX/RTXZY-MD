b050834c2ba39a9e79184bd8909477a657ec3c93
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-07-23 15:48:56 +0700

    Update downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -1,3 +1,92 @@
+let search = require("yt-search");
+let { youtube } = require("btch-downloader");
+
+let handler = async (m, { conn, text, usedPrefix }) => {
+    if (!text) throw 'Enter Title / Link From YouTube!';
+    try {
+        const look = await search(text);
+        const convert = look.videos[0];
+        if (!convert) throw 'Video/Audio Tidak Ditemukan';
+        if (convert.seconds >= 3600) {
+            return conn.reply(m.chat, 'Video is longer than 1 hour!', m);
+        } else {
+            let audioUrl;
+            try {
+                audioUrl = await youtube(convert.url);
+            } catch (e) {
+                conn.reply(m.chat, 'Please wait...', m);
+                audioUrl = await youtube(convert.url);
+            }
+
+            let caption = '';
+            caption += `∘ Title : ${convert.title}\n`;
+            caption += `∘ Ext : Search\n`;
+            caption += `∘ ID : ${convert.videoId}\n`;
+            caption += `∘ Duration : ${convert.timestamp}\n`;
+            caption += `∘ Viewers : ${convert.views}\n`;
+            caption += `∘ Upload At : ${convert.ago}\n`;
+            caption += `∘ Author : ${convert.author.name}\n`;
+            caption += `∘ Channel : ${convert.author.url}\n`;
+            caption += `∘ Url : ${convert.url}\n`;
+            caption += `∘ Description : ${convert.description}\n`;
+            caption += `∘ Thumbnail : ${convert.image}`;
+
+            await conn.relayMessage(m.chat, {
+                extendedTextMessage: {
+                    text: caption,
+                    contextInfo: {
+                        externalAdReply: {
+                            title: convert.title,
+                            mediaType: 1,
+                            previewType: 0,
+                            renderLargerThumbnail: true,
+                            thumbnailUrl: convert.image,
+                            sourceUrl: audioUrl.mp3
+                        }
+                    },
+                    mentions: [m.sender]
+                }
+            }, {});
+
+            await conn.sendMessage(m.chat, {
+                audio: {
+                    url: audioUrl.mp3
+                },
+                mimetype: 'audio/mpeg',
+                contextInfo: {
+                    externalAdReply: {
+                        title: convert.title,
+                        body: "",
+                        thumbnailUrl: convert.image,
+                        sourceUrl: audioUrl.mp3,
+                        mediaType: 1,
+                        showAdAttribution: true,
+                        renderLargerThumbnail: true
+                    }
+                }
+            }, {
+                quoted: m
+            });
+        }
+    } catch (e) {
+        conn.reply(m.chat, `*Error:* ` + e.message, m);
+    }
+};
+
+handler.command = handler.help = ['play', 'song', 'ds'];
+handler.tags = ['downloader'];
+handler.exp = 0;
+handler.limit = true;
+handler.premium = false;
+
+module.exports = handler;
+
+
+/**
+Matikan komentar ini jika ingin menggunakan ytdl-core kembali.
+/**
+
+/**
 let ytdl = require('ytdl-core');
 let fs = require('fs');
 let ffmpeg = require('fluent-ffmpeg');
@@ -96,3 +185,7 @@ function formatViews(views) {
     return views.toString();
   }
 }
+
+
+
+**/
