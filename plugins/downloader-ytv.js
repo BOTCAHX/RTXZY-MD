6249ba5b400d7348b3ba074f77716b05b20a57e9
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-07-23 15:42:54 +0700

    Update downloader-ytv.js

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -1,3 +1,36 @@
+const { youtube } = require('btch-downloader');
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `*Example:* ${usedPrefix + command} https://www.youtube.com/watch?v=Z28dtg_QmFw` 
+  try {
+    const data = await youtube(text);
+    await conn.sendMessage(m.chat, { 
+      video: { url: data.mp4 }, 
+      mimetype: 'video/mp4' 
+    }, { quoted: m });
+  } catch (error) {
+    console.error(error);
+    throw eror
+  }
+};
+
+handler.help = handler.command = ['ytshorts', 'shorts', 'short', 'ytmp4'];
+handler.tags = ['downloader'];
+handler.limit = true;
+handler.premium = false;
+
+module.exports = handler;
+
+
+
+
+/** 
+
+Jika ingin menggunakan ytdl-core maka matikan komentar ini!
+/** 
+
+/**
+
 const fs = require("fs");
 const ytdl = require("ytdl-core");
 
@@ -73,3 +106,5 @@ handler.command = /^yt(v|mp4)?$/i;
 handler.register = false;
 
 module.exports = handler;
+
+**/
