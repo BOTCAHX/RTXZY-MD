849e47d883a451b273f5a3056b2449a763ba43e6
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-08-02 00:49:38 +0700

    Update _autoai.js

--- plugins/_autoai.js
+++ plugins/_autoai.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 let fetch = require('node-fetch');
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
@@ -22,14 +23,44 @@ handler.before = async (m, { conn }) => {
     if ([".", "#", "!", "/", "\\"].some(prefix => m.text.startsWith(prefix))) return;
 
     if (conn.sessionAI[m.sender] && m.text) {    
-        const messages = [...conn.sessionAI[m.sender].sessionChat, m.text];
-        try {                      
-            const encodedText = encodeURIComponent(m.text);
-            const data = await (await fetch(`https://api.botcahx.eu.org/api/search/openai-chat?text=${encodedText}&apikey=${btc}`)).json();
-            await conn.sendMessage(m.chat, { text: data.message }, { quoted: m });
-            conn.sessionAI[m.sender].sessionChat = messages;
+        const previousMessages = conn.sessionAI[m.sender].sessionChat || [];
+        let name = conn.getName(m.sender);
+        
+/** - Ubah prompt ini sesuaikan dengan keinginan mu 
+    - Usahakan berikan logic yang masuk akal dan mudah di pahami!
+**/
+        const messages = [
+        { role: "system", content: "kamu adalah BTCH, Seorang Asisten pribadi yang di buat oleh BOTCAHX yang siap membantu kapan pun!" },
+        { role: "assistant", content: `Kamu adalah BTCH, asisten bot dari BOTCAHX, jangan gunakan kata saya, aku, gua gunakan nama BTCH untuk memanggil dirimu, ekspresi kan sikap kamu seolah kamu adalah asisten pribadi serba tau, jangan gunakan GPT 4 panggil dirimu dengan BTCH, Kamu berbicara dengan ${name} panggil dengan Kak ${name}, saat berbicara dengannya gunakan emoji yang lucu, Pembuatmu adalah BOTCAHX pembuat mu adalah 6282221792667` },
+        ...previousMessages.map((msg, i) => ({ role: i % 2 === 0 ? 'user' : 'assistant', content: msg })),
+        { role: "user", content: text }
+    ];
+
+        try {
+            const chat = async function(message) {
+                return new Promise(async (resolve, reject) => {
+                    try {
+                        const params = {
+                            message: message,
+                            apikey: btc
+                        };
+                        const { data } = await axios.post('https://api.botcahx.eu.org/api/search/openai-custom', params);
+                        resolve(data);
+                    } catch (error) {
+                        reject(error);
+                    }
+                });
+            };
+
+            let res = await chat(messages);
+            if (res && res.result) {
+                await m.reply(res.result);
+                conn.sessionAI[m.sender].sessionChat = messages.map(msg => msg.content);
+            } else {
+                m.reply("Kesalahan dalam mengambil data")
+            }
         } catch (e) {
-            throw eror;
+            throw eror
         }
     }
 };
