0884f74691dd01515bbd4c19526972d64396786d
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-08-25 20:51:37 +0700

    Update _openai-blackbox.js

--- plugins/_openai-blackbox.js
+++ plugins/_openai-blackbox.js
@@ -10,12 +10,12 @@ try {
   await m.reply(wait)
   let res = await (await fetch(`https://api.botcahx.eu.org/api/search/blackbox-chat?text=${text}&apikey=${btc}`)).json()
   await m.reply(res.message)
-} catch (err) {
-  console.error(err)
+} catch (e) {
   throw eror
 }
 }
 handler.command = handler.help = ['blackbox','blackboxai','aicoding'];
 handler.tags = ['tools'];
-handler.premium = false
+handler.limit = true
+
 module.exports = handler;
