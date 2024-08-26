76814027954ec99a4b9f7ef3f21a08e226408b92
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-08-26 20:07:23 +0700

    Update tools-bitly.js

--- plugins/tools-bitly.js
+++ plugins/tools-bitly.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  if (!text) throw 'Masukan url/link nya mana?\n> .bitly2 https://google.com'
+  if (!text) throw 'Masukan url/link nya mana?\n> .bitly https://google.com'
   let res = await fetch(`https://api.botcahx.eu.org/api/linkshort/bitly?link=${text}&apikey=${btc}`)
   let json = await res.json()
   if (json.status) m.reply(json.result)
