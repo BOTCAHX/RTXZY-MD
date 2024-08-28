476e83b462be23dd4d69e30def7d51ee2b303e6f
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-08-29 02:59:38 +0700

    Update Result

--- plugins/info-server.js
+++ plugins/info-server.js
@@ -2,53 +2,64 @@ const os = require('os');
 const fetch = require('node-fetch');
 
 let handler = async (m, { conn }) => {
-  let format = await fetch('https://ip-json.vercel.app/')
-    .then(res => res.json())
-    .then(json => {
-      delete json.status;
-      let caption = `乂  *S E R V E R*\n\n`;
-      caption += `┌  ◦  OS : ${os.type()} (${os.arch()} / ${os.release()})\n`;
-      caption += `│  ◦  Ram : ${formatSize(os.totalmem() - os.freemem())} / ${formatSize(os.totalmem())}\n`;
-      for (let key in json.result) caption += `│  ◦  ${ucword(key)} : ${json.result[key]}\n`;
-      caption += `│  ◦  Uptime : ${toTime(os.uptime() * 1000)}\n`;
-      caption += `└  ◦  Processor : ${os.cpus()[0].model}\n\n`;
-      conn.relayMessage(m.chat, {
-        extendedTextMessage: {
-          text: caption, 
-          contextInfo: {
-            externalAdReply: {
-              title: `${toTime(os.uptime() * 1000)}`,
-              mediaType: 1,
-              previewType: 0,
-              renderLargerThumbnail: true,
-              thumbnailUrl: 'https://telegra.ph/file/cf4f28ed3b9ebdfb30adc.png',
-              sourceUrl: ''
-            }
-          },
-          mentions: [m.sender]
-        }
-      }, {})
-      deleteMessage();
-    })
-    .catch(error => {
-      console.log(error);
-      deleteMessage();
-    });
+  try {
+    let response = await fetch('https://ip-json.vercel.app/');
+    let json = await response.json();    
+    delete json.status;
+    let caption = `乂  *S E R V E R*\n\n`;
+    caption += `┌  ◦  OS : ${os.type()} (${os.arch()} / ${os.release()})\n`;
+    caption += `│  ◦  Ram : ${formatSize(os.totalmem() - os.freemem())} / ${formatSize(os.totalmem())}\n`;    
+    json.result.timeZones = [json.result.timeZones[0]];
+    let currency = json.result.currency || {};
+    let currencyCode = currency.code || 'N/A';
+    let currencyName = currency.name || 'N/A';
+
+    for (let key in json.result) {
+      if (key === 'currency') {
+        caption += `│  ◦  Currency Code : ${currencyCode}\n`;
+        caption += `│  ◦  Currency Name : ${currencyName}\n`;
+      } else {
+        caption += `│  ◦  ${ucword(key)} : ${json.result[key]}\n`;
+      }
+    }
+    caption += `│  ◦  Uptime : ${toTime(os.uptime() * 1000)}\n`;
+    caption += `└  ◦  Processor : ${os.cpus()[0].model}\n\n`;
+    conn.relayMessage(m.chat, {
+      extendedTextMessage: {
+        text: caption, 
+        contextInfo: {
+          externalAdReply: {
+            title: `${toTime(os.uptime() * 1000)}`,
+            mediaType: 1,
+            previewType: 0,
+            renderLargerThumbnail: true,
+            thumbnailUrl: 'https://telegra.ph/file/cf4f28ed3b9ebdfb30adc.png',
+            sourceUrl: ''
+          }
+        },
+        mentions: [m.sender]
+      }
+    }, {});
+  } catch (error) {
+    console.log(error);
+  } finally {
+    deleteMessage();
+  }
 };
 
-handler.command = handler.help = ['server']
-handler.tags = ['info']
+handler.command = handler.help = ['server'];
+handler.tags = ['info'];
 module.exports = handler;
 
 function deleteMessage() {
-  //chaunima😁   
+  //chaunima😁
 }
 
 function formatSize(bytes) {
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
   if (bytes === 0) return '0 Bytes';
   const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
-  return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+  return (Math.round(bytes / Math.pow(1024, i) * 100) / 100) + ' ' + sizes[i];
 }
 
 function ucword(str) {
