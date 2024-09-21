7a84fba13a8ac0f3da5a52143d1abc51afe25055
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-09-21 17:35:19 +0700

    Update internet-gimage.js

--- plugins/internet-gimage.js
+++ plugins/internet-gimage.js
@@ -1,18 +1,23 @@
-const fetch = require('node-fetch')
+let fetch = require('node-fetch');
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
-    const res = await (await fetch(`https://widipe.com/googleimage?query=${text}`)).json()
-    let image = pickRandom(res.result)
-    let link = image
-    conn.sendFile(m.chat, link, 'google.jpg', `*G O O G L E*\n*Result:* ${text}\n*Source:* https://google.com`, m)
-}
-handler.help = ['gimage <query>', 'image <query>']
-handler.tags = ['internet']
-handler.command = /^(gimage|image)$/i
+    if (!text) throw `Use example ${usedPrefix}${command} Minecraft`;  
+    try {
+    const res = await (await fetch(`https://api.botcahx.eu.org/api/search/googleimage?text1=${encodeURIComponent(text)}&apikey=${btc}`)).json();
+    if (!res.status) throw eror
+    let image = pickRandom(res.result).url;
+    conn.sendFile(m.chat, image, 'google.jpg', `*G O O G L E*\n*Result:* ${text}\n*Source:* https://google.com`, m);
+   } catch (e) {
+   throw eror
+  }
+};
 
-module.exports = handler
+handler.help = ['gimage <query>', 'image <query>'];
+handler.tags = ['internet'];
+handler.command = /^(gimage|image)$/i;
+
+module.exports = handler;
 
 function pickRandom(arr) {
-  return arr[Math.floor(Math.random() * arr.length)]
+    return arr[Math.floor(Math.random() * arr.length)];
 }
