6995fd32c474075877c764678ce2f42f24da8e1a
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-10-02 11:06:28 +0700

    Video & Audio

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,26 +1,42 @@
-const axios = require('axios');
+let axios = require('axios');
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `Masukan URL!\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`;    
     try {
         if (!text.match(/tiktok/gi)) throw `URL Tidak Ditemukan!`;        
         m.reply(wait);      
-        const response = await axios.get(`https://api.botcahx.eu.org/api/dowloader/tiktok?url=${text}&apikey=${btc}`);        
-        const res = response.data.result;      
-        var { video, title, title_audio, audio } = res;
-        let capt = `乂 *T I K T O K*\n\n`;
-        capt += `◦ *Title* : ${title}\n`;
-        capt += `◦ *Audio* : ${title_audio}\n`;
-        capt += `\n`;        
-        await conn.sendFile(m.chat, video, null, capt, m);
-        conn.sendMessage(m.chat, { audio: { url: audio[0] }, mimetype: 'audio/mpeg' }, { quoted: m });         
+
+        try {
+            const response = await axios.get(`https://api.botcahx.eu.org/api/dowloader/tiktok?url=${text}&apikey=${btc}`);
+            const res = response.data.result;      
+            var { video, title, title_audio, audio } = res;
+            if (!video[0] || !audio[0]) {
+                throw eror
+            }
+            let capt = `乂 *T I K T O K*\n\n`;
+            capt += `◦ *Title* : ${title}\n`;
+            capt += `◦ *Audio* : ${title_audio}\n`;
+            capt += `\n`;        
+            
+            if (video.length > 1) {
+                for (let v of video) {
+                    await conn.sendFile(m.chat, v, null, capt, m);
+                }
+            } else {
+                await conn.sendFile(m.chat, video[0], null, capt, m);
+            }
+            conn.sendMessage(m.chat, { audio: { url: audio[0] }, mimetype: 'audio/mpeg' }, { quoted: m });
+            return;
+        } catch (e) {
+            throw eror
+        }
     } catch (e) {
-        console.log(e);
-        throw `🚩 ${eror}`;
+        throw eror
     }
 };
+
 handler.help = ['tiktok'];
-handler.command = /^(tiktok|tt|tiktokdl|tiktoknowm)$/i
+handler.command = /^(tiktok|tt|tiktokdl|tiktoknowm)$/i;
 handler.tags = ['downloader'];
 handler.limit = true;
 handler.group = false;
