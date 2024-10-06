d557a9fb0bf1d118621bb25917b092e52fb12c9b
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-10-06 17:40:17 +0700

    Update main-api.js

--- plugins/main-api.js
+++ plugins/main-api.js
@@ -1,16 +1,35 @@
-let fetch = require('node-fetch')
-let handler = async (m, { text }) => {
-if (!text) throw `Masukan Apikey!`
+let fetch = require('node-fetch');
+
+let handler = async (m, { conn }) => {
   try {
-    let api = await fetch(`https://api.botcahx.eu.org/api/checkkey?apikey=${text}`)
-    let body = await api.text()
-    m.reply(body)  
+    conn.reply(wait)
+    let api = await fetch(`https://api.botcahx.eu.org/api/checkkey?apikey=${btc}`);
+    let body = await api.json();
+    let { 
+      email, 
+      username, 
+      limit, 
+      premium, 
+      expired, 
+      todayHit,
+      totalHit
+    } = body.result;
+    
+    let capt = `乂 *C H E C K   A P I K E Y*\n\n`;
+    capt += `◦ *Email*: ${email}\n`;
+    capt += `◦ *Username*: ${username}\n`;
+    capt += `◦ *Limit*: ${limit}\n`;
+    capt += `◦ *Premium*: ${premium}\n`;
+    capt += `◦ *Expired*: ${expired}\n`;
+    capt += `◦ *Today Hit*: ${todayHit}\n`;
+    capt += `◦ *Total Hit*: ${totalHit}\n\n`;
+    await conn.reply(m.chat, capt, m);
   } catch (e) {
-    console.log(e) 
-    m.reply('Apikey tidak terdaftar!')
+    throw eror
   }
-}          
-handler.command = handler.help = ['checkapi','api'];
+};
+
+handler.command = handler.help = ['checkapi', 'api'];
 handler.tags = ['main'];
-handler.private = true
-module.exports = handler;
+handler.owner = true;
+module.exports = handler
