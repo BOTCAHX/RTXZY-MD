2ce35b476bfa669f0197144fb7fd71933dd0ef96
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-10-07 11:43:06 +0700

    catch error

--- plugins/sticker-gif-toimg.js
+++ plugins/sticker-gif-toimg.js
@@ -1,32 +1,36 @@
-const fetch = require('node-fetch');
-const uploader = require('../lib/uploadFile');
+let fetch = require('node-fetch');
+let uploader = require('../lib/uploadFile');
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-	let q = m.quoted ? m.quoted : m
-	let mime = (q.msg || q).mimetype || q.mediaType || ''
-	if (/webp/.test(mime)) {
-		let buffer = await q.download()
-		await m.reply(wait)
-		try {
-			let media = await uploader(buffer)
-			let json;
-			if (command === 'togif') {		
-				json = await (await fetch(`https://api.botcahx.eu.org/api/tools/webp2mp4?url=${media}&apikey=${btc}`)).json();
-			} else if (command === 'toimg') {
-				json = await (await fetch(`https://api.botcahx.eu.org/api/tools/webp2png?url=${media}&apikey=${btc}`)).json();
-			}
-			await conn.sendFile(m.chat, json.result, null, "*DONE*", m)
-		} catch (err) {
-			throw eror
-		}
-	} else {
-		throw `Reply sticker with command ${usedPrefix + command}`
-	}
+    let q = m.quoted ? m.quoted : m;
+    let mime = (q.msg || q).mimetype || q.mediaType || '';
+    if (/webp/.test(mime)) {
+        let buffer = await q.download();
+        await m.reply(wait);
+        try {
+            let media = await uploader(buffer);
+            let json;
+            if (command === 'togif') {        
+                json = await (await fetch(`https://api.botcahx.eu.org/api/tools/webp2mp4?url=${media}&apikey=${btc}`)).json();
+            } else if (command === 'toimg') {
+                json = await (await fetch(`https://api.botcahx.eu.org/api/tools/webp2png?url=${media}&apikey=${btc}`)).json();
+            }
+            if (json && json.result) {
+                await conn.sendFile(m.chat, json.result, null, "*DONE*", m);
+            } else {
+                await m.reply('Error: Failed to convert file. Please try again.');
+            }
+        } catch (err) {
+            await m.reply('An error occurred while processing your request.');
+        }
+    } else {
+        await m.reply(`Reply sticker with command ${usedPrefix + command}`);
+    }
 }
 
-handler.help = ['toimg', 'togif']
-handler.tags = ['tools']
-handler.command = /^(toimg|togif)$/i
+handler.help = ['toimg', 'togif'];
+handler.tags = ['tools'];
+handler.command = /^(toimg|togif)$/i;
 handler.limit = true;
 
 module.exports = handler;
