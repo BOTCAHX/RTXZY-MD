b7c5543d837562e7d9336c8b1d3f9f71b51f1504
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-10-12 18:03:21 +0700

    Update _AI-text2img.js

--- plugins/_AI-text2img.js
+++ plugins/_AI-text2img.js
@@ -4,7 +4,7 @@ let handler = async (m, { text, usedPrefix, command }) => {
   if (!text) throw `🚩 *Masukan detail gambar!* `;
   try {
     conn.reply(m.chat, wait, m)
-    const res = await fetch(`https://api.botcahx.eu.org/api/maker/text2img?text=${text}&apikey=${btc}`).then(res => res.buffer());
+    const res = await fetch(`https://api.botcahx.eu.org/api/maker/text2img?apikey=${btc}&text=${text}`).then(res => res.buffer());
     conn.sendFile(m.chat, res, 'image.jpg', `Result: ${text}`, m);
   } catch (error) {
     m.reply(`Error: ${eror}`);
