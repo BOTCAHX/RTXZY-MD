c7eb661489057c29162aae5eeb36784ad4235067
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-10-20 19:38:47 +0700

    Create Internet-cosplay.js

---
+++ plugins/Internet-cosplay.js
@@ -0,0 +1,17 @@
+let fetch = require('node-fetch');
+
+let handler = async (m, { conn }) => {    
+    try {
+        const response = await fetch(`https://api.botcahx.eu.org/api/wallpaper/cosplay?apikey=${btc}`);
+        const buffer = await response.buffer();
+        conn.sendFile(m.chat, buffer, 'hasil.jpg', 'Random Cosplay', m);
+    } catch (err) {
+        throw eror
+    }
+}
+
+handler.help = handler.command = ['cosplay'];
+handler.tags = ['downloader'];
+handler.limit = true;
+
+module.exports = handler;
