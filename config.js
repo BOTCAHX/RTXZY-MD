0485230f4ce66f9903fb88d636ce764678f710ee
Soru/Yuzi-Tachibana.xy <93211817+Absoru@users.noreply.github.com>
2025-08-18 23:00:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-global.owner = ['62895331520602', '275664439611636'] // wajib di isi tidak boleh kosong
+global.owner = ['6289697488609', '275664439611636'] // wajib di isi tidak boleh kosong
 global.mods  = ['62895331520602', '275664439611636'] // wajib di isi tidak boleh kosong
 global.prems = ['62895331520602', '275664439611636'] // wajib di isi tidak boleh kosong
-global.nameowner = 'Tio' // wajib di isi tidak boleh kosong
-global.numberowner = '62895331520602' // wajib di isi tidak boleh kosong
+global.nameowner = 'Shiyta Own' // wajib di isi tidak boleh kosong
+global.numberowner = '6289697488609' // wajib di isi tidak boleh kosong
 global.mail = 'support@tioprm.eu.org' // wajib di isi tidak boleh kosong
 global.gc = 'https://chat.whatsapp.com/I5RpePh2b5u37OyFjzCNTr' // wajib di isi tidak boleh kosong
 global.instagram = 'https://instagram.com/prm2.0' // wajib di isi tidak boleh kosong
-global.wm = '© BOTCAHX' // isi nama bot atau nama kalian
+global.wm = '© YUZUHA' // isi nama bot atau nama kalian
 global.wait = '_*Tunggu sedang di proses...*_' // ini pesan simulasi loading
 global.eror = '_*Server Error*_' // ini pesan saat terjadi kesalahan
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*' // ini pesan simulasi saat loading pembuatan sticker
