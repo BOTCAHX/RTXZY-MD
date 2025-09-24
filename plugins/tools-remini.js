c312b7c8cbcb28fad692ce352e2f240140bf49ec
Tio <94074076+BOTCAHX@users.noreply.github.com>
2025-09-24 20:54:07 +0700

    Update tools-remini.js

--- plugins/tools-remini.js
+++ plugins/tools-remini.js
@@ -2,11 +2,11 @@ const fetch = require('node-fetch');
 const uploadImage = require('../lib/uploadImage.js');
 
 let handler = async (m, { conn, usedPrefix, command }) {
-  try {
     const q = m.quoted ? m.quoted : m;
     const mime = (q.msg || q).mimetype || q.mediaType || '';
     if (/^image/.test(mime) && !/webp/.test(mime)) {
       await conn.reply(m.chat, wait, m);
+      try {
       const img = await q.download();
       const out = await uploadImage(img);
       const api = await fetch(`https://api.botcahx.eu.org/api/tools/remini?url=${out}&apikey=${btc}`);
