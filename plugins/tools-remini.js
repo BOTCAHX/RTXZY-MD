66072bc8c784d91d1eff23f1544e7ad34d3b9bb6
Tio <94074076+BOTCAHX@users.noreply.github.com>
2025-09-24 21:05:23 +0700

    Update tools-remini.js

--- plugins/tools-remini.js
+++ plugins/tools-remini.js
@@ -1,26 +1,26 @@
 const fetch = require('node-fetch');
 const uploadImage = require('../lib/uploadImage.js');
 
-let handler = async (m, { conn, usedPrefix, command }) {
+let handler = async (m, { conn, usedPrefix, command }) => {
     const q = m.quoted ? m.quoted : m;
     const mime = (q.msg || q).mimetype || q.mediaType || '';
     if (/^image/.test(mime) && !/webp/.test(mime)) {
-      await conn.reply(m.chat, wait, m);
-      try {
-      const img = await q.download();
-      const out = await uploadImage(img);
-      const api = await fetch(`https://api.botcahx.eu.org/api/tools/remini?url=${out}&apikey=${btc}`);
-      const image = await api.json();
-      const { url } = image 
-       conn.sendFile(m.chat, url, null, wm, m);
+        await conn.reply(m.chat, wait, m);
+        try {
+            const img = await q.download();
+            const out = await uploadImage(img);
+            const api = await fetch(`https://api.botcahx.eu.org/api/tools/remini?url=${out}&apikey=${btc}`);
+            const image = await api.json();
+            const { url } = image;
+            conn.sendFile(m.chat, url, null, wm, m);
+        } catch (e) {
+            console.error(e);
+            m.reply(`Identifikasi gagal. Silakan coba lagi.`);
+        }
     } else {
-      m.reply(`Kirim gambar dengan caption *${usedPrefix + command}* atau tag gambar yang sudah dikirim.`);
+        m.reply(`Kirim gambar dengan caption *${usedPrefix + command}* atau tag gambar yang sudah dikirim.`);
     }
-  } catch (e) {
-    console.error(e);
-    m.reply(`Identifikasi gagal. Silakan coba lagi.`);
-  }
-}
+};
 
 handler.help = ['remini'];
 handler.tags = ['tools'];
