2543eafaab13e36c80d4b51fe10d6ceb63d23b9d
Tio <94074076+BOTCAHX@users.noreply.github.com>
2025-12-17 08:07:11 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -1255,6 +1255,23 @@ module.exports = {
     break
     }
  }
+},
+    async delete({ remoteJid, fromMe, id, participant }) {
+        if (fromMe) return
+        let chats = Object.entries(conn.chats).find(([user, data]) => data.messages && data.messages[id])
+        if (!chats) return
+        let msg = JSON.parse(chats[1].messages[id])
+        let chat = global.db.data.chats[msg.key.remoteJid] || {}
+        if (chat.delete) return
+        await this.reply(msg.key.remoteJid, `
+Terdeteksi @${participant.split`@`[0]} telah menghapus pesan
+Untuk mematikan fitur ini, ketik
+*.enable delete*
+`.trim(), msg, {
+            mentions: [participant]
+        })
+        this.copyNForward(msg.key.remoteJid, msg).catch(e => console.log(e, msg))
+    }
 }
 
 global.dfail = (type, m, conn) => {
