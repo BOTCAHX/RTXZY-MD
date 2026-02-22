2a77460e4bad9df4503c7736a43412c8baee744c
Tio <94074076+BOTCAHX@users.noreply.github.com>
2026-02-22 15:32:28 +0700

    Update rpg-craft.js

--- plugins/rpg-craft.js
+++ plugins/rpg-craft.js
@@ -150,7 +150,7 @@ let handler = async (m, { conn, command, args, usedPrefix }) => {
                         contextInfo: {
                             externalAdReply: {
                                 mediaType: 1,
-                                title: 'BETABOTZ RPG',
+                                title: wm,
                                 thumbnailUrl: 'https://telegra.ph/file/ed878d04e7842407c2b89.jpg',
                                 renderLargerThumbnail: true,
                                 sourceUrl: ''
