037fcb8619b54c2adc30ca14a1c338295e4a9185
Tio <94074076+BOTCAHX@users.noreply.github.com>
2026-02-27 16:12:49 +0700

    Update image URL in rpg-mancing.js

--- plugins/rpg-mancing.js
+++ plugins/rpg-mancing.js
@@ -28,7 +28,7 @@ let handler = async (m, {
 				let paus = Math.floor(Math.random() * 2);
 				let kepiting = Math.floor(Math.random() * 27);
 
-				let imageUrl = 'https://api.betabotz.eu.org/api/tools/get-upload?id=f/arit56zv.jpg';
+				let imageUrl = 'https://api.botcahx.eu.org/api/tools/get-upload?id=f/arit56zv.jpg';
 				let totalCatch = nila + bawal + ikan + lele + udang + paus + kepiting;
 
 				let mcng = `•  *Hasil Mancing:*
