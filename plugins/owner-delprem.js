77f72b5dd2ce591634aae895c4cbd6a3c12d2f13
Tio <94074076+BOTCAHX@users.noreply.github.com>
2026-03-27 20:18:20 +0700

    Update owner-delprem.js

--- plugins/owner-delprem.js
+++ plugins/owner-delprem.js
@@ -4,24 +4,43 @@ let baileys;
 
 let handler = async (m, { conn, text, usedPrefix }) => {
     if (!baileys) {
-    baileys = await loadBaileys();
-  }
+        baileys = await loadBaileys();
+    }
 
-  const { MessageType } = baileys;
-  
-  function no(number){
-    return number.replace(/\s/g, '').replace(/([@+-])/g, '');
-  }
-  
-  if (!text) {
-    return conn.reply(m.chat, `*『 G A G A L 』*\n\n${usedPrefix}unprem @tag/nomor\n\n*Example:* ${usedPrefix}unprem 6285764068784`, m);
-  }
+    function no(number) {
+        if (!number) return '';
+        return number.replace(/\s/g, '').replace(/[@+-]/g, '').replace(/[^0-9]/g, '');
+    }
 
-  text = no(text) + "@s.whatsapp.net";
-  global.db.data.users[text].premium = false;
-  global.db.data.users[text].premiumTime = 0;
-  
-  conn.reply(m.chat, `*Berhasil menghapus akses premium untuk @${text.split('@')[0]}.*`, m, { contextInfo: { mentionedJid: [text] } });
+    let who = '';
+
+    if (m.mentionedJid && m.mentionedJid.length > 0) {
+        who = m.mentionedJid[0];
+    } else if (text) {
+        let cleaned = no(text);
+        if (cleaned) {
+            who = cleaned + '@s.whatsapp.net';
+        }
+    }
+
+    if (!who) {
+        return conn.reply(m.chat, `*『 G A G A L 』*\n\n${usedPrefix}unprem @tag / nomor\n\n*Contoh:*\n${usedPrefix}unprem @user\n${usedPrefix}unprem 6285764068784`, m);
+    }
+
+    if (who.endsWith('@lid')) {
+        who = who.replace('@lid', '@s.whatsapp.net');
+    }
+
+    if (!global.db.data.users[who]) {
+        return conn.reply(m.chat, `❌ User dengan ID ${who.split('@')[0]} tidak ditemukan di database.`, m);
+    }
+
+    global.db.data.users[who].premium = false;
+    global.db.data.users[who].premiumTime = 0;
+
+    await conn.reply(m.chat, `*Berhasil menghapus akses premium untuk @${who.split('@')[0]}.*`, m, {
+        contextInfo: { mentionedJid: [who] }
+    });
 };
 
 handler.help = ['unprem'];
@@ -31,5 +50,3 @@ handler.owner = true;
 handler.fail = null;
 
 module.exports = handler;
-
-// hapis skibidi
