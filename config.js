6c59409951f8b9ec701e7827092de2853654e445
BOTCAHX <tioclkp02@gmail.com>
2026-08-03 14:00:20 +0700

    Remove autobio from config.js as it's controlled via .enable autobio

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ global.packname = "Made With"; // watermark stikcker packname
 global.author = "Bot WhatsApp"; // watermark stikcker author
 global.maxwarn = "5"; // Peringatan maksimum Warn
 
-global.autobio = false; // Set true/false untuk mengaktifkan atau mematikan autobio (default: false)
+
 global.spam = false; // Set true/false untuk anti spam (default: false)
 global.gcspam = false; // Set true/false untuk menutup grup ketika spam (default: false)
 
