e8c5f18cc2f22fee6a5ece4e7ba6ecadc2b9cf47
BOTCAHX <BOTCAHX@users.noreply.github.com>
2026-08-29 20:01:22 +0000

    fix: autoread private chat - kirim receipt tanpa participant

--- handler.js
+++ handler.js
@@ -1279,7 +1279,7 @@ export default {
              } catch (e) {
                  // console.log(m, m.quoted, e)
              }
-            if (opts['autoread'] && m.key?.id) await this.sendReadReceipt(m.chat, m.key.participant || m.sender, [m.key.id])
+            if (opts['autoread'] && m.key?.id) await this.sendReadReceipt(m.chat, m.isGroup ? (m.key.participant || m.sender) : null, [m.key.id])
         }
     },
 	
