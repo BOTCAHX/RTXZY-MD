db2b9c51366dcc39f7dd194fd1c18cd967bc62a9
BOTCAHX <tioclkp02@gmail.com>
2026-09-09 17:53:13 +0700

    fix: move image validation outside try/catch and fix throw eror bug in maker plugins

--- plugins/maker-fakedana.js
+++ plugins/maker-fakedana.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         
     } catch (e) {
         console.log(e);
-        throw eror;
+        m.reply('❌ Terjadi kesalahan saat memproses.');
     }
 };  
 
--- plugins/maker-faketele.js
+++ plugins/maker-faketele.js
@@ -29,7 +29,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
     } catch (e) {
         console.log(e);
-        throw eror
+        m.reply('❌ Terjadi kesalahan saat memproses.');
     }
 }
 
